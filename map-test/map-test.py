2fd14bfb1169e1cf6abd4aaf3f8c022cd81a2d47
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2021-05-10 13:29:45 -0700

    Add list comprehension.

--- map-test/map-test.py
+++ map-test/map-test.py
@@ -2,5 +2,11 @@
 def square(number):
     return number * number
 
-result = list(map(square, [3, 4, 5]))
+result_one = list(map(square, [3, 4, 5]))
 # returns [9, 16, 25]
+
+# alternative
+def map_list(action, numbers):
+    return [ action(number) for number in numbers ]
+
+result_two = map_list(lambda x: x * x, [3, 4, 5])
