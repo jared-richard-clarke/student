c0b4dc645556ffdbb6f97821f896340038f529f9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2021-11-08 16:37:13 -0800

    Create stack-object.rkt

---
+++ stack-object/stack-object.rkt
@@ -0,0 +1,28 @@
+#lang racket
+; === constructor ===
+(define (new-stack)
+  ; === object instance ===
+  (define stack '())
+  ; === methods ===
+  ; empty?: empty stack? true or false
+  (define (empty?)
+    (null? stack))
+  ; push!: adds one or more elements to the end of stack.
+  (define (push! args)
+    (set! stack (foldr cons stack args)))
+  ; peek: shows last element added to stack.
+  (define (peek)
+    (car stack))
+  ; pop!: removes last element from stack and returns that element.
+  (define (pop!)
+    (let ([item (car stack)])
+      (set! stack (cdr stack))
+      item))
+  ; === interface === 
+  (lambda (message . arguments)
+    (cond
+      [(eqv? message 'empty?) (empty?)]
+      [(eqv? message 'push!) (push! arguments)]
+      [(eqv? message 'peek) (peek)]
+      [(eqv? message 'pop!) (pop!)]
+      [else (error "invalid input")])))
