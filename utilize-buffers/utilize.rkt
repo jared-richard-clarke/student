413d01e6f0958326a38de78c0984094f501db8f7
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2021-12-30 18:38:32 -0800

    Use #lang macro for module.

--- utilize-buffers/utilize.rkt
+++ utilize-buffers/utilize.rkt
@@ -1,21 +1,22 @@
-(module utilize racket
-  (begin
-    (define pattern #rx"[uU]tili([zs]e|[zs]ed|[zs]ing)")
-    (define dictionary
-      #hash(["utilize" . "use"]
-            ["utilise" . "use"]
-            ["Utilize" . "Use"]
-            ["Utilise" . "Use"]
-            ["utilizes" . "uses"]
-            ["utilises" . "uses"]
-            ["utilized" . "used"]
-            ["utilised" . "used"]
-            ["utilizing" . "using"]
-            ["utilising" . "using"]
-            ["Utilizing" . "Using"]
-            ["Utilising" . "Using"]))
-    (define (replace text) 
-      (regexp-replace* pattern
-                       text
-                       (lambda (match other)
-                         (hash-ref dictionary match))))))
+#lang racket
+(define pattern #rx"[uU]tili([zs]e|[zs]ed|[zs]ing)")
+
+(define dictionary
+  #hash(["utilize" . "use"]
+        ["utilise" . "use"]
+        ["Utilize" . "Use"]
+        ["Utilise" . "Use"]
+        ["utilizes" . "uses"]
+        ["utilises" . "uses"]
+        ["utilized" . "used"]
+        ["utilised" . "used"]
+        ["utilizing" . "using"]
+        ["utilising" . "using"]
+        ["Utilizing" . "Using"]
+        ["Utilising" . "Using"]))
+
+(define (replace text) 
+  (regexp-replace* pattern
+                   text
+                   (lambda (match other)
+                     (hash-ref dictionary match))))
