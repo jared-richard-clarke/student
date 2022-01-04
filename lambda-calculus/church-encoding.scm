b27ce5d2b130240c441b2b40bde3753f7d237ae5
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-03 17:08:37 -0800

    Create church-encoding.scm

---
+++ lambda-calculus/church-encoding.scm
@@ -0,0 +1,11 @@
+; Church Encoding
+; reimplementation of the cons cell using only functions
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
