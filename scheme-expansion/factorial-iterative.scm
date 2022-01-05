1af02ba2bdb49407ebae195c322cf66c5c80cbf8
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-04 20:28:24 -0800

    Add factorial base

--- scheme-expansion/factorial-iterative.scm
+++ scheme-expansion/factorial-iterative.scm
@@ -1,3 +1,12 @@
+; factorial base
+(define (factorial x)
+  (letrec ([iter (lambda (result counter)
+                   (if (> counter x)
+                       result
+                       (iter (* result counter) (+ counter 1))))])
+    (iter 1 1)))
+
+; lambda expansion
 (define factorial-lambda
   (lambda (x)
     ((lambda (iter)
