d4ff28607d358fa997021e4180d2a43a25834fc3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-09 01:44:21 -0800

    Add macro expansion.

--- inside-scheme/count-nodes.scm
+++ inside-scheme/count-nodes.scm
@@ -5,3 +5,14 @@
     [(not (pair? tree)) 1]
     [else (+ (count-nodes (car tree))
              (count-nodes (cdr tree)))]))
+
+; macro expansion
+(define count-nodes-ex
+  (lambda (tree)
+    (if (null? tree)
+        (begin 0)
+        (if (not (pair? tree))
+            (begin 1)
+            (begin
+              (+ (count-nodes-ex (car tree))
+                 (count-nodes-ex (cdr tree))))))))
