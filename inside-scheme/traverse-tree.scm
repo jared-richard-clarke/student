03ad439aa4694d1426ffaa595a1a51a2d2a3dd64
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-09 01:23:53 -0800

    Delete traverse-tree.scm

--- inside-scheme/traverse-tree.scm
+++
@@ -1,7 +0,0 @@
-; Counts the nodes of a tree data structure
-(define (traverse tree)
-  (cond
-    [(null? tree) 0]
-    [(not (pair? tree)) 1]
-    [else (+ (traverse (car tree))
-             (traverse (cdr tree)))]))
