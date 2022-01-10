d53eca5a02c960b8a00ed665e4be628d1567f7b6
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-09 18:05:42 -0800

    Simplify macro expansions

--- scheme/macros/range-let.scm
+++ scheme/macros/range-let.scm
@@ -5,36 +5,25 @@
       lst
       (range (- number 1) (cons number lst))))
 
-; expanded to letrec
-((letrec ([range (lambda (number lst)
-                   (if (= number 0)
-                       lst
-                       (range (- number 1) (cons number lst))))])
+; letrec expansion
+((letrec ((range
+           (lambda (number lst)
+             (if (= number 0) lst (range (- number 1) (cons number lst))))))
    range)
- 11 '())
-
-; expanded to nested let expressions
-(let ([number 11]
-      [lst '()])
-  (let ([range #f])
-    (let ([temp (lambda (number lst)
-                  (if (= number 0)
-                      lst
-                      (range (- number 1) (cons number lst))))])
-      (set! range temp)
-      (let ()
-        (range number lst)))))
-
-; expanded to lambda expressions
-((lambda (number lst)
-   ((lambda (range)
-      ((lambda (temp)
-         (set! range temp)
-         ((lambda ()
-            (range number lst))))
-       (lambda (number lst)
-         (if (= number 0)
-             lst
-             (range (- number 1) (cons number lst))))))
-    #f))
- 11 '())
+ 11
+ '())
+; let expansion
+((let ((range #f))
+   (set! range
+         (lambda (number lst) (if (= number 0) lst (range (- number 1) (cons number lst)))))
+   (let () range))
+ 11
+ '())
+; lambda expansion
+(((lambda (range)
+    (set! range
+          (lambda (number lst) (if (= number 0) lst (range (- number 1) (cons number lst)))))
+    ((lambda () range)))
+  #f)
+ 11
+ '())
