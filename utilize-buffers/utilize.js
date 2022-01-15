c61b4d6c0c4a89442f0484cfb109e4e1e6418b84
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-14 16:15:09 -0800

    Add read, write, and transform streams

--- utilize-buffers/utilize.js
+++ utilize-buffers/utilize.js
@@ -1,4 +1,13 @@
-const replace_utilize = (function () {
+// TODO: add shebang line?
+const { createReadStream, createWriteStream } = require("fs");
+const { Transform } = require("stream");
+
+const readStream = createReadStream("test.txt");
+const writeStream = createWriteStream("outfile.txt");
+
+// replace(string) -> string
+// replaces 'utilize' and its variants with 'use' and its variants.
+const replace = (function () {
     const dictionary = {
         utilize: "use",
         utilise: "use",
@@ -22,3 +31,10 @@ const replace_utilize = (function () {
         });
     };
 }());
+
+const transform_text = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, replace(chunk.toString()));
+    }
+});
+readStream.pipe(transform_text).pipe(writeStream).on("error", console.error);
