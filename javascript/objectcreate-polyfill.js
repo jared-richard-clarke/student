951b04742e166047dc20e43849c82a9673a6f14d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-21 18:53:36 -0800

    Create objectcreate-polyfill.js

---
+++ javascript/objectcreate-polyfill.js
@@ -0,0 +1,10 @@
+// === polyfill by Douglas Crockford ===
+// creates object that inherits properties and methods from another.
+
+if (typeof Object.create !== 'function') {
+    Object.create = function (object) {
+        function F() {}
+        F.prototype = object;
+        return new F();
+    };
+}
