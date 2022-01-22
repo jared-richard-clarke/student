94b346442c86ce12e9c1df379410ada726f45389
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-21 18:27:02 -0800

    Create sum.js

---
+++ javascript/javascript-versions/sum.js
@@ -0,0 +1,18 @@
+// sum(...number) -> number
+// returns the sum of variable number arguments, adding pairwise from left to right.
+// sum(1, 2, 4) -> 7
+
+// === ES6 ===
+function sum_es6(...numbers) {
+    return numbers.reduce((result, number) => result + number);
+}
+
+// === ES5 ===
+"use strict";
+function sum_es5() {
+    var numbers = [];
+    for (var i = 0; i < arguments.length; i += 1) {
+        numbers[i] = arguments[i];
+    }
+    return numbers.reduce(function (result, number) { return result + number; });
+}
