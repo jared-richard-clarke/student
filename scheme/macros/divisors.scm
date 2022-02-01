3efebab5ba95f785ac401d95614948ac3fc494b7
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-01 06:45:18 -0800

    Create divisors.scm

---
+++ scheme/macros/divisors.scm
@@ -0,0 +1,53 @@
+; (divisors 10) -> '(5 2)
+; do syntax: (do ((var init update) ...) (test result ...) expr ...)
+
+(define divisors
+  (lambda (n)
+    (do ([i 2 (+ i 1)]
+         [lst '()
+             (if (integer? (/ n i))
+                 (cons i lst)
+                 lst)])
+      ((>= i n) lst))))
+
+; === letrec expansion ===
+(define divisors-letrec
+  (lambda (n)
+    (letrec ([loop (lambda (i lst)
+                     (if (>= i n)
+                         (begin (if #f #f #f) lst)
+                         (begin (loop (+ i 1)
+                                      (if (integer? (/ n i))
+                                          (cons i lst)
+                                          lst)))))])
+      (loop 2 (quote ())))))
+
+; === let expansion ===
+(define divisors-let
+  (lambda (n)
+    (let ((loop #f))
+      (let ([temp (lambda (i lst)
+                    (if (>= i n)
+                        (begin (if #f #f #f) lst)
+                        (begin (loop (+ i 1)
+                                     (if (integer? (/ n i))
+                                         (cons i lst)
+                                         lst)))))])
+        (set! loop temp)
+        (loop 2 (quote ()))))))
+
+; === lambda expansion ===
+(define divisors-lambda
+  (lambda (n)
+    ((lambda (loop)
+       ((lambda (temp)
+          (set! loop temp)
+          (loop 2 (quote ())))
+        (lambda (i lst)
+          (if (>= i n)
+              (begin (if #f #f #f) lst)
+              (begin (loop (+ i 1)
+                           (if (integer? (/ n i))
+                               (cons i lst)
+                               lst)))))))
+     #f)))
