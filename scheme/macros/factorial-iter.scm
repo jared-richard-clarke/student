1f2e968eb5d1b767374d84ab8c2318ca9b97803c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-31 19:39:51 -0800

    Revise macro expansions

--- scheme/macros/factorial-iter.scm
+++ scheme/macros/factorial-iter.scm
@@ -1,4 +1,8 @@
-; base
+
+; factorial: where n! is the product of all positive integers less than or equal to n.
+; ( iterative implementation )
+; example: (factorial 4) -> 24
+
 (define (factorial n)
   (let loop ([result 1]
              [number n])
@@ -6,24 +10,41 @@
         result
         (loop (* result number) (- number 1)))))
 
-; letrec expansion
-(define factorial-letr
+; === letrec expansion ===
+(define factorial-letrec
   (lambda (n)
-    ((letrec ((loop
-               (lambda (result number)
-                 (if (< number 1) result (loop (* result number) (- number 1))))))
+    ((letrec ([loop (lambda (result number)
+                      (if (< number 1)
+                          result
+                          (loop (* result number)
+                                (- number 1))))])
        loop)
-     1
-     n)))
+     1 n)))
 
-; lambda expansion
+; === let expansion ===
+(define factorial-let
+  (lambda (n)
+    ((let ([loop #f])
+       (let ([temp (lambda (result number)
+                     (if (< number 1)
+                         result
+                         (loop (* result number)
+                               (- number 1))))])
+         (set! loop temp)
+         loop))
+     1 n)))
+
+; === lambda expansion ===
 (define factorial-lambda
   (lambda (n)
     (((lambda (loop)
-        (set! loop
-              (lambda (result number)
-                (if (< number 1) result (loop (* result number) (- number 1)))))
-        ((lambda () loop)))
+        ((lambda (temp)
+           (set! loop temp)
+           loop)
+         (lambda (result number)
+           (if (< number 1)
+               result
+               (loop (* result number)
+                     (- number 1))))))
       #f)
-     1
-     n)))
+     1 n)))
