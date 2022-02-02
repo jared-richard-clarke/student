5d5a3b6fc7fb5f91c74fe2799de8efcbbe6e7ccb
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-01 17:13:27 -0800

    Simplify macro expansion

--- scheme/macros/factorial/factorial-do.scm
+++ scheme/macros/factorial/factorial-do.scm
@@ -2,28 +2,28 @@
 ; simplified macro expansion for clarity.
 
 (define (factorial n)
-  (do ([i n (- i 1)] [a 1 (* a i)])
-    ((zero? i) a)))
+  (do ([number n (- number 1)] [result 1 (* result number)])
+    ((zero? number) result)))
 
 ; === letrec expansion ===
 (define factorial-letrec
   (lambda (n)
-    (letrec ([loop (lambda (i a)
-                     (if (zero? i)
-                         a
-                         (loop (- i 1)
-                               (* a i))))])
+    (letrec ([loop (lambda (number result)
+                     (if (zero? number)
+                         result                        ; (begin (if #f #f #f) result)
+                         (loop (- number 1)            ; (begin (loop (do "step" number (- number 1))
+                               (* result number))))])  ;              (do "step" result (* result number))))))])
       (loop n 1))))
 
 ; === let expansion ===
 (define factorial-let
   (lambda (n)
     (let ([loop #f])
-      (let ([temp (lambda (i a)
-                    (if (zero? i)
-                        a
-                        (loop (- i 1)
-                              (* a i))))])
+      (let ([temp (lambda (number result)
+                    (if (zero? number)
+                        result                          ; (begin (if #f #f #f) result)
+                        (loop (- number 1)              ; (begin (loop (do "step" number (- number 1))
+                              (* result number))))])    ;              (do "step" result (* result number))))))])
         (set! loop temp)
         (loop n 1)))))
 
@@ -34,9 +34,9 @@
        ((lambda (temp)
           (set! loop temp)
           (loop n 1))
-        (lambda (i a)
-          (if (zero? i)
-              a
-              (loop (- i 1)
-                    (* a i))))))
+        (lambda (number result)
+          (if (zero? number)
+              result
+              (loop (- number 1)
+                    (* result number))))))
      #f)))
