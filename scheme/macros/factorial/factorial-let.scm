625c81339ac024c21aee718bb9a545aaece8498d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-01 16:01:59 -0800

    Create factorial-let.scm

---
+++ scheme/macros/factorial/factorial-let.scm
@@ -0,0 +1,50 @@
+; factorial: where n! is the product of all positive integers less than or equal to n.
+; ( iterative implementation )
+; example: (factorial 4) -> 24
+
+(define (factorial n)
+  (let loop ([result 1]
+             [number n])
+    (if (< number 1)
+        result
+        (loop (* result number) 
+              (- number 1)))))
+
+; === letrec expansion ===
+(define factorial-letrec
+  (lambda (n)
+    ((letrec ([loop (lambda (result number)
+                      (if (< number 1)
+                          result
+                          (loop (* result number)
+                                (- number 1))))])
+       loop)
+     1 n)))
+
+; === let expansion ===
+(define factorial-let
+  (lambda (n)
+    ((let ([loop #f])
+       (let ([temp (lambda (result number)
+                     (if (< number 1)
+                         result
+                         (loop (* result number)
+                               (- number 1))))])
+         (set! loop temp)
+         loop))
+     1 n)))
+
+; === lambda expansion ===
+(define factorial-lambda
+  (lambda (n)
+    (((lambda (loop)
+        ((lambda (temp)
+           (set! loop temp)
+           loop)
+         (lambda (result number)
+           (if (< number 1)
+               result
+               (loop (* result number)
+                     (- number 1))))))
+      #f)
+     1 n)))
