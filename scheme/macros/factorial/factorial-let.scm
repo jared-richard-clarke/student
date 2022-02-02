0e5bea535c88bc07b1e0a0ed68c298936908ce02
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-01 17:12:45 -0800

    Revert to original

--- scheme/macros/factorial/factorial-let.scm
+++ scheme/macros/factorial/factorial-let.scm
@@ -1,42 +1,50 @@
-; do syntax: (do ((var init update) ...) (test result ...) expr ...)
-; simplified macro expansion for clarity.
+; factorial: where n! is the product of all positive integers less than or equal to n.
+; ( iterative implementation )
+; example: (factorial 4) -> 24
 
 (define (factorial n)
-  (do ([number n (- number 1)] [result 1 (* result number)])
-    ((zero? number) result)))
+  (let loop ([result 1]
+             [number n])
+    (if (< number 1)
+        result
+        (loop (* result number) 
+              (- number 1)))))
 
 ; === letrec expansion ===
 (define factorial-letrec
   (lambda (n)
-    (letrec ([loop (lambda (number result)
-                     (if (zero? number)
-                         result                        ; (begin (if #f #f #f) result)
-                         (loop (- number 1)            ; (begin (loop (do "step" number (- number 1))
-                               (* result number))))])  ;              (do "step" result (* result number))))))])
-      (loop n 1))))
+    ((letrec ([loop (lambda (result number)
+                      (if (< number 1)
+                          result
+                          (loop (* result number)
+                                (- number 1))))])
+       loop)
+     1 n)))
 
 ; === let expansion ===
 (define factorial-let
   (lambda (n)
-    (let ([loop #f])
-      (let ([temp (lambda (number result)
-                    (if (zero? number)
-                        result                          ; (begin (if #f #f #f) result)
-                        (loop (- number 1)              ; (begin (loop (do "step" number (- number 1))
-                              (* result number))))])    ;              (do "step" result (* result number))))))])
-        (set! loop temp)
-        (loop n 1)))))
+    ((let ([loop #f])
+       (let ([temp (lambda (result number)
+                     (if (< number 1)
+                         result
+                         (loop (* result number)
+                               (- number 1))))])
+         (set! loop temp)
+         loop))
+     1 n)))
 
 ; === lambda expansion ===
 (define factorial-lambda
   (lambda (n)
-    ((lambda (loop)
-       ((lambda (temp)
-          (set! loop temp)
-          (loop n 1))
-        (lambda (number result)
-          (if (zero? number)
-              result
-              (loop (- number 1)
-                    (* result number))))))
-     #f)))
+    (((lambda (loop)
+        ((lambda (temp)
+           (set! loop temp)
+           loop)
+         (lambda (result number)
+           (if (< number 1)
+               result
+               (loop (* result number)
+                     (- number 1))))))
+      #f)
+     1 n)))
