f37a01d891ed78654a1225b152c60d9b8a7f13b5
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-01 18:02:54 -0800

    Create factorial-def.scm

---
+++ scheme/macros/factorial/factorial-def.scm
@@ -0,0 +1,41 @@
+(define (factorial n)
+  (define (iter result number)
+    (if (< number 1)
+        result
+        (iter (* result number)
+              (- number 1))))
+  (iter 1 n))
+
+; === letrec* expansion ===
+(define factorial-letrec*
+  (lambda (n)
+    (letrec* ([iter (lambda (result number)
+                      (if (< number 1)
+                          result
+                          (iter (* result number)
+                                (- number 1))))])
+             (iter 1 n))))
+
+; === let expansion ===
+(define factorial-let
+  (lambda (n)
+    (let ([iter #f])
+      (set! iter (lambda (result number)
+                   (if (< number 1)
+                       result
+                       (iter (* result number)
+                             (- number 1)))))
+      (let () (iter 1 n)))))
+
+; === lambda expansion ===
+(define factorial-lambda
+  (lambda (n)
+    ((lambda (iter)
+       (set! iter
+             (lambda (result number)
+               (if (< number 1)
+                   result
+                   (iter (* result number)
+                         (- number 1)))))
+       ((lambda () (iter 1 n))))
+     #f)))
