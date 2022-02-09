10c00166c730b8e94f7b61feec7bcd7be4eede0e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-09 08:26:53 -0800

    Create reader.go

---
+++ golang/interfaces/reader.go
@@ -0,0 +1,32 @@
+// strings.NewReader implements the io.Reader interface, which represents the read end of a stream of data.
+// io.Reader wraps the Read method. Read fills a given byte slice with data. 
+// Read returns the number of bytes pushed to the slice and an error value. Returns io.EOF when the stream ends.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
+// n = 8 err = <nil> b = [72 101 108 108 111 44 32 82]
+// b[:n] = "Hello, R"
+// n = 6 err = <nil> b = [101 97 100 101 114 33 32 82]
+// b[:n] = "eader!"
+// n = 0 err = EOF b = [101 97 100 101 114 33 32 82]
+// b[:n] = ""
