b2793af56ff8c06f2fe4c8d917fb3405a8bac1e4
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-10 12:38:37 -0800

    Create errors.go

---
+++ golang/errors/errors.go
@@ -0,0 +1,19 @@
+// The errors package as implemented in the Go standard library.
+// https://github.com/golang/go/blob/master/src/errors/errors.go
+
+package errors
+
+// New returns an error that formats as the given text.
+// Each call to New returns a distinct error value even if the text is identical.
+func New(text string) error {
+	return &errorString{text}
+}
+
+// errorString is a trivial implementation of error.
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
