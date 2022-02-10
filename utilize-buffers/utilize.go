7e4b2582fa2e1406802573bf19ff34ffb79213bd
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-10 10:59:51 -0800

    Wrap documention

--- utilize-buffers/utilize.go
+++ utilize-buffers/utilize.go
@@ -1,4 +1,7 @@
-// utilize replaces variations of "utilize" with variations of "use", writing the content of one plain text file into the body of another. Call program with commands -input "old.txt" -output "new.txt". Files must be plain text.
+// utilize replaces variations of "utilize" with variations of "use", 
+// writing the content of one plain text file into the body of another. 
+// Call program with commands -input "old.txt" -output "new.txt". Files must be plain text.
+
 package main
 
 import (
