a35cef08ec7eeda7ccb8032987638246a2489589
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-16 12:03:16 -0800

    Create linked-list.js

---
+++ javascript/data-structures/linked-list.js
@@ -0,0 +1,32 @@
+// === Linked List ===
+// Defined using LISP terminology.
+const node = function (data, node = null) {
+    return {
+        data: data,
+        node: node,
+    };
+};
+
+const cons = function (node, list) {
+    node.node = list;
+    return node;
+};
+const car = function (list) {
+    return list.data;
+};
+const cdr = function (list) {
+    return list.node;
+};
+// === List Construction ===
+const list = node(1, node(2, node(3)));
+// === list value ===
+// {
+//     data: 3,
+//     node: {
+//         data: 2,
+//         node: {
+//             data: 1,
+//             node: null,
+//         },
+//     },
+// };
