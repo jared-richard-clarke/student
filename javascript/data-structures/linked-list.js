de1555615f7d32a4558e62245c1ea74e7dd24e43
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-21 10:56:08 -0800

    Correct typos

--- javascript/data-structures/linked-list.js
+++ javascript/data-structures/linked-list.js
@@ -10,11 +10,11 @@ const node = function (data, node = null) {
 const list = node(1, node(2, node(3)));
 // === value ===
 // {
-//     data: 3,
+//     data: 1,
 //     node: {
 //         data: 2,
 //         node: {
-//             data: 1,
+//             data: 3,
 //             node: null,
 //         },
 //     },
