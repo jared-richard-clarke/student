99dba9259b8ed713bb9f74bbc9f42d21f24bad93
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-02-21 11:20:23 -0800

    Add "not" conditional

--- javascript/boolean-functions/conditionals.js
+++ javascript/boolean-functions/conditionals.js
@@ -12,19 +12,24 @@ const conditionals = (function () {
   ]);
   // and(...expressions) -> boolean
   function and(...expressions) {
-    return expressions.every((value) => {
+    return expressions.every(function (value) {
       return edge_cases.has(value) ? edge_cases.get(value) : value;
     });
   }
   // or(...expressions) -> boolean
   function or(...expressions) {
-    return expressions.some((value) => {
+    return expressions.some(function (value) {
       return edge_cases.has(value) ? edge_cases.get(value) : value;
     });
   }
-  // interface: const { and, or } = conditionals; 
+  // not(expression) -> boolean
+  function not(value) {
+  	return !(edge_cases.has(value) ? edge_cases.get(value) : value);
+  }
+  // interface: const { and, or, not } = conditionals; 
   return Object.freeze({
     and,
-    or
+    or,
+    not
   });
 }());
