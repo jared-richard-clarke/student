4ed0dc30c1bdf01b7287f91e73410549a6fab862
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-03 10:26:42 -0800

    Add traverse function

--- javascript/data-structures/linked-list.js
+++ javascript/data-structures/linked-list.js
@@ -6,6 +6,13 @@ function link(head, tail = null) {
     return list;
 }
 
+function traverse(list, action) {
+  list.head = action(list.head);
+  if (list.tail !== null) {
+    traverse(list.tail, action);
+  }
+}
+
 // === Link Construction ===
 const linked_list = link(1, link(2, link(3)));
 // === value ===
