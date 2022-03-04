476511eefcfa81efc6b9de2047e7e8de2da3f90e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-03 17:30:09 -0800

    Add functional linked list

--- python/data-structures/linked-list.py
+++ python/data-structures/linked-list.py
@@ -1,4 +1,4 @@
-# === Linked List ===
+# === Linked List: Object Oriented ===
 class Link:
     def __init__(self, head, tail = None):
         self.head = head
@@ -12,4 +12,20 @@ class Link:
 # === Examples ====
 linked_list = Link(1, Link(2, Link(3)))
 linked_list.traverse(lambda x: x * x)
-four = linked_list.tail.head
+
+# === Linked List: Functional ===
+def link(head, tail = None):
+    linker = dict()
+    linker["head"] = head
+    linker["tail"] = tail
+    return linker
+
+def traverse(linked_list, action):
+    if linked_list["tail"] == None:
+        return link(action(linked_list["head"]))
+    else:
+        return link(action(linked_list["head"]), traverse(linked_list["tail"], action))
+
+# === Examples ===
+fun_list = link(1, link(2, link(3)))
+square_list = traverse(fun_list, lambda x: x * x)
