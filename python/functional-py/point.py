465cff0c2846e70c4cacb25c3aaac2fac35b025d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-04 16:06:12 -0800

    Update and rename point-distance.py to point.py

--- python/functional-py/point.py
+++ python/functional-py/point.py
@@ -1,5 +1,11 @@
 from math import sqrt
 
+# point(number, number) -> tuple
+# Constructs a two dimensional point represented as a tuple.
+# point(1, 2) -> (1, 2)
+def point(x, y):
+    return (x, y)
+
 # distance(tuple, tuple) -> float
 # Calculates the distance between two points.
 # distance((3, 0), (2, 0)) -> 1.0
