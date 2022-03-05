d21f8289c0a1860829983ff5e5db575bc109c354
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-04 20:39:29 -0800

    Update regular expressions

--- javascript/functional-js/palindrome.js
+++ javascript/functional-js/palindrome.js
@@ -1,19 +1,34 @@
-// === functions ===
-const lowercase = (text) => text.toLowerCase();
-const strip_whitespace = (text) => text.replace(/\s+/g, "");
-const strip_punctuation = (text) => text.replace(/[?!.]+/g, "");
-const reverse = (text) => text.split("").reverse().join("");
-const is_equal = (x, y) => x === y;
+const lowercase = function (text) {
+    return text.toLowerCase();
+};
+const strip_whitespace = function (text) {
+    return text.replace(/\s/g, "");
+};
+const strip_punctuation = function (text) {
+    return text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+};
+const reverse = function (text) {
+    return text.split("").reverse().join("");
+};
+const is_equal = function (x, y) {
+    return x === y;
+};
 
 const pipe = function (...actions) {
     return function (input) {
-        return actions.reduce((reduction, action) => action(reduction), input);
+        return actions.reduce(function (accum, action) {
+            return action(accum);
+        }, input);
     };
 };
 
-// === composite functions ===
 const clean = pipe(lowercase, strip_whitespace, strip_punctuation);
 
+// is_palindrome(string) -> boolean
+// Check whether text is spelled the same backwards and forwards.
+// is_palindrome("Anna") -> true
 const is_palindrome = function (corpus) {
-    return is_equal(clean(corpus), reverse(clean(corpus)));
+    const text = clean(corpus);
+    const txet = reverse(text);
+    return is_equal(text, txet);
 };
