6cca471859784f6ca177338a132fee4de2bdb94d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-04 20:45:11 -0800

    Make whitespace regex explicit

--- javascript/functional-js/palindrome.js
+++ javascript/functional-js/palindrome.js
@@ -2,7 +2,8 @@ const lowercase = function (text) {
     return text.toLowerCase();
 };
 const strip_whitespace = function (text) {
-    return text.replace(/\s/g, "");
+    // whitespace before tab deliberate.
+    return text.replace(/[ \t\n\r\v\f]/g, "");
 };
 const strip_punctuation = function (text) {
     return text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
@@ -13,7 +14,6 @@ const reverse = function (text) {
 const is_equal = function (x, y) {
     return x === y;
 };
-
 const pipe = function (...actions) {
     return function (input) {
         return actions.reduce(function (accum, action) {
@@ -21,7 +21,7 @@ const pipe = function (...actions) {
         }, input);
     };
 };
-
+// composite function
 const clean = pipe(lowercase, strip_whitespace, strip_punctuation);
 
 // is_palindrome(string) -> boolean
