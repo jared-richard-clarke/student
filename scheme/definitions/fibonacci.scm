742fde59ed52d3470252d3205b96b00854b4e2d5
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-09 11:23:09 -0800

    Create fibonacci.scm

---
+++ scheme/definitions/fibonacci.scm
@@ -0,0 +1,42 @@
+#|        
+         |  0                       if n = 0
+  Fib(n) |  1                       if n = 1 
+         |  Fib(n - 1) + Fib(n - 2) otherwise 
+|#
+
+; === tree-recursion ===
+; Space consumption is exponential. Process expands like a tree.
+; In general, the number if steps required by a tree-recursive process
+; will be proportional to the number of nodes in the tree, while the space
+; will be proportional to the maximum depth of the tree.
+
+(define (fibonacci n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fibonacci (- n 1))
+                 (fibonacci (- n 2))))))
+
+#|
+  Depth: 3
+  Steps: 5
+                fibonacci 3
+                    /\
+                   /  \
+        fibonacci 2    fibonacci 1
+                 / \             |
+                /   \            1
+     fibonacci 1     fibonacci 0
+               |               |
+               1               0
+|#
+
+; === iterative ===
+(define (fibonacci-iter n)
+  (let loop ([x 1]
+             [y 0]
+             [count n])
+    (if (= count 0)
+        y
+        (loop (+ x y)
+              x
+              (- count 1)))))
