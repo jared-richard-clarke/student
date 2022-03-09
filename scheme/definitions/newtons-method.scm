0dc9521e5451c379ebf6347bed26f2808c289e3b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-09 15:17:49 -0800

    Update newtons-method.scm

--- scheme/definitions/newtons-method.scm
+++ scheme/definitions/newtons-method.scm
@@ -1,23 +1,22 @@
-#|
-  === Newton's Method for approximating square roots ===
-  √x = the y such that y ≥ 0 and y ^ 2 = x
-  Average y with x ÷ y in successive approximations — each average
-  closer than the last.
-  guess        quotient                 average
-  1            (2 ÷ 1) = 2              ((2 + 1) ÷ 2) = 1.5
-  1.5          (2 ÷ 1.5) = 1.3333       ((1.3333 ÷ 1.5) ÷ 2) = 1.4167
-  1.4167 ...
-|#
+
+;; === Newton's Method for approximating square roots ===
+;; √x = the y such that y ≥ 0 and y ^ 2 = x
+;; Average y with x ÷ y in successive approximations — each average
+;; closer than the last.
+;; guess        quotient                 average
+;; 1            (2 ÷ 1) = 2              ((2 + 1) ÷ 2) = 1.5
+;; 1.5          (2 ÷ 1.5) = 1.3333       ((1.3333 ÷ 1.5) ÷ 2) = 1.4167
+;; 1.4167 ...
 
 (define (square-root x)
   (fixed-point (average-damp (lambda (y) (/ x y)))
                1.0))
 
-; === fixed point ===
-; (fixed-point function number) -> number
-; A number x is called a fixed point of a function ƒ if x satisfies  
-; the equation ƒ(x) = x. For some functions ƒ we can locate a fixed point
-; by beginning with an initial guess and applying ƒ repeatedly.
+;; === fixed point ===
+;; (fixed-point function number) -> number
+;; A number x is called a fixed point of a function ƒ if x satisfies  
+;; the equation ƒ(x) = x. For some functions ƒ we can locate a fixed point
+;; by beginning with an initial guess and applying ƒ repeatedly.
 
 (define (fixed-point func first-guess)
   
@@ -34,10 +33,10 @@
   
   (try first-guess))
 
-; === average-damp ===
-; (average-damp function) -> (function number) -> number
-; Average damping forces successive approximations to converge
-; where they might otherwise loop infinitely.
+;; === average-damp ===
+;; (average-damp function) -> (function number) -> number
+;; Average damping forces successive approximations to converge
+;; where they might otherwise loop infinitely.
 
 (define (average-damp func)
   (let ([average (lambda (a b)
