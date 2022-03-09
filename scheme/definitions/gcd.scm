7f4a8c7baed82a0195c7719cbedd695dcf201b18
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-09 15:09:26 -0800

    Revise documentation

--- scheme/definitions/gcd.scm
+++ scheme/definitions/gcd.scm
@@ -1,8 +1,7 @@
-; === Euclid's Algorithm for Greatest Common Divisors ===
-; (GCD integer integer) -> integer
-; The greatest common divisor of x and y is defined to be the largest
-; integer that divides both x and y with no remainder.
-; (GCD 10 5) -> 5
+;; === Euclid's Algorithm for Greatest Common Divisors ===
+;; (GCD integer integer) -> integer
+;; The greatest common divisor of x and y is the largest integer that divides both x and y with no remainder.
+;; (GCD 10 5) -> 5
 
 (define (GCD x y)
   (if (= y 0)
