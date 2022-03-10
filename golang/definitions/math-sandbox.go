bfeeeca74f7699e2488182e9b27f0fe28f0d8e38
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-09 18:32:44 -0800

    Add SquareRoot function

--- golang/definitions/math-sandbox.go
+++ golang/definitions/math-sandbox.go
@@ -1,5 +1,32 @@
 package mathsandbox
 
+import "math"
+
+// === Newton's Method for approximating square roots ===
+// √x = the y such that y ≥ 0 and y ^ 2 = x
+// Average y with x ÷ y in successive approximations — each average
+// closer than the last.
+// guess        quotient                 average
+// 1            (2 ÷ 1) = 2              ((2 + 1) ÷ 2) = 1.5
+// 1.5          (2 ÷ 1.5) = 1.3333       ((1.3333 ÷ 1.5) ÷ 2) = 1.4167
+// 1.4167 ...
+
+func SquareRoot(n float64) float64 {
+	tolerance := 0.00001
+	guess := 1.0
+	approximate := func(g float64) float64 {
+		return (g + n/g) / 2
+	}
+	next := approximate(guess)
+	for {
+		if math.Abs(guess-next) < tolerance {
+			return next
+		}
+		guess = next
+		next = approximate(guess)
+	}
+}
+
 // === Greatest Common Divisor ===
 // The greatest common divisor of x and y is the largest integer that divides both x and y with no remainder.
 // GCD(10 5) -> 5
