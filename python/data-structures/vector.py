3f6742fa8aec805c61246effd7d72766a9d07b7c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-10 00:32:40 -0800

    Create vector.py

---
+++ python/data-structures/vector.py
@@ -0,0 +1,21 @@
+import math
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def is_vector(data):
+        return type(data) == Vector
+
+    def coordinates(self):
+        return (self.x, self.y)
+
+    def magnitude(self):
+        return math.hypot(self.x, self.y)
+
+    def scale(self, factor):
+        self.x *= factor
+        self.y *= factor
