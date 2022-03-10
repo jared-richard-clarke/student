e76eb0ee6ef84e74321f977258e5c482acf57e41
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-10 13:42:08 -0800

    Rename m, message

--- scheme/lambda-calculus/message-passing.scm
+++ scheme/lambda-calculus/message-passing.scm
@@ -1,10 +1,10 @@
 ;; Message passing style as defined in The Structure and Interpretation of Computer Programs.
-;; Implementation of cons cell using procedures.
+;; Implementation of cons cell using procedures. Alternative implementation of Church Encoding.
 
 (define (cons x y)
-  (define (dispatch m)
-    (cond ((= m 0) x)
-          ((= m 1) y)
+  (define (dispatch message)
+    (cond ((= message 0) x)
+          ((= message 1) y)
           (else (error "Argument not 0 or 1 -- CONS" m))))
   dispatch)
 
