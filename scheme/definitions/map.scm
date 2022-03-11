605ed01e9c1661dd03241e550c18db8417f02c94
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-10 18:56:04 -0800

    Add simplified map definition

--- scheme/definitions/map.scm
+++ scheme/definitions/map.scm
@@ -1,3 +1,7 @@
+;; map function as defined in The Scheme Programming Language.
+;; Unlike the map-simple definition below, this implementation can iterate over multiple lists.
+;; Both definitions omit error handling for simplificity.
+
 (define map
   (lambda (func lst . more)
     (if (null? more)
@@ -15,3 +19,14 @@
                            (map car more))
                     (map-more (cdr lst)
                               (map cdr more))))))))
+
+;; (map-simple function list) -> list
+;; Constructs a new sequence populated with the results of calling 
+;; the provided function on each element of the provided sequence.
+;; (map-simple add-1 '(1 2 3)) -> '(2 3 4)
+
+(define (map-simple action sequence)
+  (if (null? sequence)
+      null
+      (cons (action (car sequence))
+            (map-simple action (cdr sequence)))))
