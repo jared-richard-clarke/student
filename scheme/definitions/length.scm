92f11251b332bd974184465aa1a7ea79a73b33db
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-13 22:31:15 -0700

    Redefine length function

--- scheme/definitions/length.scm
+++ scheme/definitions/length.scm
@@ -1,24 +1,17 @@
-; function: (length list)
-; purpose: counts elements in list
+; (length list) -> number
+; Counts elements in proper list.
 ; (length '(1 2 3)) -> 3
 ; (length '()) -> 0
 
-(define length
-  (lambda (x)
-    (define improper-list
-      (lambda ()
-        (assertion-violation 'length "not a proper list" x)))
-
-    (let f ([h x] [t x] [n 0])
-      (if (pair? h)
-          (let ([h (cdr h)])
-            (if (pair? h)
-                (if (eq? h t)
-                    (improper-list)
-                    (f (cdr h) (cdr t) (+ n 2)))
-                (if (null? h)
-                    (+ n 1)
-                    (improper-list))))
-          (if (null? h)
-              n
-              (improper-list))))))
+(define (length lst)
+  (define improper-list
+    (lambda ()
+      (error 'length "argument not a proper list")))
+  (if (not (list? lst))
+      (improper-list)
+      (let loop ([lst lst]
+                 [count 0])
+        (if (null? lst)
+            count
+            (loop (cdr lst)
+                  (+ count 1))))))
