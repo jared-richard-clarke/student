6b5f155dd581b0007c76c02931c9215ede258dbf
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-19 15:49:08 -0700

    Create fibonacci.go

---
+++ golang/goroutines/fibonacci.go
@@ -0,0 +1,26 @@
+// Channels are typed conduits through which values can be sent and received.
+// go routine "fibonacci" sends values to channel "c". "fibonacci" closes "c" at the end of its loop.
+// Example pulled from "https://go.dev/tour/concurrency/4"
+
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i += 1 {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
