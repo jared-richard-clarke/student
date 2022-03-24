749e76fcce955ee4956f849182d6e3f50466176c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-24 15:39:36 -0700

    Insert add method

--- scheme/data-structures/vector-object.scm
+++ scheme/data-structures/vector-object.scm
@@ -8,7 +8,14 @@
     (let ([x (car tip)]
           [y (cdr tip)])
       (sqrt (+ (sqr x) (sqr y)))))
-
+  
+  (define (add vect)
+    (let ([x1 (car tip)]
+          [y1 (cdr tip)]
+          [x2 (car (vect 'tip))]
+          [y2 (cdr (vect 'tip))])
+      (set! tip (cons (+ x1 x2) (+ y1 y2)))))
+  
   (define (scale factor)
     (set! tip (cons (* factor (car tip))
                     (* factor (cdr tip)))))
@@ -18,5 +25,6 @@
     (cond [(eq? message 'tip) tip]
           [(eq? message 'type) type]
           [(eq? message 'magnitude) (magnitude)]
+          [(eq? message 'add) (add (car arguments))]
           [(eq? message 'scale) (scale (car arguments))]
           [else (error "invalid input" message)])))
