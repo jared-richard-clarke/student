861534a79341449d2a285909129ec4bcc2a634c2
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-25 00:02:33 -0700

    Replace mutative methods with pure functions

--- scheme/data-structures/vector-object.scm
+++ scheme/data-structures/vector-object.scm
@@ -1,37 +1,49 @@
 ;; Object implementation of vectors and operations in linear vector space.
-;; Vector renamed 2d-vector to avoid namespace clash with vector function.
+
+;; (2d-vector number number) -> 2d-vector
+;; Creates 2d-vector object.
+;; (define vector-object (2d-vector 3 4))
+;; (vector-object 'point) -> '(3 . 4)
+;; (vector-object 'type) -> '2d-vector
+;; (vector-object 'magnitude) -> 5
 
 (define (2d-vector x y)
-  ;; === instance ===
-  ;; Contains vector object state.
-  ;; Call to receive instance of state.
+  ;; === properties ===
   (define point (cons x y))
-  ;; === methods ===
-  ;; Returns type of vector object.
+  
   (define type '2d-vector)
-  ;; Returns the magnitude of a vector object.
+  
   (define (magnitude)
     (let ([x (car point)]
           [y (cdr point)])
       (sqrt (+ (sqr x) (sqr y)))))
-  ;; Adds vector object to vector object. Mutates object. Returns void.
-  (define (add v)
-    (let ([x1 (car point)]
-          [y1 (cdr point)]
-          [x2 (car (v 'point))]
-          [y2 (cdr (v 'point))])
-      (set! point (cons (+ x1 x2) (+ y1 y2)))))
-  ;; Scales vector object by factor. Mutates object. Returns void.
-  (define (scale factor)
-    (set! point (cons (* factor (car point))
-                      (* factor (cdr point)))))
   
   ;; === interface ===
-  ;; Send specific messages to object to receive create specific behaviors
   (lambda (message . arguments)
     (cond [(eq? message 'point) point]
           [(eq? message 'type) type]
           [(eq? message 'magnitude) (magnitude)]
-          [(eq? message 'add) (add (car arguments))]
-          [(eq? message 'scale) (scale (car arguments))]
           [else (error "invalid input" message)])))
+
+;; (add 2d-vector 2d-vector) -> 2d-vector
+;; Computes and returns a 2d-vector that is the sum of two 2d-vectors.
+;; (add (2d-vector 1 2) (2d-vector 3 4)) -> (2d-vector 'point) -> '(4 . 5)
+
+(define (add v1 v2)
+  (let* ([p1 (v1 'point)]
+         [p2 (v2 'point)]
+         [x1 (car p1)]
+         [y1 (cdr p1)]
+         [x2 (car p2)]
+         [y2 (cdr p2)])
+    (2d-vector (+ x1 x2) (+ y1 y2))))
+
+;; (scale 2d-vector number) -> 2d-vector
+;; Returns a scales 2d-vector that is the product of a 2d-vector and a number.
+;; (scale (2d-vector 3 4) 2) -> (2d-vector 'point) -> '(6 . 8)
+
+(define (scale v factor)
+  (let* ([p (v 'point)]
+         [x (car p)]
+         [y (cdr p)])
+    (2d-vector (* x factor) (* y factor))))
