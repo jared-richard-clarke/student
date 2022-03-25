db465d4b8dcf496683c62e62255810ab3bae7af3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-24 20:00:42 -0700

    Rename bindings

--- scheme/data-structures/vector-object.scm
+++ scheme/data-structures/vector-object.scm
@@ -1,35 +1,35 @@
 ;; Object implementation of vectors and operations in linear vector space.
-;; Vector renamed vector-object to avoid namespace clash with vector function.
+;; Vector renamed 2d-vector to avoid namespace clash with vector function.
 
-(define (vector-object x y)
+(define (2d-vector x y)
   ;; === instance ===
   ;; Contains vector object state.
   ;; Call to receive instance of state.
-  (define tip (cons x y))
+  (define point (cons x y))
   ;; === methods ===
   ;; Returns type of vector object.
-  (define type 'vector-object)
+  (define type '2d-vector)
   ;; Returns the magnitude of a vector object.
   (define (magnitude)
-    (let ([x (car tip)]
-          [y (cdr tip)])
+    (let ([x (car point)]
+          [y (cdr point)])
       (sqrt (+ (sqr x) (sqr y)))))
   ;; Adds vector object to vector object. Mutates object. Returns void.
-  (define (add vect)
-    (let ([x1 (car tip)]
-          [y1 (cdr tip)]
-          [x2 (car (vect 'tip))]
-          [y2 (cdr (vect 'tip))])
-      (set! tip (cons (+ x1 x2) (+ y1 y2)))))
+  (define (add v)
+    (let ([x1 (car point)]
+          [y1 (cdr point)]
+          [x2 (car (v 'point))]
+          [y2 (cdr (v 'point))])
+      (set! point (cons (+ x1 x2) (+ y1 y2)))))
   ;; Scales vector object by factor. Mutates object. Returns void.
   (define (scale factor)
-    (set! tip (cons (* factor (car tip))
-                    (* factor (cdr tip)))))
+    (set! point (cons (* factor (car point))
+                      (* factor (cdr point)))))
   
   ;; === interface ===
   ;; Send specific messages to object to receive create specific behaviors
   (lambda (message . arguments)
-    (cond [(eq? message 'tip) tip]
+    (cond [(eq? message 'point) point]
           [(eq? message 'type) type]
           [(eq? message 'magnitude) (magnitude)]
           [(eq? message 'add) (add (car arguments))]
