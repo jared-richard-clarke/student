014d3dadf0d0f54cc2a28b5f371f26a878c99586
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-24 19:54:31 -0700

    Rename bindings

--- scheme/data-structures/vector-function.scm
+++ scheme/data-structures/vector-function.scm
@@ -1,38 +1,38 @@
 ;; Functional implementation of vectors and operations in linear vector space.
-;; Vector renamed vect to avoid namespace clash with vector function.
+;; Vector renamed 2d-vector to avoid namespace clash with vector function.
 
-;; (vect number number) -> pair
+;; (2d-vector number number) -> pair
 ;; Returns two-dimensional coordinates as a number pair.
-;; (vect 3 4) -> '(3 . 4)
+;; (2d-vector 3 4) -> '(3 . 4)
 
-(define (vect x y)
+(define (2d-vector x y)
   (cons x y))
 
-;; (add vect vect) -> vect
+;; (add 2d-vector 2d-vector) -> 2d-vector
 ;; Returns the sum of two vectors.
-;; (add (vect 1 2) (vect 1 2)) -> '(2 . 4)
+;; (add (2d-vector 1 2) (2d-vector 1 2)) -> '(2 . 4)
 
-(define (add vect-1 vect-2)
-  (let ([x1 (car vect-1)]
-        [y1 (cdr vect-1)]
-        [x2 (car vect-2)]
-        [y2 (cdr vect-2)])
+(define (add v1 v2)
+  (let ([x1 (car v1)]
+        [y1 (cdr v1)]
+        [x2 (car v2)]
+        [y2 (cdr v2)])
     (cons (+ x1 x2) (+ y1 y2))))
 
-;; (scale vect number) -> vect
+;; (scale 2d-vector number) -> vect
 ;; Returns a vector multiplied by a number.
-;; (scale (vect 1 2) 2) -> '(2 . 4)
+;; (scale (2d-vector 1 2) 2) -> '(2 . 4)
 
-(define (scale vect factor)
-  (let ([x (car vect)]
-        [y (cdr vect)])
+(define (scale v factor)
+  (let ([x (car v)]
+        [y (cdr v)])
     (cons (* factor x) (* factor y))))
 
-;; (magnitude vect) -> number
-;; Returns the magnitude of a vector.
-;; (magnitude (vect 3 4)) -> 5
+;; (magnitude 2d-vector) -> number
+;; Returns the magnitude of a 2d vector.
+;; (magnitude (2d-vector 3 4)) -> 5
 
-(define (magnitude vect)
-  (let ([x (car vect)]
-        [y (cdr vect)])
+(define (magnitude v)
+  (let ([x (car v)]
+        [y (cdr v)])
     (sqrt (+ (sqr x) (sqr y)))))
