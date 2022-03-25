bd8aa61d23e638af7ab125b105d6ef9b512134dc
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-25 00:12:48 -0700

    Rename bindings, revise comments

--- scheme/data-structures/point.scm
+++ scheme/data-structures/point.scm
@@ -7,19 +7,19 @@
 
 ; (point-distance pair pair) -> number
 ; Calculates the distance between two points.
-; (point-distance '(3, 0) '(2, 0)) -> 1
+; (point-distance (point 3 0) (point 2 0)) -> 1
 
 (define point-distance
   (let ([hypotenuse (lambda (x y)
                       (sqrt (+ (sqr x) (sqr y))))])
-    (lambda (x y)
-      (let ([x1 (car x)]
-            [x2 (cdr x)]
-            [y1 (car y)]
-            [y2 (cdr y)])
-        (hypotenuse (- y1 x1) (- y2 x2))))))
+    (lambda (p1 p2)
+      (let ([x1 (car p1)]
+            [y1 (cdr p1)]
+            [x2 (car p2)]
+            [y2 (cdr p2)])
+        (hypotenuse (- x2 x1) (- y2 y1))))))
 
-;; (path points) -> list of point pairs
+;; (path points) -> list
 ;; Returns a list of points.
 ;; (path (point 1 2) (point 3 4)) -> '((1 . 2) (3 . 4))
 
@@ -27,7 +27,7 @@
   points)
 
 ;; (path-length path) -> number
-;; Computes the path length of a series of paths.
+;; Computes the path length of a series of points.
 ;; (path-length (path (point 1 1) (point 5 1) (point 5 4) (point 1 1))) -> 12
 
 (define (path-length path)
