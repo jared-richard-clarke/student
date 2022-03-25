fc02a4ad4fa17c680ecb98977f9d733dc81e59ae
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-25 00:43:30 -0700

    Add path, revise hypotenuse

--- python/data-structures/point.py
+++ python/data-structures/point.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
 # point(number, number) -> tuple
-# Constructs a two dimensional point represented as a tuple.
+# Constructs a two dimensional point as a tuple.
 # point(1, 2) -> (1, 2)
 
 def point(x, y):
@@ -12,7 +12,15 @@ def point(x, y):
 # distance((3, 0), (2, 0)) -> 1.0
 
 def distance(p1, p2):
+    def hypotenuse(x, y):
+        return sqrt((x * x) + (y * y))
     x1, y1 = p1 
     x2, y2 = p2
-    hypotenuse = lambda x, y: sqrt((x * x) + (y * y))
     return hypotenuse(x2 - x1, y2 - y1)
+
+# path(points) -> tuple
+# Constructs a path as a tuple of points.
+# path(point(1, 2), point(3, 4)) -> ((1, 2), (3, 4))
+
+def path(*points):
+    return points
