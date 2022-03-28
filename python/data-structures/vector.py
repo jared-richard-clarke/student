87420bcee2dd37f8424d59db5acd2dda80e9fa07
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-27 22:51:52 -0700

    Add unit_vector and add

--- python/data-structures/vector.py
+++ python/data-structures/vector.py
@@ -14,6 +14,10 @@ class Vector:
 
     def magnitude(self):
         return math.hypot(self.x, self.y)
+
+    def unit_vector(self):
+        mag = self.magnitude()
+        return (self.x / mag, self.y / mag)
         
 def scale(v, factor):
     if type(v) != Vector:
@@ -21,3 +25,10 @@ def scale(v, factor):
     x = v.x
     y = v.y
     return Vector((x * factor), (y * factor))
+
+def add(v1, v2):
+    if type(v1) != Vector and type(v2) != Vector:
+        raise TypeError("Both arguments must be type Vector.")
+    x1, y1 = v1.x, v1.y
+    x2, y2 = v2.x, v2.y
+    return Vector(x1 + x2, y1 + y2)
