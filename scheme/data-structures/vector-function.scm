4ba7284640309ae756844f0e48dd0d5797833e68
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-27 18:36:22 -0700

    Remove memoize

--- scheme/data-structures/vector-function.scm
+++ scheme/data-structures/vector-function.scm
@@ -28,31 +28,11 @@
         [y (cdr v)])
     (cons (* factor x) (* factor y))))
 
-;; (magn 2d-vector) -> number
+;; (magnitude 2d-vector) -> number
 ;; Returns the magnitude of a 2d vector.
-;; (magn (2d-vector 3 4)) -> 5
+;; (magnitude (2d-vector 3 4)) -> 5
 
-(define (magn v)
+(define (magnitude v)
   (let ([x (car v)]
         [y (cdr v)])
     (sqrt (+ (sqr x) (sqr y)))))
-
-;; (memoize function) -> function -> value
-;; Wraps function in a function that stores previously-computed values.
-;; Eliminates redundant computation.
-;; (define add (memoize (lambda (x y) (+ x y))))
-;; (add 1 6) -> caches then returns 7
-
-(define (memoize function)
-  (define cache '())
-  (lambda args
-    (let ([cached (assoc args cache)])
-      (if (not cached)
-          (let ([result (apply function args)])
-            (set! cache (cons (cons args result) cache))
-            result)
-          (cdr cached)))))
-
-;; magnitude is magn except that it caches previously-computed values.
-
-(define magnitude (memoize magn))
