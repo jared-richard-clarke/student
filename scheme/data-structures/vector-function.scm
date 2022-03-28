7a23ea2406fa018fb83cffaa566380f8f94dc3dd
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-27 23:41:19 -0700

    Add dot-product

--- scheme/data-structures/vector-function.scm
+++ scheme/data-structures/vector-function.scm
@@ -35,6 +35,17 @@
         [y (cdr v)])
     (cons (* factor x) (* factor y))))
 
+;; (dot-product 2d-vector 2d-vector) -> number
+;; Computes the dot product of two 2d-vectors.
+;; (dot-product (2d-vector 1 2) (2d-vector 3 4)) -> 11
+
+(define (dot-product v1 v2)
+  (let ([x1 (car v1)]
+        [y1 (cdr v1)]
+        [x2 (car v2)]
+        [y2 (cdr v2)])
+    (+ (* x1 x2) (* y1 y2))))
+
 ;; (magnitude 2d-vector) -> number
 ;; Returns the magnitude of a 2d vector.
 ;; (magnitude (2d-vector 3 4)) -> 5
