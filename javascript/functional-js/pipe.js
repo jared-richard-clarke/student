3972d453ffcb3010b572d8145456fe14906e32a6
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-01 12:15:36 -0700

    Document and reformat

--- javascript/functional-js/pipe.js
+++ javascript/functional-js/pipe.js
@@ -1,7 +1,12 @@
-function pipe( ...actions ) {
-    return function ( input ) {
+// pipe(...function) -> function -> value
+// Composes a series of pure functions into a single function expression.
+// Functions must take the same number of arguments as the values returned by the previous function.
+// const edit = pipe((text) => text.toLowerCase(), (text) => text.trim()) -> edit("  hEllO, worLd   ") -> "hello, world"
+
+function pipe(...actions) {
+    return function (input) {
         return actions.reduce(
-            ( accum, action ) => action( accum ), 
+            (accum, action) => action(accum), 
             input
         );
     };
