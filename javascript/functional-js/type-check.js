ff946485bc18cf9cacdb37d01e1716f61ab73533
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-01 11:57:31 -0700

    Narrow scope of type_check function

--- javascript/functional-js/type-check.js
+++ javascript/functional-js/type-check.js
@@ -1,5 +1,12 @@
+// type_check(string, function) -> function -> value or type error
+// Wraps single-argument function in a function that checks the type of its argument.
+// const add1 = type_check("number", (value) => value + 1) -> add1(10) -> 11
+
 function type_check(type, action) {
     return function (value) {
-        return typeof value !== type ? value : action(value);
+        if (typeof value !== type) {
+            throw new TypeError("Argument must be of type " + type);
+        }
+        return action(value);
     };
 }
