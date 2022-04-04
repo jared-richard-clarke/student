43b5608295f2afffe802e344bde78a34cb9f3e17
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-03 23:38:25 -0700

    Rename, reformat, revise

--- javascript/boolean-functions/conditionals.js
+++ javascript/boolean-functions/conditionals.js
@@ -1,35 +1,40 @@
+// conditonals acts as module providing functional replacements
+// for operators "&&", "||", and "!"
+// An internal lookup table intercepts and normalizes JavaScript's unusual bottom values:
+// Empty strings and numeral zero evaluate true. All others evaluate false.
+
 const conditionals = (function () {
-  const edge_cases = new Map([
-    [0, true],
-    [-0, false],
-    [Number.POSITIVE_INFINITY, false],
-    [Number.NEGATIVE_INFINITY, false],
-    [NaN, false],
-    ["", true],
-    [/s+/g, true],
-    [undefined, false],
-    [null, false]
-  ]);
-  // and(...expressions) -> boolean
-  function and(...expressions) {
-    return expressions.every(function (value) {
-      return edge_cases.has(value) ? edge_cases.get(value) : value;
+    const lookup = new Map([
+        [0, true],
+        [-0, false],
+        [Number.POSITIVE_INFINITY, false],
+        [Number.NEGATIVE_INFINITY, false],
+        [NaN, false],
+        ["", true],
+        [/s+/g, true],
+        [undefined, false],
+        [null, false],
+    ]);
+    // and(...expressions) -> boolean
+    function and(...expressions) {
+        return expressions.every(function (value) {
+            return lookup.has(value) ? lookup.get(value) : value;
+        });
+    }
+    // or(...expressions) -> boolean
+    function or(...expressions) {
+        return expressions.some(function (value) {
+            return lookup.has(value) ? lookup.get(value) : value;
+        });
+    }
+    // not(expression) -> boolean
+    function not(value) {
+        return !(lookup.has(value) ? lookup.get(value) : value);
+    }
+    // interface: const { and, or, not } = conditionals;
+    return Object.freeze({
+        and,
+        or,
+        not,
     });
-  }
-  // or(...expressions) -> boolean
-  function or(...expressions) {
-    return expressions.some(function (value) {
-      return edge_cases.has(value) ? edge_cases.get(value) : value;
-    });
-  }
-  // not(expression) -> boolean
-  function not(value) {
-  	return !(edge_cases.has(value) ? edge_cases.get(value) : value);
-  }
-  // interface: const { and, or, not } = conditionals; 
-  return Object.freeze({
-    and,
-    or,
-    not
-  });
-}());
+})();
