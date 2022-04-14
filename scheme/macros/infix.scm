5c23018afe604877930c64806531fe353ea6b440
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-13 17:59:15 -0700

    Create infix.scm

---
+++ scheme/macros/infix.scm
@@ -0,0 +1,28 @@
+;; === case syntax ===
+(define infix
+  (lambda (expr)
+    (let ([op (cadr expr)]
+          [a (car expr)]
+          [b (caddr expr)])
+      (case op
+        [(+)     ((lambda (x y) (+ x y)) a b)]
+        [(-)     ((lambda (x y) (- x y)) a b)]
+        [(* ⋅ ×) ((lambda (x y) (* x y)) a b)]
+        [(/ ÷)   ((lambda (x y) (/ x y)) a b)]
+        [else (error "invalid operator: " op)]))))
+
+;; === case-syntax expansion ===
+(define infix-ex
+  (lambda (expr)
+    (let ([op (cadr expr)]
+          [a (car expr)]
+          [b (caddr expr)])
+      (if (member op '(+))
+          (begin ((lambda (x y) (+ x y)) a b))
+          (if (member op '(-))
+              (begin ((lambda (x y) (- x y)) a b))
+              (if (member op '(* ⋅ ×))
+                  (begin ((lambda (x y) (* x y)) a b))
+                  (if (member op '(/ ÷))
+                      (begin ((lambda (x y) (/ x y)) a b))
+                      (begin (error "invalid operator: " op)))))))))
