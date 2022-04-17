dca6a5860028664e96223c23a863e635e43f8343
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-17 16:07:22 -0700

    Rename vector2d, add cross product

--- golang/definitions/vector2d.go
+++ golang/definitions/vector2d.go
@@ -6,50 +6,50 @@ import (
 )
 
 // A two-dimensional quantity that has direction and magnitude. Represented by coordinates X and Y.
-type Vector struct {
+type Vector2d struct {
 	X, Y float64
 }
 
 // Fulfills the Stringer interface for the fmt package.
-func (v Vector) String() string {
+func (v Vector2d) String() string {
 	x := v.X
 	y := v.Y
 	return fmt.Sprintf("(%.2f, %.2f)", x, y)
 }
 
 // Computes the distance of a 2d-vector's point from the origin.
-func (v Vector) Magnitude() float64 {
+func (v Vector2d) Magnitude() float64 {
 	x := v.X
 	y := v.Y
 	return math.Hypot(x, y)
 }
 
 // Computes the unit vector of a 2d-vector. Unit vectors have a magnitude of 1.
-func (v Vector) UnitVector() Vector {
+func (v Vector2d) UnitVector() Vector2d {
 	x := v.X
 	y := v.Y
 	m := math.Hypot(x, y)
-	return Vector{x / m, y / m}
+	return Vector2d{x / m, y / m}
 }
 
 // Scales a 2d-vector by a factor of n.
-func Scale(v Vector, n float64) Vector {
+func Scale(v Vector2d, n float64) Vector2d {
 	x := v.X
 	y := v.Y
-	return Vector{x * n, y * n}
+	return Vector2d{x * n, y * n}
 }
 
 // Computes the sum of two 2d-vectors.
-func Add(v1, v2 Vector) Vector {
+func Add(v1, v2 Vector2d) Vector2d {
 	x1 := v1.X
 	y1 := v1.Y
 	x2 := v2.X
 	y2 := v2.Y
-	return Vector{x1 + x2, y1 + y2}
+	return Vector2d{x1 + x2, y1 + y2}
 }
 
 // Computes the dot product of two 2d-vectors.
-func DotProduct(v1, v2 Vector) float64 {
+func DotProduct(v1, v2 Vector2d) float64 {
 	x1 := v1.X
 	y1 := v1.Y
 	x2 := v2.X
@@ -57,9 +57,18 @@ func DotProduct(v1, v2 Vector) float64 {
 	return x1*x2 + y1*y2
 }
 
+// Computes the cross product of two 2d-vectors.
+func CrossProduct(v1, v2 Vector2d) float64 {
+	x1 := v1.X
+	y1 := v1.Y
+	x2 := v2.X
+	y2 := v2.Y
+	return x1*y2 - y1*x2
+}
+
 // Generates functions for comparing the magnitudes of two 2d-vectors.
-func compare(op func(float64, float64) bool) func(Vector, Vector) bool {
-	return func(v1, v2 Vector) bool {
+func compare(op func(float64, float64) bool) func(Vector2d, Vector2d) bool {
+	return func(v1, v2 Vector2d) bool {
 		m1 := v1.Magnitude()
 		m2 := v2.Magnitude()
 		return op(m1, m2)
@@ -71,9 +80,10 @@ var Gt = compare(func(x, y float64) bool { return x > y })
 var Lt = compare(func(x, y float64) bool { return x < y })
 var Eq = compare(func(x, y float64) bool { return x == y })
 
-// var hypotenuse = func() func(v Vector) float64 {
-//     cache := make(map[Vector]float64)
-//     return func(v Vector) float64 {
+
+// var hypotenuse = func() func(v Vector2d) float64 {
+//     cache := make(map[Vector2d]float64)
+//     return func(v Vector2d) float64 {
 //         if n, ok := cache[v]; ok {
 //             return n
 //         }
