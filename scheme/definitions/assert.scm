401961a00530e376d7e87b606a5789548a11d615
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-18 01:59:51 -0700

    Update and rename assert.scm to assert-equal.scm

--- scheme/definitions/assert.scm
+++
@@ -1,6 +0,0 @@
-;; (assert expression value) -> boolean
-;; Asserts expression evaluates to value.
-;; (assert (list 1 2 3) '(1 2 3)) -> true
-
-(define (assert expression value)
-  (equal? expression value))
