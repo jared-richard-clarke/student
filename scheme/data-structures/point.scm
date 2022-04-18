1f20bc190364a25e6a99cc78b3fe7c3cad39d153
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-17 18:02:23 -0700

    Add path-length

--- scheme/data-structures/point.scm
+++ scheme/data-structures/point.scm
@@ -1,3 +1,10 @@
+;; (hypotenuse number number) -> number
+;; Computes the longest side of a right triangle.
+;; (hypotenuse 3 4) -> 5
+
+(define (hypotenuse x y)
+  (sqrt (+ (sqr x) (sqr y))))
+
 ; (point number number) -> pair
 ; Constructs a two-dimensional point represented as a pair.
 ; (point 1 2) -> '(1 . 2)
@@ -10,18 +17,27 @@
 ; (point-distance (point 3 0) (point 2 0)) -> 1
 
 (define point-distance
-  (let ([hypotenuse (lambda (x y)
-                      (sqrt (+ (sqr x) (sqr y))))])
-    (lambda (p1 p2)
-      (let ([x1 (car p1)]
-            [y1 (cdr p1)]
-            [x2 (car p2)]
-            [y2 (cdr p2)])
-        (hypotenuse (- x2 x1) (- y2 y1))))))
+  (lambda (p1 p2)
+    (let ([x1 (car p1)]
+          [y1 (cdr p1)]
+          [x2 (car p2)]
+          [y2 (cdr p2)])
+      (hypotenuse (- x2 x1) (- y2 y1)))))
 
 ;; (path points) -> list
 ;; Returns a list of points.
 ;; (path (point 1 2) (point 3 4)) -> '((1 . 2) (3 . 4))
 
-(define (path . points)
-  points)
+(define (path . points) points)
+
+;; (path-length path) -> number
+;; Computes the length of a path.
+;; (path (point 2 0) (point 3 0) (point 4 0))
+
+(define (path-length path)
+  (let loop ([sum 0]
+             [pth path])
+    (if (<= (length pth) 1)
+        sum
+        (loop (+ sum (distance (car pth) (cadr pth)))
+              (cdr pth)))))
