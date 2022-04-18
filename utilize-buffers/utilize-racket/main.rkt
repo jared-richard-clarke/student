48a29aff4d222898aac3d8f679988bd081668c3f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-18 15:50:48 -0700

    Create main.rkt

---
+++ utilize-buffers/utilize-racket/main.rkt
@@ -0,0 +1,34 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require "lib.rkt")
+
+;; grab first command-line argument
+(define file
+  (command-line #:program "utilize"
+                #:args (filename.txt)
+                filename.txt))
+
+;; check if file exists
+(when (not (file-exists? file))
+  (raise-user-error "file does not exist in current directory"))
+
+;; check file type
+(when (not (path-has-extension? file ".txt"))
+  (raise-user-error "argument must be plain text file <example.txt>"))
+
+;; open input and output files
+(define IN (open-input-file file))
+(define OUT (open-output-file (string-append "re-" file)
+                              #:exists 'truncate))
+
+;; process text stream line by line
+(stream IN OUT replace)
+
+;; close files
+(close-input-port IN)
+(close-output-port OUT)
+
+;; end program
+(when (and (port-closed? IN) (port-closed? OUT))
+  (println "Stream complete. Files closed."))
