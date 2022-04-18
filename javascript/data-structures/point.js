35f13b5ae41af609ecc48f9d2df5573db3c47c38
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-17 18:23:17 -0700

    Reformat

--- javascript/data-structures/point.js
+++ javascript/data-structures/point.js
@@ -3,7 +3,7 @@
 // point(1, 2) -> [1, 2]
 
 function point(x, y) {
-  return Object.freeze([x, y]);
+    return Object.freeze([x, y]);
 }
 
 // path(...point) -> array
@@ -11,7 +11,7 @@ function point(x, y) {
 // path(point(1, 2), path(3, 4)) -> [[1, 2], [3, 4]]
 
 function path(...points) {
-  return Object.freeze(points);
+    return Object.freeze(points);
 }
 
 // distance(array, array) -> number
@@ -29,15 +29,15 @@ function distance(p1, p2) {
 // path_length(path(point(1, 1), point(5, 1), point(5, 4), point(1, 1))) -> 12
 
 function path_length(path) {
-	let sum = 0;
-  // subtract 1 to prevent indexing out of bounds
-  const length = path.length - 1;
-  for (let i = 0; i < length; i += 1) {
-  	const p1 = path[i];
-    const p2 = path[i + 1];
-    sum += distance(p1, p2);
-  }
-  return sum;
+    let sum = 0;
+    // subtract 1 to prevent indexing out of bounds
+    const length = path.length - 1;
+    for (let i = 0; i < length; i += 1) {
+  	  const p1 = path[i];
+      const p2 = path[i + 1];
+      sum += distance(p1, p2);
+    }
+    return sum;
 }
 
 // hypotenuse(number, number) -> number
@@ -45,5 +45,5 @@ function path_length(path) {
 // hypotenuse(3, 4) -> 5
 
 function hypotenuse(x, y) {
-  return Math.sqrt((x * x) + (y * y));
+    return Math.sqrt((x * x) + (y * y));
 }
