94c729bace2f23ff0722e834a34589711d059014
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-18 20:57:24 -0700

    Create counter.rs

---
+++ rust/definitions/counter.rs
@@ -0,0 +1,27 @@
+use std::iter::Iterator
+
+// Method implementation of the Iterator trait.
+// Example pulled from The Rust Programming Language, Chapter 13.2
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
