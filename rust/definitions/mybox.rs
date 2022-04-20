74d394babc404b718758f50dae9eb4a0ba56c683
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-20 14:54:58 -0700

    Add "deref coercion" example

--- rust/definitions/mybox.rs
+++ rust/definitions/mybox.rs
@@ -1,12 +1,12 @@
 // Example pulled from The Rust Programming Language, chapter 15.2
-// Side Note: MyBox does not store data on the heap like Box<T>. 
+// Side Note: MyBox does not store data on the heap like Box<T>.
 // This is an an example implementation of the Deref trait.
 
 use std::ops::Deref;
 
 impl<T> Deref for MyBox<T> {
     // associated type: an alternative syntax for generic types
-    type Target = T; 
+    type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -21,10 +21,22 @@ impl<T> MyBox<T> {
     }
 }
 
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
 fn main() {
     let x = 7;
     let y = MyBox::new(x);
 
     assert_eq!(7, x);
     assert_eq!(7, *y);
+
+    // === with deref coercion ===
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    // === without deref coercion ===
+    // let m = MyBox::new(String::from("Rust"));
+    // hello(&(*m)[..]);
 }
