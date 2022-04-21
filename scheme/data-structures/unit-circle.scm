a691c07acd3fab00783502bfd7bab0c1782f4298
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-20 23:52:21 -0700

    Change values to list

--- scheme/data-structures/unit-circle.scm
+++ scheme/data-structures/unit-circle.scm
@@ -2,7 +2,7 @@
 ;; For a unit circle, returns either one of four orthogonal points or a series of three intermediate points.
 ;; Points are implemented as improper lists.
 ;; (unit-circle '90deg)    -> '(0 . 1)
-;; (unit-circle 'quad-one) -> '(0.866 . 0.5) '(0.707 . 0.707) '(0.5 . 0.866)
+;; (unit-circle 'quad-one) -> '((0.866 . 0.5) '(0.707 . 0.707) '(0.5 . 0.866))
 
 (define unit-circle
   (let ([0deg   '(1 . 0)]
@@ -24,11 +24,11 @@
     (lambda (message)
       (case message
         [(0deg) 0deg]
-        [(quad-one)   (values   30deg  45deg  60deg)]
+        [(quad-one) (list 30deg 45deg 60deg)]
         [(90deg) 90deg]
-        [(quad-two)   (values  120deg 135deg 150deg)]
+        [(quad-two) (list 120deg 135deg 150deg)]
         [(180deg) 180deg]
-        [(quad-three) (values  210deg 225deg 240deg)]
+        [(quad-three) (list 210deg 225deg 240deg)]
         [(270deg) 270deg]
-        [(quad-four)  (values  300deg 315deg 330deg)]
+        [(quad-four) (list 300deg 315deg 330deg)]
         [else (error "invalid argument: " message)]))))
