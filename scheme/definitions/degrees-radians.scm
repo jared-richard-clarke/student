cf0da55382ff22f435986b217ed070fecd3b0e97
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-21 15:06:45 -0700

    Remove library syntax

--- scheme/definitions/degrees-radians.scm
+++ scheme/definitions/degrees-radians.scm
@@ -1,21 +1,17 @@
-(library (degrees-radians)
-         (export deg->rad rad->deg)
-         (import (rnrs base (6)))
-         
-         ;; PI: approximation of π.
+;; PI: approximation of π.
 
-         (define PI 3.141592653589793)
+(define PI 3.141592653589793)
 
-         ;; (deg->rad number) -> number
-         ;; Converts degrees to radians.
-         ;; (deg->rad 7) -> 0.12217304763960307
+;; (deg->rad number) -> number
+;; Converts degrees to radians.
+;; (deg->rad 7) -> 0.12217304763960307
 
-         (define (deg->rad degrees)
-           (* degrees (/ PI 180)))
+(define (deg->rad degrees)
+  (* degrees (/ PI 180)))
 
-         ;; (rad->deg number) -> number
-         ;; Converts radians to degrees.
-         ;; (rad->deg 0.12217304763960307) -> 7
+;; (rad->deg number) -> number
+;; Converts radians to degrees.
+;; (rad->deg 0.12217304763960307) -> 7
 
-         (define (rad->deg radians)
-           (* radians (/ 180 PI))))
+(define (rad->deg radians)
+  (* radians (/ 180 PI)))
