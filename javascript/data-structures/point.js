d310853893453cf6cd768dacdd24fe3e46ac4c4f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-26 01:26:10 -0700

    Add ORIGIN, rename distance, segment

--- javascript/data-structures/point.js
+++ javascript/data-structures/point.js
@@ -6,22 +6,26 @@ function point(x, y) {
     return Object.freeze([x, y]);
 }
 
+// ORIGIN: point of origin in a two-dimensional coordinate system.
+
+const ORIGIN = point(0, 0);
+
+// segment(array, array) -> number
+// Calculates the distance between two points.
+// segment(point(3, 0), point(2, 0)) -> 1
+
+function segment(p1, p2) {
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+    return hypotenuse(x1 - x2, y1 - y2);
+}
+
 // path(...point) -> array
 // Constructs an array of two-dimensional points.
 // path(point(1, 2), path(3, 4)) -> [[1, 2], [3, 4]]
 
 function path(...points) {
     return Object.freeze(points);
-}
-
-// distance(array, array) -> number
-// Calculates the distance between two points.
-// distance(point(3, 0), point(2, 0)) -> 1
-
-function distance(p1, p2) {
-    const [x1, y1] = p1;
-    const [x2, y2] = p2;
-    return hypotenuse(x1 - x2, y1 - y2);
 }
 
 // path_length(array) -> number
@@ -35,7 +39,7 @@ function path_length(path) {
     for (let i = 0; i < length; i += 1) {
       const p1 = path[i];
       const p2 = path[i + 1];
-      sum += distance(p1, p2);
+      sum += segment(p1, p2);
     }
     return sum;
 }
