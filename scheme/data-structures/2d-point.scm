f323f5dbc89fb04cee2858a0ec6ee87329c496ba
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-26 01:15:54 -0700

    Add ORIGIN, rename point-length, segment

--- scheme/data-structures/2d-point.scm
+++ scheme/data-structures/2d-point.scm
@@ -5,38 +5,42 @@
 (define (hypotenuse x y)
   (sqrt (+ (sqr x) (sqr y))))
 
-; (point2D number number) -> pair
+; (point number number) -> pair
 ; Constructs a two-dimensional point represented as a pair.
-; (point2D 1 2) -> '(1 . 2)
+; (point 1 2) -> '(1 . 2)
 
-(define (point2D x y)
+(define (point x y)
   (cons x y))
 
-; (point2D-length pair pair) -> number
-; Calculates the distance between two points.
-; (point2D-length (point2D 3 0) (point2D 2 0)) -> 1
+;; ORIGIN: point of origin.
 
-(define (point2D-length p1 p2)
+(define ORIGIN (point 0 0))
+
+; (segment pair pair) -> number
+; Calculates the length of a line segment between two given points.
+; (segment (point 3 0) (point 2 0)) -> 1
+
+(define (segment p1 p2)
   (let ([x1 (car p1)]
         [y1 (cdr p1)]
         [x2 (car p2)]
         [y2 (cdr p2)])
     (hypotenuse (- x2 x1) (- y2 y1))))
 
-;; (path2D points) -> list
+;; (path points) -> list
 ;; Returns a list of points.
-;; (path2D (point2D 1 2) (point2D 3 4)) -> '((1 . 2) (3 . 4))
+;; (path (point 1 2) (point 3 4)) -> '((1 . 2) (3 . 4))
 
-(define (path2D . points) points)
+(define (path . points) points)
 
-;; (path2D-length path) -> number
+;; (path-length path) -> number
 ;; Computes the length of a path.
-;; (path2D-length (path2D (point2D 1 1) (point2D 5 1) (point2D 5 4) (point2D 1 1))) -> 12
+;; (path-length (path (point 1 1) (point 5 1) (point 5 4) (point 1 1))) -> 12
 
-(define (path2D-length path)
+(define (path-length path)
   (let loop ([sum 0]
              [pth path])
     (if (<= (length pth) 1)
         sum
-        (loop (+ sum (point2D-length (car pth) (cadr pth)))
+        (loop (+ sum (segment (car pth) (cadr pth)))
               (cdr pth)))))
