6c08e06d977ff7fe833a976400e1b84e47bc610b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-26 23:18:53 -0700

    Add approximate function

--- python/data-structures/vector2d.py
+++ python/data-structures/vector2d.py
@@ -1,4 +1,5 @@
 import math
+
 """
 Vector2D(number, number) -> Vector2D
 Constructs a two-dimensional vector object.
@@ -30,6 +31,7 @@ v = scale(Vector2D(3, 4), 2) ->  v.point -> (6, 8)
 def scale(v, factor):
     x, y = v.point
     return Vector2D((x * factor), (y * factor))
+
 """
 add(Vector2D, Vector2D) -> Vector2D
 Returns a two-dimensional vector that is the sum of two vectors.
@@ -39,6 +41,7 @@ def add(v1, v2):
     x1, y1 = v1.point
     x2, y2 = v2.point
     return Vector2D(x1 + x2, y1 + y2)
+
 """
 dot(Vector2D, Vector2D) -> number
 Returns a number that is the dot product of two, two-dimensional vectors.
@@ -48,6 +51,7 @@ def dot(v1, v2):
     x1, y1 = v1.point
     x2, y2 = v2.point
     return (x1 * x2) + (y1 * y2)
+
 """
 cross(Vector2D, Vector2D) -> number
 Returns a number that is the cross product of two, two-dimensional vectors.
@@ -57,6 +61,7 @@ def cross(v1, v2):
     x1, y1 = v1.point
     x2, y2 = v2.point
     return (x1 * y2) - (y1 * x2)
+
 """
 compare(function) -> function -> boolean
 Generates functions for sequentially comparing the magnitudes of a series of two-dimensional vectors.
@@ -72,3 +77,18 @@ def compare(operation):
 gt = compare(lambda x, y: x > y)
 lt = compare(lambda x, y: x < y)
 eq = compare(lambda x, y: x == y)
+
+""" 
+approximate(function) -> function(Vector2D) -> Vector2D
+Generates approximation functions for simplifying vector components.
+vec_round = approximate(round) -> vec_round(Vector2D(1.3, 1.7)) -> Vector2D.point -> (1.0, 2.0)
+"""
+def approximate(operation): 
+    def approximater(vec):
+        x, y = vec.point
+        return Vector2D(operation(x), operation(y))
+    return approximater
+
+vec_round = approximate(round)
+vec_ceiling = approximate(math.ceil)
+vec_floor = approximate(math.floor)
