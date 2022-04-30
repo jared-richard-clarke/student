0570dcea58935dcb09c9257fedb215179e1363cc
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-30 08:16:27 -0700

    Add unit tests

--- geometry/scheme/degrees-radians.scm
+++ geometry/scheme/degrees-radians.scm
@@ -11,7 +11,29 @@
 
 ;; (rad->deg number) -> number
 ;; Converts radians to degrees.
-;; (rad->deg 0.12217304763960307) -> 7
+;; (rad->deg 0.12217304763960307) -> 7.0
 
 (define (rad->deg radians)
   (* radians (/ 180 PI)))
+
+;; === testing ===
+
+;; (assert-equal expression value) -> current-output-port
+;; If expression does not evaluate to value, macro prints failed test to current-output-port.
+;; (assert-equal (vec2 4 4) '(3 . 4)) ->
+;; Test: (add 2 2)
+;; Expect: (3 . 4), Got: (4 . 4)
+
+(define-syntax assert-equal
+  (syntax-rules ()
+    [(_ expression value)
+     (when (not (equal? expression value))
+       (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
+               (quote expression)
+               value
+               expression))]))
+
+;; === unit tests ===
+
+(assert-equal (deg->rad 7) 0.12217304763960307)
+(assert-equal (rad->deg 0.12217304763960307) 7.0)
