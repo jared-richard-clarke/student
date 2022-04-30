24b36ec63eaa968b6d064a655598051a3b06119b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-04-30 08:25:22 -0700

    Add unit tests

--- geometry/scheme/2d-point.scm
+++ geometry/scheme/2d-point.scm
@@ -1,3 +1,5 @@
+;; Implementation of two-dimensional points and point operations.
+
 ;; (hypotenuse number number) -> number
 ;; Computes the longest side of a right triangle.
 ;; (hypotenuse 3 4) -> 5
@@ -58,3 +60,31 @@
 (define point-round (approximate round))
 (define point-ceiling (approximate ceiling))
 (define point-floor (approximate floor))
+
+;; === testing ===
+
+;; (assert-equal expression value) -> current-output-port
+;; If expression does not evaluate to value, macro prints failed test to current-output-port.
+;; (assert-equal (vec2 4 4) '(3 . 4)) ->
+;; Test: (add 2 2)
+;; Expect: (3 . 4), Got: (4 . 4)
+
+(define-syntax assert-equal
+  (syntax-rules ()
+    [(_ expression value)
+     (when (not (equal? expression value))
+       (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
+               (quote expression)
+               value
+               expression))]))
+
+;; === unit tests ===
+
+(assert-equal (hypotenuse 3 4) 5)
+(assert-equal (point 1 2) '(1 . 2))
+(assert-equal (segment (point 3 0) (point 2 0)) 1)
+(assert-equal (path (point 1 2) (point 3 4)) '((1 . 2) (3 . 4)))
+(assert-equal (path-length (path (point 1 1) (point 5 1) (point 5 4) (point 1 1))) 12)
+(assert-equal (point-round (point 1.3 1.7)) '(1.0 . 2.0))
+(assert-equal (point-ceiling (point 1.3 1.7)) '(2.0 . 2.0))
+(assert-equal (point-floor (point 1.3 1.7)) '(1.0 . 1.0))
