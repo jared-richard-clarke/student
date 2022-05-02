72ea4af573b8bdc43280a95fe9ad8c516b9d9df1
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-02 10:17:00 -0700

    Change docstring to comment

--- python/functional-py/pipe.py
+++ python/functional-py/pipe.py
@@ -1,9 +1,8 @@
-"""
-pipe(*functions) -> function
-Composes a series of value functions into a single value function.
-sums = pipe(lambda x: x + 1, lambda x: x * 10)
-sums(6) -> 70
-"""
+# pipe(*functions) -> function
+# Composes a series of value functions into a single value function.
+# sums = pipe(lambda x: x + 1, lambda x: x * 10)
+# sums(6) -> 70
+
 def pipe(*actions):
     def reducer(input):
         accum = input
