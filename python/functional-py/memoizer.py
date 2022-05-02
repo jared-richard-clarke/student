8f8daac92c87f56818c3ade4fa79e8247d815c6d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-02 10:16:31 -0700

    Change docstring to comment

--- python/functional-py/memoizer.py
+++ python/functional-py/memoizer.py
@@ -1,10 +1,9 @@
-"""
-memoize (function) -> function -> value
-Wraps function in a function that stores previously-computed values. Eliminates redundant computation.
-Works only with pure functions. Mutable values cannot be cached.
-const add = memoize(lambda x, y: x + y)
-add(1, 6) -> caches then returns 7
-"""
+# memoize (function) -> function -> value
+# Wraps function in a function that stores previously-computed values. Eliminates redundant computation.
+# Works only with pure functions. Mutable values cannot be cached.
+# const add = memoize(lambda x, y: x + y)
+# add(1, 6) -> caches then returns 7
+
 def memoize(func):
     cache = dict()
     def memoizer (*args):
