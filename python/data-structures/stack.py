eef9254d23a946d4ccc139a861b02313b161e44e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-02 10:15:45 -0700

    Change docstring to comment

--- python/data-structures/stack.py
+++ python/data-structures/stack.py
@@ -1,15 +1,14 @@
-"""
-Stack() -> Stack
-Wraps list in a series of methods that implement a stack.
-.push(item) appends variable number of items to top of stack.
-stack.push(1, 2, 3)
-.pop() removes top item from stack and returns it to the caller. Returns None if stack is empty.
-stack.pop() -> 3
-.peek() returns top item to caller without removing it from the stack. Returns None if stack is empty.
-stack.peek() -> 2
-.empty() If stack is empty, returns boolean True. Otherwise returns boolean False.
-stack.empty() -> False
-"""
+# Stack() -> Stack
+# Wraps list in a series of methods that implement a stack.
+# .push(item) appends variable number of items to top of stack.
+# stack.push(1, 2, 3)
+# .pop() removes top item from stack and returns it to the caller. Returns None if stack is empty.
+# stack.pop() -> 3
+# .peek() returns top item to caller without removing it from the stack. Returns None if stack is empty.
+# stack.peek() -> 2
+# .empty() If stack is empty, returns boolean True. Otherwise returns boolean False.
+# stack.empty() -> False
+
 class Stack:
     def __init__(self):
         self.store = list()
