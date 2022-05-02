fe501376a8d1f7f83fbdc1568ef2c2dcc9a81315
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-02 10:18:18 -0700

    Change docstring to comment

--- python/functional-py/type-check.py
+++ python/functional-py/type-check.py
@@ -1,17 +1,16 @@
-"""
-type_check(type, *function) -> *function
-Inputs a type and variadic function and returns 
-a variadic function that checks the types of its inputs.
+# type_check(type, *function) -> *function
+# Inputs a type and variadic function and returns 
+# a variadic function that checks the types of its inputs.
+# 
+# def add(*numbers):
+#     sum = 0
+#     for number in numbers:
+#         sum += number
+#     return sum
+# 
+# add = type_check(int, add)
+# add(1, 6) -> 7
 
-def add(*numbers):
-    sum = 0
-    for number in numbers:
-        sum += number
-    return sum
-
-add = type_check(int, add)
-add(1, 6) -> 7
-"""
 def type_check(check, action):
     def wrapper(*inputs):
         for input in inputs:
