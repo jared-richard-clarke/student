0b030e0d1fbe4fd116b0441c6a1d18af8db56a13
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-02 15:53:54 -0700

    Revise pipe, add compose_pipe

--- python/functional-py/pipe.py
+++ python/functional-py/pipe.py
@@ -1,9 +1,9 @@
-# pipe(*functions) -> function
+# compose_pipe(*functions) -> function
 # Composes a series of value functions into a single value function.
-# sums = pipe(lambda x: x + 1, lambda x: x * 10)
+# sums = compose_pipe(lambda x: x + 1, lambda x: x * 10)
 # sums(6) -> 70
 
-def pipe(*actions):
+def compose_pipe(*actions):
     def reducer(input):
         accum = input
         for action in actions:
@@ -11,3 +11,12 @@ def pipe(*actions):
         return accum
     return reducer
  
+# pipe(value, *function) -> value
+# Transforms a value through a series of single-argument functions.
+# pipe(6, lambda x: x + 1, lambda x: x * 10) -> 70
+
+def pipe(input, *actions):
+    accum = input
+    for action in actions:
+        accum = action(accum)
+    return accum
