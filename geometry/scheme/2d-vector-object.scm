9baa8ff2705348d3d1bdf34a567c0ba68a1eb9ca
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-04 15:28:49 -0700

    Replace list with vector, redefine tests

--- geometry/scheme/2d-vector-object.scm
+++ geometry/scheme/2d-vector-object.scm
@@ -3,7 +3,7 @@
 ;; (vec2 number number) -> vec2
 ;; Creates two-dimensional vector object implemented as a function. Captures values within its closure.
 ;; (define v (vec2 3 4))
-;; (v 'point) -> '(3 . 4)
+;; (v 'point) -> #(3 4)
 ;; (v 'type) -> 'vec2
 ;; (v 'magnitude) -> 5
 
@@ -11,7 +11,7 @@
   ;; === properties ===
   (let* ([px x]
          [py y]
-         [point (cons x y)]
+         [point (vector x y)]
          [type 'vec2]
          [magnitude (sqrt (+ (sqr px) (sqr py)))])
     ;; === interface ===
@@ -29,31 +29,31 @@
 
 ;; (add vec2 vec2) -> vec2
 ;; Returns a two-dimensional vector that is the sum of a series of two-dimensionsal vectors.
-;; (add (vec2 1 2) (vec2 3 4) (vec2 2 1)) -> (vec2 'point) -> '(6 . 7)
+;; (add (vec2 1 2) (vec2 3 4) (vec2 2 1)) -> (vec2 'point) -> #(6 7)
 
 (define (add . vecs)
-  (if (= (length vecs) 1)
+  (if (< (length vecs) 2)
       (car vecs)
-      ;; To prevent memory consumption, add processes vec2s as a series of pairs,
+      ;; To prevent memory consumption, add processes vec2s as a series of vectors,
       ;; converting only the sum to a vec2 function with closure. 
       (let ([sum (foldl (lambda (v1 v2)
-                          (let ([x1 (car v1)]
-                                [y1 (cdr v1)]
-                                [x2 (car v2)]
-                                [y2 (cdr v2)])
-                            (cons (+ x1 x2) (+ y1 y2))))
-                        '(0 . 0)
+                          (let ([x1 (vector-ref v1 0)]
+                                [y1 (vector-ref v1 1)]
+                                [x2 (vector-ref v2 0)]
+                                [y2 (vector-ref v2 1)])
+                            (vector (+ x1 x2) (+ y1 y2))))
+                        #(0 0)
                         (map (lambda (v) (v 'point)) vecs))])
-        (vec2 (car sum) (cdr sum)))))
+        (vec2 (vector-ref sum 0) (vector-ref sum 1)))))
 
 ;; (scale vec2 number) -> vec2
 ;; Returns a scaled two-dimensional vector that is the product of a two-dimensional vector and a number.
-;; (scale (vec2 3 4) 2) -> (vec2 'point) -> '(6 . 8)
+;; (scale (vec2 3 4) 2) -> (vec2 'point) -> #(6 8)
 
 (define (scale vec factor)
   (let* ([p (vec 'point)]
-         [x (car p)]
-         [y (cdr p)])
+         [x (vector-ref p 0)]
+         [y (vector-ref p 1)])
     (vec2 (* x factor) (* y factor))))
 
 ;; (dot vec2 vec2) -> number
@@ -63,10 +63,10 @@
 (define (dot v1 v2)
   (let* ([p1 (v1 'point)]
          [p2 (v2 'point)]
-         [x1 (car p1)]
-         [y1 (cdr p1)]
-         [x2 (car p2)]
-         [y2 (cdr p2)])
+         [x1 (vector-ref p1 0)]
+         [y1 (vector-ref p1 1)]
+         [x2 (vector-ref p2 0)]
+         [y2 (vector-ref p2 1)])
     (+ (* x1 x2) (* y1 y2))))
 
 ;; (cross vec2 vec2) -> number
@@ -76,10 +76,10 @@
 (define (cross v1 v2)
   (let* ([p1 (v1 'point)]
          [p2 (v2 'point)]
-         [x1 (car p1)]
-         [y1 (cdr p1)]
-         [x2 (car p2)]
-         [y2 (cdr p2)])
+         [x1 (vector-ref p1 0)]
+         [y1 (vector-ref p1 1)]
+         [x2 (vector-ref p2 0)]
+         [y2 (vector-ref p2 1)])
     (- (* x1 y2) (* y1 x2))))
 
 ;; (compare operator) -> function
@@ -106,8 +106,8 @@
 (define (approximate operation)
   (lambda (vec)
     (let* ([pt (vec 'point)]
-           [x (car pt)]
-           [y (cdr pt)])
+           [x (vector-ref pt 0)]
+           [y (vector-ref pt 1)])
       (vec2 (operation x) (operation y)))))
 
 (define vec-round (approximate round))
@@ -118,28 +118,48 @@
 
 ;; (assert-equal expression value) -> current-output-port
 ;; If expression does not evaluate to value, macro prints failed test to current-output-port.
-;; (assert-equal (vec2 4 4) '(3 . 4)) ->
+;; (assert-equal (vec2 4 4) #(3 4)) ->
 ;; Test: (vec2 4 4)
-;; Expect: (3 . 4), Got: (4 . 4)
+;; Expect: #(3 4), Got: #(4 4)
 
 (define-syntax assert-equal
   (syntax-rules ()
     [(_ expression value)
-     (when (not (equal? expression value))
-       (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
-               (quote expression)
-               value
-               expression))]))
+     (let ([computed-expr expression]) ;; <- prevents redundant computation
+       (when (not (equal? computed-expr value))
+         (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
+                 (quote expression) ;; <---- returns expression prior to evaluation
+                 value
+                 computed-expr)))]))
 
 ;; === unit tests ===
 
-(assert-equal ((add (vec2 1 2) (vec2 3 4) (vec2 2 1)) 'point) '(6 . 7))
-(assert-equal ((scale (vec2 3 4) 2) 'point) '(6 . 8))
-(assert-equal (dot (vec2 1 2) (vec2 3 4)) 11)
-(assert-equal (cross (vec2 1 2) (vec2 3 4)) -2)
-(assert-equal (vec-gt? (vec2 3 4) (vec2 1 2)) #t)
-(assert-equal (vec-lt? (vec2 3 4) (vec2 1 2)) #f)
-(assert-equal (vec-eq? (vec2 3 4) (vec2 3 4)) #t)
-(assert-equal ((vec-round (vec2 1.3 1.7)) 'point) '(1.0 . 2.0))
-(assert-equal ((vec-ceiling (vec2 1.3 1.7)) 'point) '(2.0 . 2.0))
-(assert-equal ((vec-floor (vec2 1.3 1.7)) 'point) '(1.0 . 1.0))
+(assert-equal ((add (vec2 1 2) (vec2 3 4) (vec2 2 1)) 'point)
+              #(6 7))
+
+(assert-equal ((scale (vec2 3 4) 2) 'point)
+              #(6 8))
+
+(assert-equal (dot (vec2 1 2) (vec2 3 4))
+              11)
+
+(assert-equal (cross (vec2 1 2) (vec2 3 4))
+              -2)
+
+(assert-equal (vec-gt? (vec2 3 4) (vec2 1 2))
+              #t)
+
+(assert-equal (vec-lt? (vec2 3 4) (vec2 1 2))
+              #f)
+
+(assert-equal (vec-eq? (vec2 3 4) (vec2 3 4))
+              #t)
+
+(assert-equal ((vec-round (vec2 1.3 1.7)) 'point)
+              #(1.0 2.0))
+
+(assert-equal ((vec-ceiling (vec2 1.3 1.7)) 'point)
+              #(2.0 2.0))
+
+(assert-equal ((vec-floor (vec2 1.3 1.7)) 'point)
+              #(1.0 1.0))
