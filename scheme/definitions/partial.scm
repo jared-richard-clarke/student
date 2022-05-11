ffd5af86497f11bb575dcf00f2f0a513f5e11a09
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-11 16:51:12 -0700

    Fix typo

--- scheme/definitions/partial.scm
+++ scheme/definitions/partial.scm
@@ -1,6 +1,6 @@
 ;; (partial function values ...) -> (function values ...) -> value
 ;; Fixes a number of arguments to a function, producing another function with a smaller arity.
-;; (define shout (partial string-append "!")) -> (shout "doggo") -> "Doggo!"
+;; (define shout (partial string-append "!")) -> (shout "doggo") -> "doggo!"
 
 (define (partial fn . x)
   (lambda y
