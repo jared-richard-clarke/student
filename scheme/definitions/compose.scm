94e9955f534d03520e0bb0ce3c06e314ad0e5a00
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-12 14:52:59 -0700

    Revise documentation

--- scheme/definitions/compose.scm
+++ scheme/definitions/compose.scm
@@ -1,5 +1,6 @@
 ;; (pipe value function ...) -> value
 ;; Transforms a value by passing it through a series of single-argument functions. 
+;; The first function can take any number of arguments. All proceeding functions must take one.
 ;; (pipe "dog" string-plural string-upcase) -> "DOGS"
 
 (define (pipe arg . functions)
@@ -9,7 +10,8 @@
          functions))
 
 ;; (comp function ...) -> (function value) -> value
-;; Composes a series of single-argument functions into a single function expression.
+;; Composes a series of functions into a single function expression.
+;; The first function can take any number of arguments. All proceeding functions must take one.
 ;; (define edit (comp string-plural string-upcase)) -> (edit "dog") -> "DOGS"
 
 (define (comp . functions)
