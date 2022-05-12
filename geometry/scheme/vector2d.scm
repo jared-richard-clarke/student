f06eb82f5eb12ceaf383c80c1e33c5fdd5d0e86b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-11 17:10:46 -0700

    Rename negate, flip

--- geometry/scheme/vector2d.scm
+++ geometry/scheme/vector2d.scm
@@ -20,11 +20,11 @@
 (define I-HAT (vec2 1 0))
 (define J-HAT (vec2 0 1))
 
-;; (negate vec2) -> vec2
+;; (flip vec2) -> vec2
 ;; Inverts the signs of the vector components.
-;; (negate (vec2 3 4)) -> (vec2 -3 -4)
+;; (flip (vec2 3 4)) -> (vec2 -3 -4)
 
-(define (negate vec)
+(define (flip vec)
   (let ([x (vector-ref vec 0)]
         [y (vector-ref vec 1)])
     (vec2 (* x -1) (* y -1))))
@@ -142,7 +142,7 @@
 (assert-equal (vec2 3 4)
               #(3 4))
 
-(assert-equal (negate (vec2 -3 -4))
+(assert-equal (flip (vec2 -3 -4))
               #(3 4))
 
 (assert-equal (add (vec2 1 2) (vec2 1 2))
