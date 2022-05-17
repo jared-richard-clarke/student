cb21683ac812d728346a37d432bbd8a0fbde4c76
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-17 14:39:04 -0700

    Convert functions to methods
    Convert functions to methods on Vector2D class to make code more idiomatic to Python

--- geometry/python/vectors.py
+++ geometry/python/vectors.py
@@ -14,73 +14,72 @@ class Vector2D:
         self.y = y
         self.point = (x, y)
         self.magnitude = math.hypot(x, y)
+        
+    # Vector2D.scale(number) -> Vector2D
+    # Returns a scaled two-dimensional vector that is the product of a vector and a number.
+    # Vector2D(3, 4).scale(2).point -> (6, 8)
+    
+    def scale(self, scalar):
+        return Vector2D(self.x * scalar, self.y * scalar)
+    
+    # Vector2D.add(Vector2D) -> Vector2D
+    # Returns a two-dimensional vector that is the sum of two vectors.
+    # Vector2D(1, 2).add(Vector2D(3, 4)).point -> (4, 6)
+    
+    def add(self, vec):
+        x1, y1 = self.point
+        x2, y2 = vec.point
+        return Vector2D(x1 + x2, y1 + y2)
+    
+    # Vector2D.dot(Vector2D) -> number
+    # Returns a number that is the dot product of two, two-dimensional vectors.
+    # Vector2D(1, 2).dot(Vector2D(3, 4)) -> 11
+    
+    def dot(self, vec):
+        x1, y1 = self.point
+        x2, y2 = vec.point
+        return (x1 * x2) + (y1 * y2)
+    
+    # Vector2D.cross(Vector2D) -> number
+    # Returns a number that is the cross product of two, two-dimensional vectors.
+    # Vector2D(1, 2).cross(Vector2D(3, 4)) -> -2
+    
+    def cross(self, vec):
+        x1, y1 = self.point
+        x2, y2 = vec.point
+        return (x1 * y2) - (y1 * x2)
+    
+    # Vector2D.round() -> Vector2D
+    # Returns a two-dimensional vector with coordinate components rounded.
+    # Vector2D(3.2, 4.7).round().point -> (3.0, 5.0)
+    
+    def round(self):
+        return Vector2D(round(self.x), round(self.y))
+    
+    # Vector2D.eq(Vector2D) -> boolean
+    # Checks whether the magnitudes of two vectors are equal.
+    # Vector2D(3, 4).eq(Vector2D(3, 4)) -> True
+    
+    def eq(self, vec):
+        return self.magnitude == vec.magnitude
+    
+    # Vector2D.gt(Vector2D) -> boolean
+    # Checks whether the magnitude of vector one is greater than the magnitude of vector two.
+    # Vector2D(3, 4).gt(Vector2D(1, 2)) -> True
+    
+    def gt(self, vec):
+        return self.magnitude > vec.magnitude
+    
+    # Vector2D.lt(Vector2D) -> boolean
+    # Checks whether the magnitude of vector one is less than the magnitude of vector two.
+    # Vector2D(1, 2).lt(Vector2D(3, 4)) -> True
+    
+    def lt(self, vec):
+        return self.magnitude < vec.magnitude
 
 # IHAT, JHAT
 # Mutually orthogonal two-dimensional unit vectors, forming the standard basis.
 
 IHAT = Vector2D(1, 0)
 JHAT = Vector2D(0, 1)
-    
-# scale(Vector2D, number) -> Vector2D
-# Returns a scaled two-dimensional vector that is the product of a vector and a number.
-# v = scale(Vector2D(3, 4), 2) ->  v.point -> (6, 8)
 
-def scale(v, scalar):
-    x, y = v.point
-    return Vector2D((x * scalar), (y * scalar))
-
-# add(Vector2D, Vector2D) -> Vector2D
-# Returns a two-dimensional vector that is the sum of two vectors.
-# v = add(Vector2D(1, 2), Vector2D(3, 4)) -> v.point -> (4, 6)
-
-def add(v1, v2):
-    x1, y1 = v1.point
-    x2, y2 = v2.point
-    return Vector2D(x1 + x2, y1 + y2)
-
-# dot(Vector2D, Vector2D) -> number
-# Returns a number that is the dot product of two, two-dimensional vectors.
-# dot(Vector2D(1, 2), Vector2D(3, 4)) -> 11
-
-def dot(v1, v2):
-    x1, y1 = v1.point
-    x2, y2 = v2.point
-    return (x1 * x2) + (y1 * y2)
-
-# cross(Vector2D, Vector2D) -> number
-# Returns a number that is the cross product of two, two-dimensional vectors.
-# cross(Vector2D(1, 2), Vector2D(3, 4)) -> -2
-
-def cross(v1, v2):
-    x1, y1 = v1.point
-    x2, y2 = v2.point
-    return (x1 * y2) - (y1 * x2)
-
-# compare(function) -> function -> boolean
-# Generates functions for sequentially comparing the magnitudes of a series of two-dimensional vectors.
-# eq = compare(lambda x, y: x == y) -> eq(Vector2D(3, 4), Vector2D(3, 4)) -> True
-
-def compare(operation):
-    def operator(v1, v2):
-        m1 = v1.magnitude
-        m2 = v2.magnitude
-        return operation(m1, m2)
-    return operator
-
-gt = compare(lambda x, y: x > y)
-lt = compare(lambda x, y: x < y)
-eq = compare(lambda x, y: x == y)
-
-# approximate(function) -> function(Vector2D) -> Vector2D
-# Generates approximation functions for simplifying vector components.
-# vec_round = approximate(round) -> vec_round(Vector2D(1.3, 1.7)) -> Vector2D.point -> (1.0, 2.0)
-
-def approximate(operation): 
-    def approximater(vec):
-        x, y = vec.point
-        return Vector2D(operation(x), operation(y))
-    return approximater
-
-vec_round = approximate(round)
-vec_ceiling = approximate(math.ceil)
-vec_floor = approximate(math.floor)
