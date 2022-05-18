1cb6fdaed28ae4f92bddef558035453c8819823c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-18 11:02:48 -0700

    Implement String

--- geometry/golang/angles/degrees-radians.go
+++ geometry/golang/angles/degrees-radians.go
@@ -1,11 +1,20 @@
 package angles
 
 import (
+	"fmt"
 	"math"
 )
 
 type Degrees float64
 type Radians float64
+
+func (d Degrees) String() string {
+	return fmt.Sprintf("%f°", d)
+}
+
+func (r Radians) String() string {
+	return fmt.Sprintf("%frad", r)
+}
 
 func DegreesToRadians(d Degrees) Radians {
 	return Radians(d * (math.Pi / 180.0))
