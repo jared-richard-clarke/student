ec6aa5c4c965825c63e1fc7102d9a419ceabf8b2
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-20 12:32:00 -0700

    Fix typo, revise documentation

--- geometry/scheme/vec2.scm
+++ geometry/scheme/vec2.scm
@@ -16,9 +16,11 @@
 ;; Tests for approximate equality between two floating-point numbers within an absolute
 ;; or relative tolerance of EPSILON. An absolute tolerance is used for values
 ;; less than or equal to 1.0. A relative tolerance is used for larger values.
+;; (approx-eq? 0.2 0.19999999) -> #t
 
 (define (approx-eq? x y)
-  (<= (abs (- x y)) EPSILON * (max 1.0 (abs x) (abs y))))
+  (<= (abs (- x y))
+      (* EPSILON (max 1.0 (abs x) (abs y)))))
 
 ;; (vec2 number number) -> vector
 ;; Returns two-dimensional coordinates as a number vector.
