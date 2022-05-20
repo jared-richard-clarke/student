d46f745010d0d28b2ff79cb9e6ff0af06cb87cea
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-20 13:23:26 -0700

    Create approx-eq.py

---
+++ geometry/python/utils/approx-eq.py
@@ -0,0 +1,13 @@
+# EPSILON
+# The maximum allowable difference in precision between floating-point numbers.
+
+EPSILON = 0.000001
+
+# approx_eq(number, number) -> boolean
+# Tests for approximate equality between two floating-point numbers within an absolute
+# or relative tolerance of EPSILON. An absolute tolerance is used for values
+# less than or equal to 1.0. A relative tolerance is used for larger values.
+# approx_eq(0.2, 0.19999999) -> True
+
+def approx_eq(x, y):
+    return abs(x - y) <= EPSILON * max(1.0, abs(x), abs(y))
