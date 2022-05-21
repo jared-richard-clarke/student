09d1e1e7d14e136585198bea981b933ac5374c29
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-21 00:54:20 -0700

    Create harden.js

---
+++ javascript/functional-js/harden.js
@@ -0,0 +1,12 @@
+// harden(Object) -> Object: immutable, null prototype
+// Takes an object literal and returns a new object with
+// the same key-value pairs but is immutable and inherits null.
+
+function harden(obj) {
+    return Object.freeze(
+        Object.entries(obj).reduce((accum, [key, value]) => {
+            accum[key] = value;
+            return accum;
+        }, Object.create(null))
+    );
+}
