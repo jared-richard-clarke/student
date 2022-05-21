b6aceb4393b36026bfcdf363c37d23657a3f931c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-21 00:52:03 -0700

    Make export object's prototype, null

--- javascript/functional-js/op.js
+++ javascript/functional-js/op.js
@@ -1,20 +1,21 @@
 // module op: provides a set of variadic arithmetic functions.
+// op.add(1, 2, 4) -> 7
 
 function operator(operation, base) {
     return function (...numbers) {
         if (numbers.length === 0) {
             return base;
         } else {
-            return numbers.reduce(
-                (accum, number) => operation(accum, number)
-            );
+            return numbers.reduce((accum, number) => operation(accum, number));
         }
     };
 }
 
-export default Object.freeze({
-    add: operator((x, y) => x + y, 0),
-    sub: operator((x, y) => x - y, 0),
-    mul: operator((x, y) => x * y, 1),
-    div: operator((x, y) => x / y, 1),
-});
+const op = Object.create(null);
+
+op.add = operator((x, y) => x + y, 0);
+op.sub = operator((x, y) => x - y, 0)
+op.mul = operator((x, y) => x * y, 1);
+op.div = operator((x, y) => x / y, 1);
+
+export default Object.freeze(op);
