3a86b29bed88c856c2d6b62066a56d629f3f059f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-05-30 16:47:06 -0700

    Reorder functions

--- geometry/python/vec2/func.py
+++ geometry/python/vec2/func.py
@@ -18,39 +18,6 @@ def vec2(x, y):
 IHAT = vec2(1, 0)
 JHAT = vec2(0, 1)
 
-# flip(tuple(number, number)) -> tuple(number, number)
-# Inverts the signs of the vector components. Flips the vector 180 degrees.
-# flip(vec2(3, 4)) -> vec2(-3, -4)
-
-def flip(vec):
-    x, y = vec
-    return vec2(-1 * x, -1 * y)
-
-# magnitude(tuple(number, number)) -> number
-# Computes the magnitude of a two-dimensional vector.
-# magnitude(vec2(3, 4)) -> 5.0
-
-def magnitude(vec):
-    x, y = vec
-    return math.hypot(x, y)
-
-# distance(tuple(number, number), tuple(number, number)) -> number
-# Returns the distance between two, two-dimensional vectors.
-# distance(vec2(8, 0), vec2(1, 0)) -> 7.0
-
-def distance(v1, v2):
-    x1, y1 = v1
-    x2, y2 = v2
-    return math.hypot(x2 - x1, y2 - y1)
-
-# scale(tuple(number, number), number) -> tuple(number, number)
-# Returns a scaled two-dimensional vector that is the product of a vector and a number.
-# scale(vec2(3, 4), 2) -> (6, 8)
-
-def scale(vec, scalar):
-    x, y = vec
-    return vec2(x * scalar, y * scalar)
-
 # add(*tuple(number, number)) -> tuple(number, number)
 # Returns a two-dimensional vector that is the sum of a series of two-dimensional vectors.
 # add(vec2(1, 2), vec2(3, 4), vec2(0, 2)) -> (4, 8)
@@ -62,6 +29,22 @@ def add(*vecs):
         x2, y2 = vec
         sum = (x1 + x2, y1 + y2)
     return sum
+
+# flip(tuple(number, number)) -> tuple(number, number)
+# Inverts the signs of the vector components. Flips the vector 180 degrees.
+# flip(vec2(3, 4)) -> vec2(-3, -4)
+
+def flip(vec):
+    x, y = vec
+    return vec2(-1 * x, -1 * y)
+
+# scale(tuple(number, number), number) -> tuple(number, number)
+# Returns a scaled two-dimensional vector that is the product of a vector and a number.
+# scale(vec2(3, 4), 2) -> (6, 8)
+
+def scale(vec, scalar):
+    x, y = vec
+    return vec2(x * scalar, y * scalar)
 
 # dot(tuple(number, number), tuple(number, number)) -> number
 # Returns the dot product of two, two-dimensional vectors
@@ -80,6 +63,23 @@ def normalize(vec):
     mag = magnitude(vec)
     x, y = vec
     return vec2(x / mag, y / mag)
+
+# magnitude(tuple(number, number)) -> number
+# Computes the magnitude of a two-dimensional vector.
+# magnitude(vec2(3, 4)) -> 5.0
+
+def magnitude(vec):
+    x, y = vec
+    return math.hypot(x, y)
+
+# distance(tuple(number, number), tuple(number, number)) -> number
+# Returns the distance between two, two-dimensional vectors.
+# distance(vec2(8, 0), vec2(1, 0)) -> 7.0
+
+def distance(v1, v2):
+    x1, y1 = v1
+    x2, y2 = v2
+    return math.hypot(x2 - x1, y2 - y1)
 
 # equal(tuple(number, number), tuple(number, number)) -> boolean
 # Compares the components of two, two-dimensional vectors. Checks for equality.
