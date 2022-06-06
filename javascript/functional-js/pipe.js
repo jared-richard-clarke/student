40a6925957ba2a7749a4a7e5c43ba66bcace2a98
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-06-05 22:33:47 -0700

    Rename compose_pipe, compose

--- javascript/functional-js/pipe.js
+++ javascript/functional-js/pipe.js
@@ -9,11 +9,11 @@ function pipe(input, ...actions) {
     );
 }
 
-// compose_pipe(...function) -> function -> value
+// compose(...function) -> function -> value
 // Composes a series of single-argument functions into a single function expression.
-// const edit = compose_pipe((text) => text.toLowerCase(), (text) => text.trim()) -> edit("  hEllO, worLd   ") -> "hello, world"
+// const edit = compose((text) => text.toLowerCase(), (text) => text.trim()) -> edit("  hEllO, worLd   ") -> "hello, world"
 
-function compose_pipe(...actions) {
+function compose(...actions) {
     return function (input) {
         return actions.reduce(
             (accum, action) => action(accum), 
