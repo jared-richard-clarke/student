9899f0ae576d120a685cdff96641fa172932e256
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-06-05 22:31:15 -0700

    Implement Sub trait, create vec2 function

--- geometry/rust/vectors/src/lib.rs
+++ geometry/rust/vectors/src/lib.rs
@@ -1,14 +1,11 @@
 use std::iter::Sum;
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 #[derive(Clone, Copy, PartialEq, Debug, Default)]
 struct Vector2D {
     x: f64,
     y: f64,
 }
-
-const IHAT: Vector2D = Vector2D { x: 1.0, y: 0.0 };
-const JHAT: Vector2D = Vector2D { x: 0.0, y: 1.0 };
 
 fn vec2(x: f64, y: f64) -> Vector2D {
     Vector2D { x, y }
@@ -19,17 +16,6 @@ impl Vector2D {
         let x = self.x;
         let y = self.y;
         (x * x + y * y).sqrt()
-    }
-
-    fn add(self, other: Self) -> Self {
-        let x1 = self.x;
-        let y1 = self.y;
-        let x2 = other.x;
-        let y2 = other.y;
-        Self {
-            x: x1 + x2,
-            y: y1 + y2,
-        }
     }
 
     fn scale(self, scalar: f64) -> Self {
@@ -49,7 +35,7 @@ impl Vector2D {
         x1 * x2 + y1 * y2
     }
 
-    fn flip(self) -> Self {
+    fn negate(self) -> Self {
         Self {
             x: -self.x,
             y: -self.y,
@@ -72,6 +58,16 @@ impl Add for Vector2D {
         Self {
             x: self.x + other.x,
             y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Vector2D {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
         }
     }
 }
@@ -114,13 +110,7 @@ mod tests {
         let result = v.magnitude();
         assert_eq!(result, 5.0);
     }
-    #[test]
-    fn test_add() {
-        let v1 = Vector2D { x: 1.0, y: 2.0 };
-        let v2 = Vector2D { x: 3.0, y: 4.0 };
-        let result = v1.add(v2);
-        assert_eq!(result, Vector2D { x: 4.0, y: 6.0 });
-    }
+
     #[test]
     fn test_scale() {
         let v = Vector2D { x: 3.0, y: 4.0 };
@@ -135,8 +125,8 @@ mod tests {
         assert_eq!(result, 11.0);
     }
     #[test]
-    fn test_flip() {
-        let result = Vector2D { x: -3.0, y: 4.0 }.flip();
+    fn test_negate() {
+        let result = Vector2D { x: -3.0, y: 4.0 }.negate();
         let expect = Vector2D { x: 3.0, y: -4.0 };
         assert_eq!(result, expect);
     }
@@ -144,6 +134,18 @@ mod tests {
     fn test_round() {
         let result = Vector2D { x: 0.25, y: 6.73 }.round();
         let expect = Vector2D { x: 0.0, y: 7.0 };
+        assert_eq!(result, expect);
+    }
+    #[test]
+    fn test_add() {
+        let expect = Vector2D { x: 4.0, y: 6.0 };
+        let result = Vector2D { x: 3.0, y: 4.0 } + Vector2D { x: 1.0, y: 2.0 };
+        assert_eq!(result, expect);
+    }
+    #[test]
+    fn test_sub() {
+        let expect = vec2(2.0, 2.0);
+        let result = vec2(3.0, 4.0) - vec2(1.0, 2.0);
         assert_eq!(result, expect);
     }
     #[test]
@@ -156,10 +158,5 @@ mod tests {
         let result: Vector2D = vs.iter().sum();
         assert_eq!(result, Vector2D { x: 6.0, y: 8.0 });
     }
-    #[test]
-    fn test_add_overload() {
-        let expect = Vector2D { x: 4.0, y: 6.0 };
-        let result = Vector2D { x: 3.0, y: 4.0 } + Vector2D { x: 1.0, y: 2.0 };
-        assert_eq!(result, expect);
-    }
 }
+
