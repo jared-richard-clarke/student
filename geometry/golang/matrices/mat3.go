db67daba310eb9e52e48aafb1e80247d1bec7bfc
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-06-22 01:05:29 -0700

    Add Transform method

--- geometry/golang/matrices/mat3.go
+++ geometry/golang/matrices/mat3.go
@@ -8,7 +8,49 @@ type Mat3 struct {
 	X0, Y0 float64
 }
 
-func (a Mat3) multiply(b Mat3) Mat3 {
+func Identity() Mat3 {
+	return Mat3{
+		1, 0,
+		0, 1,
+		0, 0,
+	}
+}
+
+func Translate(x, y float64) Mat3 {
+	return Mat3{
+		1, 0,
+		0, 1,
+		x, y,
+	}
+}
+
+func Scale(x, y float64) Mat3 {
+	return Mat3{
+		x, 0,
+		0, y,
+		0, 0,
+	}
+}
+
+func Rotate(angle float64) Mat3 {
+	c := math.Cos(angle)
+	s := math.Sin(angle)
+	return Mat3{
+		c, s,
+		-s, c,
+		0, 0,
+	}
+}
+
+func Shear(x, y float64) Mat3 {
+	return Mat3{
+		1, y,
+		x, 1,
+		0, 0,
+	}
+}
+
+func Multiply(a, b Mat3) Mat3 {
 	return Mat3{
 		a.XX*b.XX + a.YX*b.XY,
 		a.XX*b.YX + a.YX*b.YY,
@@ -19,60 +61,10 @@ func (a Mat3) multiply(b Mat3) Mat3 {
 	}
 }
 
-func Identity() Mat3 {
-	return Mat3{
-		1, 0,
-		0, 1,
-		0, 0,
+func (m Mat3) Transform(mats ...Mat3) Mat3 {
+	accum := m
+	for _, mat := range mats {
+		accum = Multiply(accum, mat)
 	}
-}
-
-func translate(x, y float64) Mat3 {
-	return Mat3{
-		1, 0,
-		0, 1,
-		x, y,
-	}
-}
-
-func scale(x, y float64) Mat3 {
-	return Mat3{
-		x, 0,
-		0, y,
-		0, 0,
-	}
-}
-
-func rotate(angle float64) Mat3 {
-	c := math.Cos(angle)
-	s := math.Sin(angle)
-	return Mat3{
-		c, s,
-		-s, c,
-		0, 0,
-	}
-}
-
-func shear(x, y float64) Mat3 {
-	return Mat3{
-		1, y,
-		x, 1,
-		0, 0,
-	}
-}
-
-func (a Mat3) Translate(x, y float64) Mat3 {
-	return translate(x, y).multiply(a)
-}
-
-func (a Mat3) Scale(x, y float64) Mat3 {
-	return scale(x, y).multiply(a)
-}
-
-func (a Mat3) Rotate(angle float64) Mat3 {
-	return rotate(angle).multiply(a)
-}
-
-func (a Mat3) Shear(x, y float64) Mat3 {
-	return shear(x, y).multiply(a)
+	return accum
 }
