c0811f64ec744a78af6f8227a943d6631fd29eb1
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-06-23 13:20:57 -0700

    Add variable transform function

--- geometry/scheme/mat3.scm
+++ geometry/scheme/mat3.scm
@@ -3,7 +3,7 @@
           xy yy
           x0 y0))
 
-(define (multiply a b)
+(define (m3-multiply a b)
   (let ([a-xx (vector-ref a 0)]
         [a-yx (vector-ref a 1)]
         [a-xy (vector-ref a 2)]
@@ -25,22 +25,22 @@
           [+ (* a-x0 b-xx) (* a-y0 b-xy) b-x0]
           [+ (* a-x0 b-yx) (* a-y0 b-yy) b-y0])))
 
-(define (mat3-identity)
+(define (m3-identity)
   (mat3 1 0
         0 1
         0 0))
 
-(define (translate x y)
+(define (m3-translate x y)
   (mat3 1 0
         0 1
         x y))
 
-(define (scale x y)
+(define (m3-scale x y)
   (mat3 x 0
         0 y
         0 0))
 
-(define (rotate angle)
+(define (m3-rotate angle)
   (let* ([c (cos angle)]
          [s (sin angle)]
          [-s (- s)])
@@ -48,19 +48,16 @@
           -s c
           0  0)))
 
-(define (shear x y)
+(define (m3-shear x y)
   (mat3 1 y
         x 1
         0 0))
 
-(define (mat3-translate mat x y)
-  (multiply (translate x y) mat))
-
-(define (mat3-scale mat x y)
-  (multiply (scale x y) mat))
-
-(define (mat3-rotate mat angle)
-  (multiply (rotate angle) mat))
-
-(define (mat3-shear mat x y)
-  (multiply (shear x y) mat))
+(define (m3-transform . matrices)
+  (let ([len (length matrices)])
+    (cond
+      [(= len 0) (m3-identity)]
+      [(= len 1) (car matrices)]
+      [else (foldl m3-multiply
+                   (car matrices)
+                   (cdr matrices))])))
