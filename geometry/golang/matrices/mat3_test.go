c58cf232148ef5dc69b687f2a5481c5333a70a34
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-06-26 18:00:53 -0700

    Create mat3_test.go

---
+++ geometry/golang/matrices/mat3_test.go
@@ -0,0 +1,45 @@
+package matrices
+
+import (
+	"testing"
+)
+
+func TestIdentity(t *testing.T) {
+	expect := Mat3{1.0, 0.0, 0.0, 1.0, 0.0, 0.0}
+	result := Identity()
+	if expect != result {
+		t.Errorf("Test Identity failed. Expected: %v, Got: %v", expect, result)
+	}
+}
+
+func TestTranslate(t *testing.T) {
+	expect := Mat3{1.0, 0.0, 0.0, 1.0, 3.0, 4.0}
+	result := Identity().Translate(3.0, 4.0)
+	if expect != result {
+		t.Errorf("Test Translate failed. Expected: %v, Got: %v", expect, result)
+	}
+}
+
+func TestScale(t *testing.T) {
+	expect := Mat3{3.0, 0.0, 0.0, 4.0, 0.0, 0.0}
+	result := Identity().Scale(3.0, 4.0)
+	if expect != result {
+		t.Errorf("Test Scale failed. Expected: %v, Got: %v", expect, result)
+	}
+}
+
+func TestRotate(t *testing.T) {
+	expect := Mat3{0.2836621854632263, -0.9589242746631385, 0.9589242746631385, 0.2836621854632263, 0.0, 0.0}
+	result := Identity().Rotate(5.0)
+	if expect != result {
+		t.Errorf("Test Rotate failed. Expected: %v, Got: %v", expect, result)
+	}
+}
+
+func TestShear(t *testing.T) {
+	expect := Mat3{1.0, 4.0, 3.0, 1.0, 0.0, 0.0}
+	result := Identity().Shear(3.0, 4.0)
+	if expect != result {
+		t.Errorf("Test Shear failed. Expected: %v, Got: %v", expect, result)
+	}
+}
