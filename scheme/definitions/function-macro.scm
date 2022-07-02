6faa7dabb96133def9145582f8625b7bcfd45ef7
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-01 22:53:17 -0700

    Revise documentation

--- scheme/definitions/function-macro.scm
+++ scheme/definitions/function-macro.scm
@@ -9,7 +9,7 @@
 
 ;; ᓴᓐᓇ translates to Sedna. She is the Inuit goddess of the sea and marine mammals.
 ;; A function/procedure in Scheme is a an identifier, a list of formals, and a body of one or more expressions.
-;; The identifier for functions in Scheme is "lambda" in reference to the lambda calculus; however, the 
+;; The identifier for a Scheme function is "lambda" in reference to the lambda calculus; however, the 
 ;; keyword could have been any symbol.
 
 (define-syntax ᓴᓐᓇ
