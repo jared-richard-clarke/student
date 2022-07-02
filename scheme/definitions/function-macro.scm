67928d755d48c2a3f08995990b27b2052ae644a6
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-01 22:52:20 -0700

    Add Sedna definition

--- scheme/definitions/function-macro.scm
+++ scheme/definitions/function-macro.scm
@@ -6,3 +6,13 @@
   (syntax-rules ()
     ((_ formals expression ...)
      (lambda formals expression ...))))
+
+;; ᓴᓐᓇ translates to Sedna. She is the Inuit goddess of the sea and marine mammals.
+;; A function/procedure in Scheme is a an identifier, a list of formals, and a body of one or more expressions.
+;; The identifier for functions in Scheme is "lambda" in reference to the lambda calculus; however, the 
+;; keyword could have been any symbol.
+
+(define-syntax ᓴᓐᓇ
+  (syntax-rules ()
+    [(_ formals expression ...)
+     (lambda formals expression ...)]))
