2ec728d40769e0dc0e76274576db973074322519
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-04 12:57:53 -0700

    Add unit tests

--- geometry/scheme/mat3.scm
+++ geometry/scheme/mat3.scm
@@ -61,3 +61,41 @@
       [else (foldl m3-multiply
                    (car matrices)
                    (cdr matrices))])))
+
+;; === testing ===
+
+;; (assert-equal expression value) -> current-output-port
+;; If expression does not evaluate to value, macro prints failed test to current-output-port.
+;; (assert-equal (vec2 4 4) #(3 4)) ->
+;; Test: (vec2 4 4)
+;; Expect: #(3 4), Got: #(4 4)
+
+(define-syntax assert-equal
+  (syntax-rules ()
+    [(_ expression value)
+     (let ([computed-expr expression]) ;; <- prevents redundant computation
+       (when (not (equal? computed-expr value))
+         (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
+                 (quote expression) ;; <---- returns expression prior to evaluation
+                 value
+                 computed-expr)))]))
+
+;; === unit tests ===
+
+(assert-equal (m3-identity)
+              (mat3 1 0 0 1 0 0))
+
+(assert-equal (m3-multiply (m3-identity) (m3-translate 3 4))
+              (mat3 1 0 0 1 3 4))
+
+(assert-equal (m3-multiply (m3-identity) (m3-scale 2 2))
+              (mat3 2 0 0 2 0 0))
+
+(assert-equal (m3-multiply (m3-identity) (m3-rotate 5))
+              (mat3 0.2836621854632263 -0.9589242746631385 0.9589242746631385 0.2836621854632263 0 0))
+
+(assert-equal (m3-multiply (m3-identity) (m3-shear 3 4))
+              (mat3 1 4 3 1 0 0))
+
+(assert-equal (m3-transform (m3-translate 3 4) (m3-scale 2 2) (m3-shear 1 2))
+              (mat3 2 4 2 2 3 4))
