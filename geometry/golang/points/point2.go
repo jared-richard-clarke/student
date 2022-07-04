b27e54f25e67baf23b334137d5319bbd9b82db95
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-04 10:25:29 -0700

    Fix typos

--- geometry/golang/points/point2.go
+++ geometry/golang/points/point2.go
@@ -12,7 +12,7 @@ func (p1 Point2) Distance(p2 Point2) float64 {
 	y1 := p1.Y
 	x2 := p2.X
 	y2 := p2.Y
-	return math.Hypot(x1-x2, y1-y2)
+	return math.Hypot(x2-x1, y2-y1)
 }
 
 // Interpolates a point between two points.
