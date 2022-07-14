d648c31540217a5ea54334419cb036ea5faf7af7
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-13 21:22:46 -0700

    Add example of error.

--- javascript/functional-js/type-check.js
+++ javascript/functional-js/type-check.js
@@ -1,11 +1,12 @@
 // type_check(string, function) -> function -> value or type error
 // Wraps single-argument function in a function that checks the type of its argument.
 // const add1 = type_check("number", (value) => value + 1) -> add1(10) -> 11
+// add1("text") -> TypeError: "argument must be of type number"
 
 function type_check(type, action) {
     return function (value) {
         if (typeof value !== type) {
-            throw new TypeError("Argument must be of type " + type);
+            throw new TypeError("argument must be of type " + type);
         }
         return action(value);
     };
