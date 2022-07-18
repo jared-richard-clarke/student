37d00b5f6080c4c47841717721e9ff9a8124f61a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-18 13:20:04 -0700

    Add aliases for compose

--- scheme/definitions/compose.scm
+++ scheme/definitions/compose.scm
@@ -1,4 +1,4 @@
-;; (pipe value function ...) -> value
+;; (pipe value function ...) -> any
 ;; Transforms a value by passing it through a series of single-argument functions. 
 ;; The first function can take any number of arguments. All proceeding functions must take one.
 ;; (pipe "dog" string-plural string-upcase) -> "DOGS"
@@ -9,7 +9,7 @@
          arg
          functions))
 
-;; (compose function ...) -> (function value) -> value
+;; (compose function ...) -> (function any) -> any
 ;; Composes a series of functions into a single function expression.
 ;; The first function can take any number of arguments. All proceeding functions must take one.
 ;; (define edit (compose string-plural string-upcase)) -> (edit "dog") -> "DOGS"
@@ -20,3 +20,16 @@
              (function value))
            arg
            functions)))
+
+;; (>> functions ...) -> (function any) -> any
+;; Alias for compose. Named after the composition operator in OCaml.
+
+(define (>> . functions)
+  (apply compose functions))
+
+;; (<< functions ...) -> (function any) ->  any
+;; Applies functions in reverse order. Named after the reverse composition operator in OCaml.
+
+(define (<< . functions)
+  (let ([reversion (reverse functions)])
+    (apply compose reversion)))
