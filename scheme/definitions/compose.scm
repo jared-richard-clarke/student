4313aa688473243ed4e23d177229b21fe8311ecc
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-24 14:21:36 -0700

    Reformat and rename

--- scheme/definitions/compose.scm
+++ scheme/definitions/compose.scm
@@ -1,7 +1,8 @@
-;; (pipe value function ...) -> any
+;; (pipe any function ...) -> any
 ;; Transforms a value by passing it through a series of single-argument functions. 
 ;; The first function can take any number of arguments. All proceeding functions must take one.
-;; (pipe "dog" string-plural string-upcase) -> "DOGS"
+;; Functions are applied left to right.
+;; (pipe 1 add1 div2) -> 1
 
 (define (pipe arg . functions)
   (foldl (lambda (function value)
@@ -12,7 +13,8 @@
 ;; (compose function ...) -> (function any) -> any
 ;; Composes a series of functions into a single function expression.
 ;; The first function can take any number of arguments. All proceeding functions must take one.
-;; (define edit (compose string-plural string-upcase)) -> (edit "dog") -> "DOGS"
+;; Functions are applied left to right.
+;; (define add-div (compose add1 div2)) -> (add-div 1) -> 1
 
 (define (compose . functions)
   (lambda (arg)
@@ -22,14 +24,14 @@
            functions)))
 
 ;; (>> functions ...) -> (function any) -> any
-;; Alias for compose. Named after the composition operator in OCaml.
+;; Alias for compose. Named after the composition operator in #F.
 
 (define (>> . functions)
   (apply compose functions))
 
 ;; (<< functions ...) -> (function any) ->  any
-;; Applies functions in reverse order. Named after the reverse composition operator in OCaml.
+;; Applies functions in reverse order. Named after the reverse composition operator in #F.
 
 (define (<< . functions)
-  (let ([reversion (reverse functions)])
-    (apply compose reversion)))
+  (let ([functions (reverse functions)])
+    (apply compose functions)))
