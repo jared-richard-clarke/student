07c3616fe645deddf8ae423221dc94df7bc8cf0f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-07-26 22:58:14 -0700

    Add documentation

--- geometry/julia/matrices.jl
+++ geometry/julia/matrices.jl
@@ -1,35 +1,47 @@
+
+"""Creates a 3×3 identity matrix."""
 function identity()
-    [1 0 0;
-     0 1 0;
+    [1 0 0
+     0 1 0
      0 0 1]
 end
 
+"""Creates a 3×3 translation matrix."""
 function translate(x, y)
-    [1 0 x;
-     0 1 y;
+    [1 0 x
+     0 1 y
      0 0 1]
 end
 
+"""Creates a 3×3 scaling matrix."""
 function scale(x, y)
-    [x 0 0;
-     0 y 0;
+    [x 0 0
+     0 y 0
      0 0 1]
 end
 
+"""Creates a 3×3 rotation matrix."""
 function rotate(angle)
     c = cos(angle)
     s = sin(angle)
-    [c -s 0;
-     s c 0;
+    [c -s 0
+     s c 0
      0 0 1]
 end
 
+"""Creates a 3×3 shearing matrix."""
 function shear(x, y)
-    [1 x 0;
-     y 1 0;
+    [1 x 0
+     y 1 0
      0 0 1]
 end
 
+"""
+Multiplies a collection of 3×3 transformation matrices to create a combined transform.
+Initial value is an identity matrix.
+"""
 function transform(matrices...)
     foldl(*, matrices; init=identity())
 end
+
+
