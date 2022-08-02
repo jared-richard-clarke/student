effa98974067e3ee5511847217ed3be0d4811eb6
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-01 23:02:22 -0700

    Add custom matrix implementations

--- geometry/julia/matrices.jl
+++ geometry/julia/matrices.jl
@@ -1,48 +1,68 @@
-module Mat3
+module Matrices
 
-"""Creates a 3×3 identity matrix."""
+import Base: *
+
+"""
+A 2d transformation implemented as a column-major, 3x3 matrix.
+The third row is implicit. Columns and rows inverted as in OpenGL.
+"""
+struct Mat3
+    xx::Float64
+    yx::Float64
+    xy::Float64
+    yy::Float64
+    x0::Float64
+    y0::Float64
+end
+
+"""Creates a 3x3 identity matrix."""
 function identity()
-    [1 0 0
-     0 1 0
-     0 0 1]
+    Mat3(1, 0, 0, 1, 0, 0)
 end
 
-"""Creates a 3×3 translation matrix."""
-function translate(x, y)
-    [1 0 x
-     0 1 y
-     0 0 1]
+"""Creates a 3x3 translation matrix."""
+function translate(x::Float64, y::Float64)
+    Mat3(1, 0, 0, 1, x, y)
 end
 
-"""Creates a 3×3 scaling matrix."""
-function scale(x, y)
-    [x 0 0
-     0 y 0
-     0 0 1]
+"""Creates a 3x3 scaling matrix."""
+function scale(x::Float64, y::Float64)
+    Mat3(x, 0, 0, y, 0, 0)
 end
 
-"""Creates a 3×3 rotation matrix."""
-function rotate(angle)
+"""Creates a 3x3 rotation matrix."""
+function rotate(angle::Float64)
     c = cos(angle)
     s = sin(angle)
-    [c -s 0
-     s c 0
-     0 0 1]
+    Mat3(c, s, -s, c, 0, 0)
 end
 
-"""Creates a 3×3 shearing matrix."""
-function shear(x, y)
-    [1 x 0
-     y 1 0
-     0 0 1]
+"""Creates a 3x3 shearing matrix."""
+function shear(x::Float64, y::Float64)
+    Mat3(1, y, x, 1, 0, 0)
 end
+
+"""Combines matrix transformations through multiplication."""
+function multiply(a::Mat3, b::Mat3)
+    Mat3(
+        a.xx * b.xx + a.yx * b.xy,
+        a.xx * b.yx + a.yx * b.yy,
+        a.xy * b.xx + a.yy * b.xy,
+        a.xy * b.yx + a.yy * b.yy,
+        a.x0 * b.xx + a.y0 * b.xy + b.x0,
+        a.x0 * b.yx + a.y0 * b.yy + b.y0
+    )
+end
+
+"""Matrix multiplication defined as operator."""
+(*)(a::Mat3, b::Mat3) = multiply(a, b)
 
 """
-Multiplies a collection of 3×3 transformation matrices pairwise 
+Multiplies a collection of 3x3 transformation matrices pairwise 
 to create a combined transform. Initial value is an identity matrix.
 """
-function transform(matrices...)
-    foldl(*, matrices; init=identity())
+function transform(matrices::Mat3...)
+    foldl(multiply, matrices; init=identity())
 end
 
-end
+end # module Matrices
