e38022d3aa1f83405fb7520cf716d8a0efb6d8ea
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-05 15:14:47 -0700

    Rearrange methods

--- geometry/julia/vectors.jl
+++ geometry/julia/vectors.jl
@@ -19,6 +19,10 @@ add(v1::Vec2, v2::Vec2) = Vec2(v1.x + v2.x, v1.y + v2.y)
 sub(v1::Vec2, v2::Vec2) = Vec2(v1.x - v2.x, v1.y - v2.y)
 (-)(v1, v2) = sub(v1, v2)
 
+"""Inverts the signs of the vector components. Rotates vector 180 degrees."""
+neg(v::Vec2) = Vec2(-v.x, -v.y)
+(-)(v) = neg(v)
+
 # Returns function that runs a pairwise operation over a sequence.
 function total(op, zero)
     (vs) -> length(vs == 0) ? zero : foldl(op, vs; init=zero)
@@ -30,9 +34,8 @@ sum = total(add, Vec2(0, 0))
 """Computes the difference of a series of vectors."""
 diff = total(sub, Vec2(0, 0))
 
-"""Inverts the signs of the vector components. Rotates vector 180 degrees."""
-neg(v::Vec2) = Vec2(-v.x, -v.y)
-(-)(v) = neg(v)
+"""Computes the magnitude (a.k.a. length) of a vector."""
+mag(v::Vec2) = hypot(v.x, v.y)
 
 """Scales a vector by a number."""
 scale(v::Vec2, scalar::Float64) = Vec2(v.x * scalar, v.y * scalar)
@@ -42,17 +45,6 @@ scale(v::Vec2, scalar::Float64) = Vec2(v.x * scalar, v.y * scalar)
 """Computes the dot product of two vectors."""
 dot(v1::Vec2, v2::Vec2) = (v1.x * v2.x) + (v1.y * v2.y)
 
-"""Computes the unit vector of a vector."""
-function normalize(v::Vec2)
-    m = mag(v)
-    x = v.x
-    y = v.y
-    Vec2(x / m, y / m)
-end
-
-"""Computes the magnitude (a.k.a. length) of a vector."""
-mag(v::Vec2) = hypot(v.x, v.y)
-
 """Computes the distance between the tips of two vectors."""
 distance(v1::Vec2, v2::Vec2) = hypot(v2.x - v1.x, v2.y - v1.y)
 
@@ -61,6 +53,14 @@ function lerp(v1::Vec2, v2::Vec2, t::Real)
     x = v1.x + (v2.x - v1.x) * t
     y = v1.y + (v2.y - v1.y) * t
     Vec2(x, y)
+end
+
+"""Computes the unit vector of a vector."""
+function normalize(v::Vec2)
+    m = mag(v)
+    x = v.x
+    y = v.y
+    Vec2(x / m, y / m)
 end
 
 end # Vectors module
