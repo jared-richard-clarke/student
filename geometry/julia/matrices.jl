816e9c6c8bf8786805341c0e296eafc5109b3c14
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-05 15:34:41 -0700

    Rearrange functions

--- geometry/julia/matrices.jl
+++ geometry/julia/matrices.jl
@@ -15,6 +15,21 @@ struct Mat3
     y0::Float64
 end
 
+"""Combines matrix transformations through multiplication."""
+function multiply(a::Mat3, b::Mat3)
+    Mat3(
+        a.xx * b.xx + a.yx * b.xy,
+        a.xx * b.yx + a.yx * b.yy,
+        a.xy * b.xx + a.yy * b.xy,
+        a.xy * b.yx + a.yy * b.yy,
+        a.x0 * b.xx + a.y0 * b.xy + b.x0,
+        a.x0 * b.yx + a.y0 * b.yy + b.y0
+    )
+end
+
+"""Matrix multiplication defined as operator."""
+(*)(a::Mat3, b::Mat3) = multiply(a, b)
+
 """Creates a 3x3 identity matrix."""
 function identity()
     Mat3(1, 0, 0, 1, 0, 0)
@@ -41,21 +56,6 @@ end
 function shear(x::Float64, y::Float64)
     Mat3(1, y, x, 1, 0, 0)
 end
-
-"""Combines matrix transformations through multiplication."""
-function multiply(a::Mat3, b::Mat3)
-    Mat3(
-        a.xx * b.xx + a.yx * b.xy,
-        a.xx * b.yx + a.yx * b.yy,
-        a.xy * b.xx + a.yy * b.xy,
-        a.xy * b.yx + a.yy * b.yy,
-        a.x0 * b.xx + a.y0 * b.xy + b.x0,
-        a.x0 * b.yx + a.y0 * b.yy + b.y0
-    )
-end
-
-"""Matrix multiplication defined as operator."""
-(*)(a::Mat3, b::Mat3) = multiply(a, b)
 
 """
 Multiplies a collection of 3x3 transformation matrices pairwise 
