697499200abafc24f86bd1821cddeca2db479de9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-05 18:44:07 -0700

    Reupdate tests to match package

--- geometry/golang/vectors/vec2_test.go
+++ geometry/golang/vectors/vec2_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 )
 
-func TestStringer(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := "vec(3.00, 4.00)"
-	result := v.String()
-	if expect != result {
-		t.Errorf("Test stringer failed. Expected: %v, Got: %v", expect, result)
+func TestApproxEq(t *testing.T) {
+	ep := 0.000001
+	v1 := Vec2{3.0 + ep, 4.0 + ep}
+	v2 := Vec2{3.0, 4.0}
+	if !v1.ApproxEq(v2) {
+		t.Errorf("Test ApproxEq failed. Check test epsilon (ep).")
 	}
 }
 
@@ -26,6 +26,15 @@ func TestSub(t *testing.T) {
 	result := Vec2{3.0, 4.0}.Sub(Vec2{1.0, 2.0})
 	if expect != result {
 		t.Errorf("Test Add failed. Expected: %v, Got: %v", expect, result)
+	}
+}
+
+func TestNegate(t *testing.T) {
+	v := Vec2{3.0, 4.0}
+	expect := Vec2{-3.0, -4.0}
+	result := v.Negate()
+	if expect != result {
+		t.Errorf("Test Negate failed. Expected: %v, Got %v", expect, result)
 	}
 }
 
@@ -46,12 +55,12 @@ func TestDiff(t *testing.T) {
 	}
 }
 
-func TestNegate(t *testing.T) {
+func TestMag(t *testing.T) {
 	v := Vec2{3.0, 4.0}
-	expect := Vec2{-3.0, -4.0}
-	result := v.Negate()
+	expect := 5.0
+	result := v.Mag()
 	if expect != result {
-		t.Errorf("Test Negate failed. Expected: %v, Got %v", expect, result)
+		t.Errorf("Test Mag failed. Expected: %.2f, Got: %.2f", expect, result)
 	}
 }
 
@@ -75,24 +84,6 @@ func TestDotProduct(t *testing.T) {
 	}
 }
 
-func TestNormalize(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := Vec2{0.6, 0.8}
-	result := v.Normalize()
-	if expect != result {
-		t.Errorf("Test Normalize failed. Expected: %v, Got: %v", expect, result)
-	}
-}
-
-func TestMag(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := 5.0
-	result := v.Mag()
-	if expect != result {
-		t.Errorf("Test Mag failed. Expected: %.2f, Got: %.2f", expect, result)
-	}
-}
-
 func TestDistance(t *testing.T) {
 	v1 := Vec2{8.0, 0.0}
 	v2 := Vec2{1.0, 0.0}
@@ -111,12 +102,12 @@ func TestLerp(t *testing.T) {
 	}
 }
 
-func TestEquals(t *testing.T) {
-	ep := 0.000001
-	v1 := Vec2{3.0 + ep, 4.0 + ep}
-	v2 := Vec2{3.0, 4.0}
-	if !v1.Equals(v2) {
-		t.Errorf("Test Equals failed. Check test epsilon (ep).")
+func TestNormalize(t *testing.T) {
+	v := Vec2{3.0, 4.0}
+	expect := Vec2{0.6, 0.8}
+	result := v.Normalize()
+	if expect != result {
+		t.Errorf("Test Normalize failed. Expected: %v, Got: %v", expect, result)
 	}
 }
 
@@ -129,18 +120,11 @@ func TestRound(t *testing.T) {
 	}
 }
 
-func TestCeil(t *testing.T) {
-	expect := Vec2{1.0, 5.0}
-	result := Vec2{0.75, 4.25}.Ceil()
+func TestStringer(t *testing.T) {
+	v := Vec2{3.0, 4.0}
+	expect := "vec(3.00, 4.00)"
+	result := v.String()
 	if expect != result {
-		t.Errorf("Test Ceil failed. Expected: %v, Got: %v", expect, result)
-	}
-}
-
-func TestFloor(t *testing.T) {
-	expect := Vec2{0.0, 4.0}
-	result := Vec2{0.75, 4.25}.Floor()
-	if expect != result {
-		t.Errorf("Test Floor failed. Expected: %v, Got: %v", expect, result)
+		t.Errorf("Test stringer failed. Expected: %v, Got: %v", expect, result)
 	}
 }
