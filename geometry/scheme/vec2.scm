315aac720d97a4efe407e3fd3c33dcbc02721cfe
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-05 17:35:23 -0700

    Rearrange functions

--- geometry/scheme/vec2.scm
+++ geometry/scheme/vec2.scm
@@ -31,8 +31,21 @@
 (define (vec2 x y)
   (vector x y))
 
+;; (vec2-approx-eq? (vector number number) (vector number number)) -> boolean
+;; Checks whether floating-point vector components are approximately equal.
+;; Comparisons made left to right.
+;; (vec2-approx-eq? (vec2 3.2 4.0) (vec2 3.19999999989 4.0)) -> #t
+
+(define (vec2-approx-eq? v1 v2)
+  (let ([x1 (vector-ref v1 0)]
+        [y1 (vector-ref v1 1)]
+        [x2 (vector-ref v2 0)]
+        [y2 (vector-ref v2 1)])
+    (and (approx-eq? x1 x2)
+         (approx-eq? y1 y2))))
+
 ;; (arithmetic function) -> (function (vector number number) (vector number number)) -> (vector number number)
-;; Generates functions that perform an arithmetic operation over two, two-dimensional vectors.
+;; Creates functions that perform an arithmetic operation over two vectors.
 ;; (define vec2-add (arithmetic +)) -> (vec2-add (vec2 1 2) (vec2 3 4)) -> #(4 5)
 
 (define (arithmetic operation)
@@ -55,9 +68,18 @@
 
 (define vec2-sub (arithmetic -))
 
+;; (vec2-negate (vector number number)) -> (vector number number)
+;; Inverts the signs of the vector components. Flips the vector 180 degrees.
+;; (vec2-negate (vec2 3 4)) -> (vec2 -3 -4)
+
+(define (vec2-negate vec)
+  (let ([x (vector-ref vec 0)]
+        [y (vector-ref vec 1)])
+    (vec2 (- x) (- y))))
+
 ;; (total function (vector number number)) -> (function (vector number number) ...) -> (vector number number)
-;; Returns function that performs an arithmetic operation over a series of two-dimensional vectors.
-;; (define vec2-sum (total + #(0 0))) -> (total (vec2 1 2) (vec2 3 4)) -> #(4 6)
+;; Creates function that performs an arithmetic operation over a series of vectors.
+;; (define vec2-sum (total + #(0 0))) -> (vec2-sum (vec2 1 2) (vec2 3 4)) -> #(4 6)
 
 (define (total operation base)
   (lambda vecs
@@ -80,19 +102,19 @@
 (define vec2-sum (total + #(0 0)))
 
 ;; (vec2-diff (vector number number) ...) -> (vector number number)
-;; Returns the difference of a series of two-dimensional vectors.
+;; Returns the difference of a series of vectors.
 ;; (vec2-diff (vec2 3 4) (vec2 1 2)) -> #(2 2)
 
 (define vec2-diff (total - #(0 0)))
 
-;; (vec2-negate (vector number number)) -> (vector number number)
-;; Inverts the signs of the vector components. Flips the vector 180 degrees.
-;; (vec2-negate (vec2 3 4)) -> (vec2 -3 -4)
+;; (vec2-mag (vector number number)) -> number
+;; Returns the magnitude of a two-dimensional vector.
+;; (vec2-mag (vec2 3 4)) -> 5
 
-(define (vec2-negate vec)
+(define (vec2-mag vec)
   (let ([x (vector-ref vec 0)]
         [y (vector-ref vec 1)])
-    (vec2 (- x) (- y))))
+    (hypotenuse x y)))
 
 ;; (vec2-scale (vector number number) number) -> (vector number number)
 ;; Returns a vector multiplied by a number.
@@ -104,7 +126,7 @@
     (vec2 (* scalar x) (* scalar y))))
 
 ;; (vec2-dot (vector number number) (vector number number)) -> number
-;; Returns the dot product of two, two-dimensional vectors.
+;; Returns the dot product of two vectors.
 ;; (vec2-dot (vec2 1 2) (vec2 3 4)) -> 11
 
 (define (vec2-dot v1 v2)
@@ -113,25 +135,6 @@
         [x2 (vector-ref v2 0)]
         [y2 (vector-ref v2 1)])
     (+ (* x1 x2) (* y1 y2))))
-
-;; (vec2-normalize (vector number number)) -> (vector number number)
-;; Returns the unit vector of a two-dimensional vector.
-;; (vec2-normalize (vec2 3 4)) -> #(3/5 4/5)
-
-(define (vec2-normalize vec)
-  (let ([m (vec2-mag vec)]
-        [x (vector-ref vec 0)]
-        [y (vector-ref vec 1)])
-    (vec2 (/ x m) (/ y m))))
-
-;; (vec2-mag (vector number number)) -> number
-;; Returns the magnitude of a two-dimensional vector.
-;; (vec2-mag (vec2 3 4)) -> 5
-
-(define (vec2-mag vec)
-  (let ([x (vector-ref vec 0)]
-        [y (vector-ref vec 1)])
-    (hypotenuse x y)))
 
 ;; (vec2-distance (vector number number) (vector number number)) -> number
 ;; Returns the distance between two, two-dimensional vectors.
@@ -157,34 +160,24 @@
          [y (+ y1 (* (- y2 y1) t))])
     (vec2 x y)))
 
-;; (vec2-approx-eq? (vector number number) (vector number number)) -> boolean
-;; Checks whether floating-point vector components are approximately equal.
-;; Comparisons made left to right.
-;; (vec2-approx-eq? (vec2 3.2 4.0) (vec2 3.19999999989 4.0)) -> #t
+;; (vec2-normalize (vector number number)) -> (vector number number)
+;; Returns the unit vector of a two-dimensional vector.
+;; (vec2-normalize (vec2 3 4)) -> #(3/5 4/5)
 
-(define (vec2-approx-eq? v1 v2)
-  (let ([x1 (vector-ref v1 0)]
-        [y1 (vector-ref v1 1)]
-        [x2 (vector-ref v2 0)]
-        [y2 (vector-ref v2 1)])
-    (and (approx-eq? x1 x2)
-         (approx-eq? y1 y2))))
+(define (vec2-normalize vec)
+  (let ([m (vec2-mag vec)]
+        [x (vector-ref vec 0)]
+        [y (vector-ref vec 1)])
+    (vec2 (/ x m) (/ y m))))
 
-;; (approximate function) -> (function (vector number number)) -> (vector number number)
-;; Generates approximation functions for rounding vector components.
-;; (define vec2-round (approximate round)) -> (vec2-round (vec2 1.3 1.7)) -> #(1.0 2.0)
+;; (round (vector number number)) -> (vector number number)
+;; Rounds the vector components.
+;; (vec2-round (vec2 1.3 1.7)) -> #(1.0 2.0)
 
-(define (approximate operation)
-  (lambda (vec)
-    (let ([x (vector-ref vec 0)]
-          [y (vector-ref vec 1)])
-      (vec2 (operation x) (operation y)))))
-
-;; vec2 rounding functions
-
-(define vec2-round (approximate round))
-(define vec2-ceil (approximate ceiling))
-(define vec2-floor (approximate floor))
+(define (vec2-round vec)
+  (let ([x (vector-ref vec 0)]
+        [y (vector-ref vec 1)])
+    (vec2 (round x) (round y))))
 
 ;; === testing ===
 
@@ -212,6 +205,9 @@
 (assert-equal (vec2 3 4)
               #(3 4))
 
+(assert-equal (vec2-approx-eq? (vec2 3.2 4.0) (vec2 3.19999999989 4.0))
+              #t)
+
 (assert-equal (vec2-add (vec2 3 4) (vec2 7 11))
               #(10 15))
 
@@ -227,11 +223,20 @@
 (assert-equal (vec2-negate (vec2 -3 -4))
               #(3 4))
 
+(assert-equal (vec2-mag (vec2 3 4))
+              5)
+
 (assert-equal (vec2-scale (vec2 1 2) 2)
               #(2 4))
 
 (assert-equal (vec2-dot (vec2 1 2) (vec2 3 4))
               11)
+
+(assert-equal (vec2-distance (vec2 8 0) (vec2 1 0))
+              7)
+
+(assert-equal (vec2-lerp 1/2 (vec2 0 0) (vec2 10 0))
+              (vec2 5 0))
 
 ;; rational division
 (assert-equal (vec2-normalize (vec2 3 4))
@@ -241,23 +246,5 @@
 (assert-equal (vec2-normalize (vec2 3.0 4.0))
               #(0.6 0.8))
 
-(assert-equal (vec2-mag (vec2 3 4))
-              5)
-
-(assert-equal (vec2-distance (vec2 8 0) (vec2 1 0))
-              7)
-
-(assert-equal (vec2-lerp 1/2 (vec2 0 0) (vec2 10 0))
-              (vec2 5 0))
-
-(assert-equal (vec2-approx-eq? (vec2 3.2 4.0) (vec2 3.19999999989 4.0))
-              #t)
-
 (assert-equal (vec2-round (vec2 1.3 1.7))
               #(1.0 2.0))
-
-(assert-equal (vec2-ceil (vec2 1.3 1.7))
-              #(2.0 2.0))
-
-(assert-equal (vec2-floor (vec2 1.3 1.7))
-              #(1.0 1.0))
