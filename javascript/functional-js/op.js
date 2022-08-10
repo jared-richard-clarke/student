4fc81b5111a6a3596a266d03dc727feb1c0524e0
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-09 19:05:31 -0700

    Add binary operations

--- javascript/functional-js/op.js
+++ javascript/functional-js/op.js
@@ -1,7 +1,18 @@
-// module "op" provides sum and product: variable-arity, arithmetic functions.
-// op.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, op.product(2, 5)) -> 55
-// op.product() -> 1
-// op.sum() -> 0
+// Provides arithmetic functions.
+// module op = readonly {
+//     1. add(number, number) -> number
+//     2. sub(number, number) -> number
+//     3. mul(number, number) -> number
+//     4. div(number, number) -> number
+//     5. sum(...number)      -> number
+//     6. product(...number)  -> number
+// }
+
+function binary(operation) {
+    return Object.freeze(function (x, y) {
+        return operation(x, y);
+    });
+}
 
 function monoid(operation, identity) {
     return Object.freeze(function (...operands) {
@@ -14,7 +25,13 @@ function monoid(operation, identity) {
 
 const op = Object.create(null);
 
+op.add = binary((x, y) => x + y);
+op.sub = binary((x, y) => x - y);
+op.mul = binary((x, y) => x * y);
+op.div = binary((x, y) => x / y);
+
 op.sum = monoid((x, y) => x + y, 0);
 op.product = monoid((x, y) => x * y, 1);
 
 export default Object.freeze(op);
+
