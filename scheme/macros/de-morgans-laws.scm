cf0fb482efbdedbab047fccf900cffb27e4e28d9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-23 13:22:02 -0700

    Add Clojure "and" definition in comments

--- scheme/macros/de-morgans-laws.scm
+++ scheme/macros/de-morgans-laws.scm
@@ -1,4 +1,4 @@
-;; Syntax as defined in the Revised7 Report on the Algorithmic Language Scheme
+;; "and" and "or" syntax as defined in the Revised7 Report on the Algorithmic Language Scheme
 (define-syntax and
   (syntax-rules ()
     [(and) #t]
@@ -49,3 +49,17 @@
        (if x
            x
            (not B))))
+
+;; "and" syntax as defined in Clojure. Added here for comparison with Scheme.
+;;
+;; (defmacro and
+;;   "Evaluates exprs one at a time, from left to right. If a form
+;;   returns logical false (nil or false), and returns that value and
+;;   doesn't evaluate any of the other expressions, otherwise it returns
+;;   the value of the last expr. (and) returns true."
+;;   {:added "1.0"}
+;;   ([] true)
+;;   ([x] x)
+;;   ([x & next]
+;;    `(let [and# ~x]
+;;     (if and# (and ~@next) and#))))
