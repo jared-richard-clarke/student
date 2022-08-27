43c63e120d94ce6894ab6a6118c0de64e834a1f9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-08-26 18:07:00 -0700

    Add tail-recursive example

--- scheme/definitions/call-cc.scm
+++ scheme/definitions/call-cc.scm
@@ -9,3 +9,15 @@
          [(null? lst) 1]
          [(= (car lst) 0) (return 0)] ;; <------- nonlocal exit
          [else (* (car lst) (fn (cdr lst)))])))))
+
+;; "prod" doesn't need a nonlocal exit to return early.
+;; "prod" is tail-recursive. It need not return out of a nested stack.
+
+(define (prod . lst)
+  (let loop ([lst lst]
+             [id 1])
+    (cond
+      [(null? lst) id]
+      [(= (car lst) 0) 0] 
+      [else (loop (cdr lst)
+                  (* id (car lst)))])))
