759bee20c4af2975adc5bf7553457c9517f9fc3e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-09-01 21:25:43 -0700

    Remove florin and change pattern names

--- scheme/definitions/function-macros.scm
+++ scheme/definitions/function-macros.scm
@@ -4,22 +4,15 @@
 
 (define-syntax λ
   (syntax-rules ()
-    [(_ formals body-1 body-2 ...)
-     (lambda formals body-1 body-2 ...)]))
-
-;; Defines lowercase florin (ƒ) as an alternative symbol to lambda.
-
-(define-syntax ƒ
-  (syntax-rules ()
-    [(_ formals body-1 body-2 ...)
-     (lambda formals body-1 body-2 ...)]))
+    [(_ x y z ...)
+     (lambda x y z ...)]))
 
 ;; Inspired by OCaml's anonymous function keyword.
 
 (define-syntax fun
   (syntax-rules ()
-    [(_ formals body-1 body-2 ...)
-     (lambda formals body-1 body-2 ...)]))
+    [(_ x y z ...)
+     (lambda x y z ...)]))
 
 ;; ᓴᓐᓇ translates to Sedna. She is the Inuit goddess of the sea and marine mammals.
 ;; A function/procedure in Scheme is a an identifier, a list of formals, and a body of one or more expressions.
@@ -28,5 +21,5 @@
 
 (define-syntax ᓴᓐᓇ
   (syntax-rules ()
-    [(_ formals body-1 body-2 ...)
-     (lambda formals body-1 body-2 ...)]))
+    [(_ x y z ...)
+     (lambda x y z ...)]))
