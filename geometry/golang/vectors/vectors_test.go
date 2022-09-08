091fc83beedff7bc4c7c96b7bff1b1c5c2148d8c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-09-08 14:55:32 -0700

    Add tests for Vec3

--- geometry/golang/vectors/vectors_test.go
+++ geometry/golang/vectors/vectors_test.go
@@ -20,108 +20,166 @@ func TestApproxEq(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	expect := Vec2{4.0, 6.0}
-	result := Add(Vec2{1.0, 2.0}, Vec2{3.0, 4.0})
-	if expect != result {
-		t.Errorf("Test Add failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{4.0, 6.0}
+	vec2Result := Add(Vec2{1.0, 2.0}, Vec2{3.0, 4.0})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Add Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{4.0, 4.0, 4.0}
+	vec3Result := Add(Vec3{1.0, 2.0, 3.0}, Vec3{3.0, 2.0, 1.0})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Add Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestSub(t *testing.T) {
-	expect := Vec2{2.0, 2.0}
-	result := Sub(Vec2{3.0, 4.0}, Vec2{1.0, 2.0})
-	if expect != result {
-		t.Errorf("Test Add failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{2.0, 2.0}
+	vec2Result := Sub(Vec2{3.0, 4.0}, Vec2{1.0, 2.0})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Sub Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
 	}
+	vec3Expect := Vec3{2.0, 0.0, -2.0}
+	vec3Result := Sub(Vec3{3.0, 4.0, 1.0}, Vec3{1.0, 4.0, 3.0})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Sub Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
+	}
+
 }
 
 func TestNegate(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := Vec2{-3.0, -4.0}
-	result := Negate(v)
-	if expect != result {
-		t.Errorf("Test Negate failed. Expected: %v, Got %v", expect, result)
+	vec2Expect := Vec2{-3.0, -4.0}
+	vec2Result := Negate(Vec2{3.0, 4.0})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Negate Vec2 failed. Expected: %v, Got %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{-3.0, -4.0, -1.0}
+	vec3Result := Negate(Vec3{3.0, 4.0, 1.0})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Negate Vec2 failed. Expected: %v, Got %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestSum(t *testing.T) {
-	expect := Vec2{11.0, 17.0}
-	result := Sum(Vec2{1.0, 2.0}, Vec2{3.0, 4.0}, Vec2{7.0, 11.0})
-	if expect != result {
-		t.Errorf("Test Sum failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{11.0, 17.0}
+	vec2Result := Sum(Vec2{1.0, 2.0}, Vec2{3.0, 4.0}, Vec2{7.0, 11.0})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Sum Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{11.0, 17.0, 3.0}
+	vec3Result := Sum(Vec3{1.0, 2.0, 1.0}, Vec3{3.0, 4.0, 1.0}, Vec3{7.0, 11.0, 1.0})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Sum Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestMag(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := 5.0
-	result := Mag(v)
-	if expect != result {
-		t.Errorf("Test Mag failed. Expected: %.2f, Got: %.2f", expect, result)
+	vec2Expect := 5.0
+	vec2Result := Mag(Vec2{3.0, 4.0})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Mag Vec2 failed. Expected: %.2f, Got: %.2f", vec2Expect, vec2Result)
+	}
+	vec3Expect := 3.7416573867739413
+	vec3Result := Mag(Vec3{1.0, 2.0, 3.0})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Mag Vec3 failed. Expected: %.2f, Got: %.2f", vec3Expect, vec3Result)
 	}
 }
 
 func TestScale(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	s := 11.0
-	expect := Vec2{33.0, 44.0}
-	result := Scale(v, s)
-	if expect != result {
-		t.Errorf("Test Scale failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{33.0, 44.0}
+	vec2Result := Scale(Vec2{3.0, 4.0}, 11.0)
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Scale Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{33.0, 44.0, 11.0}
+	vec3Result := Scale(Vec3{3.0, 4.0, 1.0}, 11.0)
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Scale Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestDotProduct(t *testing.T) {
 	v1 := Vec2{3.0, 4.0}
 	v2 := Vec2{1.0, 2.0}
-	expect := 11.0
-	result := Dot(v1, v2)
-	if expect != result {
-		t.Errorf("Test Dot Product failed. Expected: %.2f, Got: %.2f", expect, result)
+	vec2Expect := 11.0
+	vec2Result := Dot(v1, v2)
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Dot Product Vec2 failed. Expected: %.2f, Got: %.2f", vec2Expect, vec2Result)
+	}
+	v3 := Vec3{3.0, 4.0, 5.0}
+	v4 := Vec3{1.0, 2.0, 3.0}
+	vec3Expect := 26.0
+	vec3Result := Dot(v3, v4)
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Dot Product Vec3 failed. Expected: %.2f, Got: %.2f", vec3Expect, vec3Result)
 	}
 }
 
 func TestDistance(t *testing.T) {
 	v1 := Vec2{8.0, 0.0}
 	v2 := Vec2{1.0, 0.0}
-	expect := 7.0
-	result := Distance(v1, v2)
-	if expect != result {
-		t.Errorf("Test Distance failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := 7.0
+	vec2Result := Distance(v1, v2)
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Distance Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	v3 := Vec3{10.0, 0.0, 0.0}
+	v4 := Vec3{3.0, 0.0, 0.0}
+	vec3Expect := 7.0
+	vec3Result := Distance(v3, v4)
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Distance Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestLerp(t *testing.T) {
-	expect := Vec2{5.0, 0.0}
-	result := Lerp(Vec2{0.0, 0.0}, Vec2{10.0, 0.0}, 0.5)
-	if expect != result {
-		t.Errorf("Test Lerp failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{5.0, 0.0}
+	vec2Result := Lerp(Vec2{0.0, 0.0}, Vec2{10.0, 0.0}, 0.5)
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Lerp Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{5.0, 0.0, 0.0}
+	vec3Result := Lerp(Vec3{0.0, 0.0, 0.0}, Vec3{10.0, 0.0, 0.0}, 0.5)
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Lerp Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestNormalize(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := Vec2{0.6, 0.8}
-	result := Normalize(v)
-	if expect != result {
-		t.Errorf("Test Normalize failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{0.6, 0.8}
+	vec2Result := Normalize(Vec2{3.0, 4.0})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Normalize Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{0.5883484054145521, 0.7844645405527362, 0.19611613513818404}
+	vec3Result := Normalize(Vec3{3.0, 4.0, 1.0})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Normalize Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestRound(t *testing.T) {
-	v := Vec2{0.75, 4.25}
-	expect := Vec2{1.0, 4.0}
-	result := Round(v)
-	if expect != result {
-		t.Errorf("Test Round failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := Vec2{1.0, 4.0}
+	vec2Result := Round(Vec2{0.75, 4.25})
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Round Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := Vec3{1.0, 4.0, 2.0}
+	vec3Result := Round(Vec3{0.75, 4.25, 1.5})
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Round Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
 
 func TestStringer(t *testing.T) {
-	v := Vec2{3.0, 4.0}
-	expect := "vec(3.00, 4.00)"
-	result := v.String()
-	if expect != result {
-		t.Errorf("Test stringer failed. Expected: %v, Got: %v", expect, result)
+	vec2Expect := "vec(3.00, 4.00)"
+	vec2Result := Vec2{3.0, 4.0}.String()
+	if vec2Expect != vec2Result {
+		t.Errorf("Test Stringer Vec2 failed. Expected: %v, Got: %v", vec2Expect, vec2Result)
+	}
+	vec3Expect := "vec(3.00, 4.00, 1.00)"
+	vec3Result := Vec3{3.0, 4.0, 1.0}.String()
+	if vec3Expect != vec3Result {
+		t.Errorf("Test Stringer Vec3 failed. Expected: %v, Got: %v", vec3Expect, vec3Result)
 	}
 }
