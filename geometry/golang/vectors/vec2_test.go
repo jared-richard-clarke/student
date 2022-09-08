8e79d312a1aaf3b88c1f80364479aef22f1cd658
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-09-08 02:23:32 -0700

    Update tests to match package

--- geometry/golang/vectors/vec2_test.go
+++ geometry/golang/vectors/vec2_test.go
@@ -8,20 +8,20 @@ func TestApproxEq(t *testing.T) {
 	ep := 0.000001
 	v1 := Vec2{3.0 + ep, 4.0 + ep}
 	v2 := Vec2{3.0, 4.0}
-	if v1.ApproxEq(v2) == false {
+	if ApproxEq(v1, v2) == false {
 		t.Errorf("Test ApproxEq failed: false negative. Check epsilon (ep) in test.")
 	}
 	// Push values past threshold for approximate equality,
 	// by pushing epsilon into one figure higher in significance.
-	v1 = Vec2{3.0 + ep * 10, 4.0 + ep}
-	if v1.ApproxEq(v2) == true {
+	v1 = Vec2{3.0 + ep*10, 4.0 + ep}
+	if ApproxEq(v1, v2) == true {
 		t.Errorf("Test ApproxEq failed: false positive. Check epsilon (ep) in test.")
 	}
 }
 
 func TestAdd(t *testing.T) {
 	expect := Vec2{4.0, 6.0}
-	result := Vec2{1.0, 2.0}.Add(Vec2{3.0, 4.0})
+	result := Add(Vec2{1.0, 2.0}, Vec2{3.0, 4.0})
 	if expect != result {
 		t.Errorf("Test Add failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -29,7 +29,7 @@ func TestAdd(t *testing.T) {
 
 func TestSub(t *testing.T) {
 	expect := Vec2{2.0, 2.0}
-	result := Vec2{3.0, 4.0}.Sub(Vec2{1.0, 2.0})
+	result := Sub(Vec2{3.0, 4.0}, Vec2{1.0, 2.0})
 	if expect != result {
 		t.Errorf("Test Add failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -38,7 +38,7 @@ func TestSub(t *testing.T) {
 func TestNegate(t *testing.T) {
 	v := Vec2{3.0, 4.0}
 	expect := Vec2{-3.0, -4.0}
-	result := v.Negate()
+	result := Negate(v)
 	if expect != result {
 		t.Errorf("Test Negate failed. Expected: %v, Got %v", expect, result)
 	}
@@ -46,7 +46,7 @@ func TestNegate(t *testing.T) {
 
 func TestSum(t *testing.T) {
 	expect := Vec2{11.0, 17.0}
-	result := Vec2{1.0, 2.0}.Sum(Vec2{3.0, 4.0}, Vec2{7.0, 11.0})
+	result := Sum(Vec2{1.0, 2.0}, Vec2{3.0, 4.0}, Vec2{7.0, 11.0})
 	if expect != result {
 		t.Errorf("Test Sum failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -55,7 +55,7 @@ func TestSum(t *testing.T) {
 func TestMag(t *testing.T) {
 	v := Vec2{3.0, 4.0}
 	expect := 5.0
-	result := v.Mag()
+	result := Mag(v)
 	if expect != result {
 		t.Errorf("Test Mag failed. Expected: %.2f, Got: %.2f", expect, result)
 	}
@@ -65,7 +65,7 @@ func TestScale(t *testing.T) {
 	v := Vec2{3.0, 4.0}
 	s := 11.0
 	expect := Vec2{33.0, 44.0}
-	result := v.Scale(s)
+	result := Scale(v, s)
 	if expect != result {
 		t.Errorf("Test Scale failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -75,7 +75,7 @@ func TestDotProduct(t *testing.T) {
 	v1 := Vec2{3.0, 4.0}
 	v2 := Vec2{1.0, 2.0}
 	expect := 11.0
-	result := v1.Dot(v2)
+	result := Dot(v1, v2)
 	if expect != result {
 		t.Errorf("Test Dot Product failed. Expected: %.2f, Got: %.2f", expect, result)
 	}
@@ -85,7 +85,7 @@ func TestDistance(t *testing.T) {
 	v1 := Vec2{8.0, 0.0}
 	v2 := Vec2{1.0, 0.0}
 	expect := 7.0
-	result := v1.Distance(v2)
+	result := Distance(v1, v2)
 	if expect != result {
 		t.Errorf("Test Distance failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -93,7 +93,7 @@ func TestDistance(t *testing.T) {
 
 func TestLerp(t *testing.T) {
 	expect := Vec2{5.0, 0.0}
-	result := Vec2{0.0, 0.0}.Lerp(Vec2{10.0, 0.0}, 0.5)
+	result := Lerp(Vec2{0.0, 0.0}, Vec2{10.0, 0.0}, 0.5)
 	if expect != result {
 		t.Errorf("Test Lerp failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -102,7 +102,7 @@ func TestLerp(t *testing.T) {
 func TestNormalize(t *testing.T) {
 	v := Vec2{3.0, 4.0}
 	expect := Vec2{0.6, 0.8}
-	result := v.Normalize()
+	result := Normalize(v)
 	if expect != result {
 		t.Errorf("Test Normalize failed. Expected: %v, Got: %v", expect, result)
 	}
@@ -111,7 +111,7 @@ func TestNormalize(t *testing.T) {
 func TestRound(t *testing.T) {
 	v := Vec2{0.75, 4.25}
 	expect := Vec2{1.0, 4.0}
-	result := v.Round()
+	result := Round(v)
 	if expect != result {
 		t.Errorf("Test Round failed. Expected: %v, Got: %v", expect, result)
 	}
