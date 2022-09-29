0b626315e426c7ad6da269bd40974e9b0030e525
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-09-28 22:03:56 -0700

    Add unit tests for vec-invert

--- geometry/scheme/vectors_test.scm
+++ geometry/scheme/vectors_test.scm
@@ -25,11 +25,17 @@
 (assert-equal (vec-sub (vec2 3 4) (vec2 7 11))
               '#(-4 -7))
 
-(assert-equal (vec-sum (vec2 3 4) (vec2 1 2) (vec2 1 2))
-              '#(5 8))
-
 (assert-equal (vec-neg (vec2 -3 -4))
               '#(3 4))
+
+(assert-equal (vec-invert (vec2 2.0 2.0))
+              '#(0.5 0.5))
+
+(assert-equal (vec-invert (vec2 0.5 0.5))
+              '#(2.0 2.0))
+
+(assert-equal (vec-sum (vec2 3 4) (vec2 1 2) (vec2 1 2))
+              '#(5 8))
 
 (assert-equal (vec-mag (vec2 3 4))
               5)
