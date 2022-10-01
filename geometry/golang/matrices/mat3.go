cb575b077ab222a7f12b6981e7f9f129c1a12a95
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-09-30 18:03:31 -0700

    Expand documentation

--- geometry/golang/matrices/mat3.go
+++ geometry/golang/matrices/mat3.go
@@ -6,15 +6,30 @@ import (
 	"math"
 )
 
-// A column-major, 3 × 3 affine transformation matrix implemented as a 6-part array.
-// The third row is implicit.
+// A 3 × 3 affine transformation matrix implemented as a 6-part array.
+// Parameters are described in column-major order. Constants are implied.
 //
-// [ A B C D ] <- linear transformations
-// [ E F ] <----- translations
+// Matrix Notation
 //
-//        |-------|-------|---- implied
+// | A C E |
+// | B D F |
+// | 0 0 1 |
+//
+// Array Notation
+//
+// [ A B C D <--- linear transformations
+//   E F ] <----- translations
+//
+//        |-------|-------|---- implied constants
 // [ A B (0) C D (0) E F (1) ]
 //   0 1     2 3     4 5
+//
+// A: scale-x
+// B: skew-y
+// C: skew-x
+// D: scale-y
+// E: translate-x
+// F: translate-y
 
 type Mat3 [6]float64
 
