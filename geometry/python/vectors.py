5cb57a135a03d9dfb2380c82b85f597d2ff81665
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-09-30 19:57:17 -0700

    Remove lerp method

--- geometry/python/vectors.py
+++ geometry/python/vectors.py
@@ -72,12 +72,6 @@ class Vector:
                   x2 in zip(self.point, other.point, strict=True)]
         return math.hypot(*result)
 
-    def lerp(self, other, t):
-        """Interpolates a vector point between the tips of two vectors."""
-        result = [x1 + (x2 - x1) * t for x1,
-                  x2 in zip(self.point, other.point, strict=True)]
-        return type(self)(*result)
-
     def normalize(self):
         """Returns the unit vector of a vector."""
         mag = self.mag()
