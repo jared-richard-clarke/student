2069e9e160e3cbf06aa0fa0675088b39253c5957
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-02 00:00:18 -0700

    Expand tests

--- geometry/scheme/vectors_test.scm
+++ geometry/scheme/vectors_test.scm
@@ -7,47 +7,98 @@
 (assert-equal (hypotenuse 3 4)
               5)
 
+;; Vectors
+
 (assert-equal (vec2 3 4)
               '#(3 4))
 
 (assert-equal (vec3 3 4 1)
               '#(3 4 1))
 
+(assert-equal (vec2? (vec2 3 4))
+              #t)
+
 (assert-equal (vec3? '#(3 4 1))
               #t)
 
-(assert-equal (vec2? (vec2 3 4))
-              #t)
+;; Addition
 
 (assert-equal (vec-add (vec2 3 4) (vec2 7 11))
               '#(10 15))
 
+(assert-equal (vec-add (vec3 3 4 5) (vec3 1 2 3))
+              '#(4 6 8))
+
+;; Subtraction
+
 (assert-equal (vec-sub (vec2 3 4) (vec2 7 11))
               '#(-4 -7))
+
+(assert-equal (vec-sub (vec3 3 4 5) (vec3 1 2 3))
+              '#(2 2 2))
+
+;; Negation
 
 (assert-equal (vec-neg (vec2 -3 -4))
               '#(3 4))
 
-(assert-equal (vec-invert (vec2 2.0 2.0))
-              '#(0.5 0.5))
+(assert-equal (vec-neg (vec3 3 4 -5))
+              '#(-3 -4 5))
 
-(assert-equal (vec-invert (vec2 0.5 0.5))
-              '#(2.0 2.0))
+;; Inversion
+
+(assert-equal (vec-invert (vec2 2 2))
+              '#(1/2 1/2))
+
+(assert-equal (vec-invert (vec2 1/2 1/2))
+              '#(2 2))
+
+(assert-equal (vec-invert (vec3 3 4 5))
+              '#(1/3 1/4 1/5))
+
+(assert-equal (vec-invert (vec3 1/3 1/4 1/5))
+              '#(3 4 5))
+
+;; Summation
 
 (assert-equal (vec-sum (vec2 3 4) (vec2 1 2) (vec2 1 2))
               '#(5 8))
 
+(assert-equal (vec-sum (vec2 3 4))
+              '#(3 4))
+
+(assert-equal (vec-sum (vec3 3 4 5) (vec3 1 2 3) (vec3 1 1 1))
+              '#(5 7 9))
+
+;; Magnitude
+
 (assert-equal (vec-mag (vec2 3 4))
               5)
+
+;; Scale
 
 (assert-equal (vec-scale (vec2 1 2) 2)
               '#(2 4))
 
+(assert-equal (vec-scale (vec3 1 2 3) 2)
+              '#(2 4 6))
+
+;; Dot Product
+
 (assert-equal (vec-dot (vec2 1 2) (vec2 3 4))
               11)
 
+;; Distance
+
 (assert-equal (vec-dist (vec2 8 0) (vec2 1 0))
               7)
+
+;; Interpolation
+
+(assert-equal (vec-lerp (vec2 1 1) (vec2 3 2) 0.5)
+              (vec2 2.0 1.5))
+
+;; Normalize
 
 ;; rational division
 (assert-equal (vec-normalize (vec2 3 4))
@@ -56,6 +107,8 @@
 ;; floating-point division
 (assert-equal (vec-normalize (vec2 3.0 4.0))
               '#(0.6 0.8))
+
+;; Rounding
 
 (assert-equal (vec-round (vec2 1.3 1.7))
               '#(1.0 2.0))
