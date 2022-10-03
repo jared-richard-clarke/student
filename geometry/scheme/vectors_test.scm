54695e17c90bae07ff5e8d70d58540d29b653bfb
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-02 20:43:58 -0700

    Add unit tests for vec3

--- geometry/scheme/vectors_test.scm
+++ geometry/scheme/vectors_test.scm
@@ -42,8 +42,14 @@
 (assert-equal (vec-neg (vec2 -3 -4))
               '#(3 4))
 
+(assert-equal (vec-neg (vec2 0 0))
+              '#(0 0))
+
 (assert-equal (vec-neg (vec3 3 4 -5))
               '#(-3 -4 5))
+
+(assert-equal (vec-neg (vec3 0 0 0))
+              '#(0 0 0))
 
 ;; Inversion
 
@@ -70,10 +76,16 @@
 (assert-equal (vec-sum (vec3 3 4 5) (vec3 1 2 3) (vec3 1 1 1))
               '#(5 7 9))
 
+(assert-equal (vec-sum (vec3 1 2 3))
+              '#(1 2 3))
+
 ;; Magnitude
 
 (assert-equal (vec-mag (vec2 3 4))
               5)
+
+(assert-equal (vec-mag (vec3 3 4 5))
+              7.0710678118654755)
 
 ;; Scale
 
@@ -88,15 +100,24 @@
 (assert-equal (vec-dot (vec2 1 2) (vec2 3 4))
               11)
 
+(assert-equal (vec-dot (vec3 1 2 3) (vec3 3 4 5))
+              26)
+
 ;; Distance
 
 (assert-equal (vec-dist (vec2 8 0) (vec2 1 0))
+              7)
+
+(assert-equal (vec-dist (vec3 8 0 0) (vec3 1 0 0))
               7)
 
 ;; Interpolation
 
 (assert-equal (vec-lerp (vec2 1 1) (vec2 3 2) 0.5)
               (vec2 2.0 1.5))
+
+(assert-equal (vec-lerp (vec3 3 4 5) (vec3 1 2 3) 0.5)
+              '#(2.0 3.0 4.0))
 
 ;; Normalize
 
@@ -112,3 +133,6 @@
 
 (assert-equal (vec-round (vec2 1.3 1.7))
               '#(1.0 2.0))
+
+(assert-equal (vec-round (vec3 2.5 1.75 0.25))
+              '#(2.0 2.0 0.0))
