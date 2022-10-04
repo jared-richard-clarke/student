f5b148a9a407f219be6677411ca4fe3cebd50e0f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-04 02:08:52 -0700

    Reformat exports

--- geometry/julia/VectorGraph/src/VectorGraph.jl
+++ geometry/julia/VectorGraph/src/VectorGraph.jl
@@ -1,6 +1,7 @@
 module VectorGraph
 
-export Vectors, Matrices
+export Vec2, Vec3, invert, mag, scale, dot, distance, lerp, normalize
+export Mat3, MAT3_ID, translate, scale, rotate, shear, transform
 
 include("vectors.jl")
 include("matrices.jl")
