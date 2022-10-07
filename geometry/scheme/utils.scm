e9c51b31b07262ff91316aed92c91d96ea580479
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-06 18:42:14 -0700

    Move conversion functions into utils

--- geometry/scheme/utils.scm
+++ geometry/scheme/utils.scm
@@ -1,6 +1,31 @@
 (library (utils)
-         (export hypotenuse approx-eq? assert-equal)
+         (export deg->rad
+                 rad->deg
+                 PI
+                 π
+                 hypotenuse
+                 approx-eq?
+                 assert-equal)
          (import (scheme))
+
+         ;; PI and π: approximation of π.
+
+         (define PI 3.141592653589793)
+         (define π PI)
+
+         ;; (deg->rad number) -> number
+         ;; Converts degrees to radians.
+         ;; (deg->rad 7) -> 0.12217304763960307
+
+         (define (deg->rad degrees)
+           (* degrees (/ PI 180)))
+
+         ;; (rad->deg number) -> number
+         ;; Converts radians to degrees.
+         ;; (rad->deg 0.12217304763960307) -> 7.0
+
+         (define (rad->deg radians)
+           (* radians (/ 180 PI)))
 
          ;; (hypotenuse number number) -> number
          ;; Returns the square root of the sum of the squares of its arguments.
