9bc352503ce84eb02c818bda32cf27ad5d2a17cb
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-06 18:42:32 -0700

    Delete angles_test.scm

--- geometry/scheme/angles_test.scm
+++
@@ -1,8 +0,0 @@
-(import (rnrs base) 
-        (angles) 
-        (utils))
-
-;; === unit tests ===
-
-(assert-equal (deg->rad 7) 0.12217304763960307)
-(assert-equal (rad->deg 0.12217304763960307) 7.0)
