decd2cd665798193336f73a575b333c73ba8d551
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-08 21:52:58 -0700

    Rename compose, add vector transformer

--- geometry/julia/VectorGraph/src/VectorGraph.jl
+++ geometry/julia/VectorGraph/src/VectorGraph.jl
@@ -1,9 +1,10 @@
 module VectorGraph
 
 export Vec2, Vec3, abs, invert, mag, scale, dot, distance, lerp, normalize
-export Mat3, MAT3_ID, translate, scale, rotate, shear, transform
+export Mat3, MAT3_ID, translate, scale, rotate, shear, compose, transform
 
 include("vectors.jl")
 include("matrices.jl")
 
 end # module VectorGraph
+
