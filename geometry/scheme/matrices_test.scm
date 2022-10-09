8ea0863de3ac40c55e68316c3dc19fd7dc136b14
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-08 22:14:50 -0700

    Update tests to match module

--- geometry/scheme/matrices_test.scm
+++ geometry/scheme/matrices_test.scm
@@ -5,22 +5,23 @@
 ;; === unit tests ===
 
 (assert-equal m3-ID
-              (mat3 1 0 0 1 0 0))
+             (mat3 1 0 0 1 0 0))
 
-(assert-equal (m3-multiply m3-ID (m3-translate 3 4))
+(assert-equal (m3-compose m3-ID (m3-translate 3 4))
               (mat3 1 0 0 1 3 4))
 
-(assert-equal (m3-multiply m3-ID (m3-scale 2 2))
+(assert-equal (m3-compose m3-ID (m3-scale 2 2))
               (mat3 2 0 0 2 0 0))
 
 (define m3-ID-float (mat3 1.0 0.0 0.0 1.0 0.0 0.0))
 
-(assert-equal (m3-multiply (m3-multiply m3-ID-float (m3-rotate (deg->rad 90)))
-                           (m3-rotate (deg->rad -90)))
-              m3-ID-float)
+(assert-equal (m3-compose m3-ID-float
+                         (m3-rotate (deg->rad 90))
+                         (m3-rotate (deg->rad -90)))
+               m3-ID-float)
 
-(assert-equal (m3-multiply m3-ID (m3-shear 3 4))
+(assert-equal (m3-compose m3-ID (m3-shear 3 4))
               (mat3 1 4 3 1 0 0))
 
-(assert-equal (m3-transform (m3-translate 3 4) (m3-scale 2 2) (m3-shear 1 2))
+(assert-equal (m3-compose (m3-translate 3 4) (m3-scale 2 2) (m3-shear 1 2))
               (mat3 2 4 2 2 3 4))
