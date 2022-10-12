35196794fe3f6c13ed37928e945d20728e5e6bb8
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-10-12 14:38:10 -0700

    Change Vec2 and Vec3 to tuple structs

--- geometry/rust/descartes/src/vectors.rs
+++ geometry/rust/descartes/src/vectors.rs
@@ -4,104 +4,87 @@ use std::ops::{Add, Mul, Neg, Sub};
 use crate::matrices::Mat3;
 
 #[derive(Clone, Copy, PartialEq, Debug, Default)]
-pub struct Vector2D {
-    pub x: f64,
-    pub y: f64,
-}
+pub struct Vec2(pub f64, pub f64);
 
-pub fn vec2(x: f64, y: f64) -> Vector2D {
-    Vector2D { x, y }
-}
-
-impl Add for Vector2D {
+impl Add for Vec2 {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+        let Vec2(x1, y1) = self;
+        let Vec2(x2, y2) = other;
+        Self(x1 + x2, y1 + y2)
     }
 }
 
-impl Sub for Vector2D {
+impl Sub for Vec2 {
     type Output = Self;
     fn sub(self, other: Self) -> Self {
-        Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        }
+        let Vec2(x1, y1) = self;
+        let Vec2(x2, y2) = other;
+        Self(x1 - x2, y1 - y2)
     }
 }
-// lhs: Vector2D * rhs: f64 -> Vector2D
-impl Mul<f64> for Vector2D {
+
+// lhs: Vec2 * rhs: f64 -> Vec2
+impl Mul<f64> for Vec2 {
     type Output = Self;
     fn mul(self, rhs: f64) -> Self {
-        Self {
-            x: self.x * rhs,
-            y: self.y * rhs,
-        }
-    }
-}
-// lhs: f64 * rhs: Vector2D -> Vector2D
-impl Mul<Vector2D> for f64 {
-    type Output = Vector2D;
-    fn mul(self, rhs: Vector2D) -> Self::Output {
-        Self::Output {
-            x: self * rhs.x,
-            y: self * rhs.y,
-        }
+        let Self(x, y) = self;
+        Self(x * rhs, y * rhs)
     }
 }
 
-impl Neg for Vector2D {
+// lhs: f64 * rhs: Vec2 -> Vec2
+impl Mul<Vec2> for f64 {
+    type Output = Vec2;
+    fn mul(self, rhs: Vec2) -> Vec2 {
+        let Vec2(x, y) = rhs;
+        Vec2(self * x, self * y)
+    }
+}
+
+impl Neg for Vec2 {
     type Output = Self;
     fn neg(self) -> Self {
-        Self {
-            x: -self.x,
-            y: -self.y,
-        }
+        let Self(x, y) = self;
+        Self(-x, -y)
     }
 }
 
-impl<'a> Sum<&'a Self> for Vector2D {
+impl<'a> Sum<&'a Self> for Vec2 {
     fn sum<I>(iter: I) -> Self
     where
         I: Iterator<Item = &'a Self>,
     {
-        iter.fold(vec2(0.0, 0.0), |accum, el| Self {
-            x: accum.x + el.x,
-            y: accum.y + el.y,
+        iter.fold(Vec2(0.0, 0.0), |accum, el| {
+            Self(accum.0 + el.0, accum.1 + el.1)
         })
     }
 }
 
-impl Vector2D {
+impl Vec2 {
     pub fn abs(self) -> Self {
-        Self {
-            x: self.x.abs(),
-            y: self.y.abs(),
-        }
+        let Self(x, y) = self;
+        Self(x.abs(), y.abs())
     }
     pub fn invert(self) -> Self {
-        Self {
-            x: 1.0 / self.x,
-            y: 1.0 / self.y,
-        }
+        let Self(x, y) = self;
+        Self(1.0 / x, 1.0 / y)
     }
     // Returns the magnitude of a vector.
     pub fn mag(self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+        let Self(x, y) = self;
+        (x * x + y * y).sqrt()
     }
 
     pub fn scale(self, scalar: f64) -> Self {
-        Self {
-            x: self.x * scalar,
-            y: self.y * scalar,
-        }
+        let Self(x, y) = self;
+        Self(x * scalar, y * scalar)
     }
     // Dot product.
     pub fn dot(self, other: Self) -> f64 {
-        self.x * other.x + self.y * other.y
+        let Self(x1, y1) = self;
+        let Self(x2, y2) = other;
+        x1 * x2 + y1 * y2
     }
     // Calculates the distance between the tips of two vectors.
     pub fn distance(self, other: Self) -> f64 {
@@ -114,136 +97,101 @@ impl Vector2D {
 
     pub fn normalize(self) -> Self {
         let mag = self.mag();
-        Self {
-            x: self.x / mag,
-            y: self.y / mag,
-        }
+        let Self(x, y) = self;
+        Self(x / mag, y / mag)
     }
 
     pub fn round(self) -> Self {
-        Self {
-            x: self.x.round(),
-            y: self.y.round(),
-        }
+        let Self(x, y) = self;
+        Self(x.round(), y.round())
     }
 
     pub fn transform_by(self, m: Mat3) -> Self {
-        Self {
-            x: m.a * self.x + m.c * self.y,
-            y: m.b * self.x + m.d * self.y,
-        }
+        let Self(x, y) = self;
+        Self(m.a * x + m.c * y, m.b * x + m.d * y)
     }
 }
 
 #[derive(Clone, Copy, PartialEq, Debug, Default)]
-pub struct Vector3D {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-}
+pub struct Vec3(pub f64, pub f64, pub f64);
 
-pub fn vec3(x: f64, y: f64, z: f64) -> Vector3D {
-    Vector3D { x, y, z }
-}
-
-impl Add for Vector3D {
+impl Add for Vec3 {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-        }
+        let Self(x1, y1, z1) = self;
+        let Self(x2, y2, z2) = other;
+        Self(x1 + x2, y1 + y2, z1 + z2)
     }
 }
 
-impl Sub for Vector3D {
+impl Sub for Vec3 {
     type Output = Self;
     fn sub(self, other: Self) -> Self {
-        Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
-            z: self.z - other.z,
-        }
+        let Self(x1, y1, z1) = self;
+        let Self(x2, y2, z2) = other;
+        Self(x1 - x2, y1 - y2, z1 - z2)
     }
 }
-// lhs: Vector3D * rhs: f64 -> Vector3D
-impl Mul<f64> for Vector3D {
+// lhs: Vec3 * rhs: f64 -> Vec3
+impl Mul<f64> for Vec3 {
     type Output = Self;
     fn mul(self, rhs: f64) -> Self {
-        Self {
-            x: self.x * rhs,
-            y: self.y * rhs,
-            z: self.z * rhs,
-        }
+        let Self(x, y, z) = self;
+        Self(x * rhs, y * rhs, z * rhs)
     }
 }
-// lhs: f64 * rhs: Vector3D -> Vector3D
-impl Mul<Vector3D> for f64 {
-    type Output = Vector3D;
-    fn mul(self, rhs: Vector3D) -> Self::Output {
-        Self::Output {
-            x: self * rhs.x,
-            y: self * rhs.y,
-            z: self * rhs.z,
-        }
+// lhs: f64 * rhs: Vec3 -> Vec3
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        let Vec3(x, y, z) = rhs;
+        Vec3(self * x, self * y, self * z)
     }
 }
 
-impl Neg for Vector3D {
+impl Neg for Vec3 {
     type Output = Self;
     fn neg(self) -> Self {
-        Self {
-            x: -self.x,
-            y: -self.y,
-            z: -self.z,
-        }
+        let Self(x, y, z) = self;
+        Self(-x, -y, -z)
     }
 }
 
-impl<'a> Sum<&'a Self> for Vector3D {
+impl<'a> Sum<&'a Self> for Vec3 {
     fn sum<I>(iter: I) -> Self
     where
         I: Iterator<Item = &'a Self>,
     {
-        iter.fold(vec3(0.0, 0.0, 0.0), |accum, el| Self {
-            x: accum.x + el.x,
-            y: accum.y + el.y,
-            z: accum.z + el.z,
+        iter.fold(Vec3(0.0, 0.0, 0.0), |accum, el| {
+            Self(accum.0 + el.0, accum.1 + el.1, accum.2 + el.2)
         })
     }
 }
 
-impl Vector3D {
+impl Vec3 {
     pub fn abs(self) -> Self {
-        Self {
-            x: self.x.abs(),
-            y: self.y.abs(),
-            z: self.z.abs(),
-        }
+        let Self(x, y, z) = self;
+        Self(x.abs(), y.abs(), z.abs())
     }
     pub fn invert(self) -> Self {
-        Self {
-            x: 1.0 / self.x,
-            y: 1.0 / self.y,
-            z: 1.0 / self.z,
-        }
+        let Self(x, y, z) = self;
+        Self(1.0 / x, 1.0 / y, 1.0 / z)
     }
     // Returns the magnitude of a vector.
     pub fn mag(self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+        let Self(x, y, z) = self;
+        (x.powi(2) + y.powi(2) + z.powi(2)).sqrt()
     }
 
     pub fn scale(self, scalar: f64) -> Self {
-        Self {
-            x: self.x * scalar,
-            y: self.y * scalar,
-            z: self.z * scalar,
-        }
+        let Self(x, y, z) = self;
+        Self(x * scalar, y * scalar, z * scalar)
     }
     // Dot product.
     pub fn dot(self, other: Self) -> f64 {
-        self.x * other.x + self.y * other.y + self.z * other.z
+        let Self(x1, y1, z1) = self;
+        let Self(x2, y2, z2) = other;
+        x1 * x2 + y1 * y2 + z1 * z2
     }
     // Calculates the distance between the tips of two vectors.
     pub fn distance(self, other: Self) -> f64 {
@@ -256,93 +204,78 @@ impl Vector3D {
 
     pub fn normalize(self) -> Self {
         let mag = self.mag();
-        Self {
-            x: self.x / mag,
-            y: self.y / mag,
-            z: self.z / mag,
-        }
+        let Self(x, y, z) = self;
+        Self(x / mag, y / mag, z / mag)
     }
 
     pub fn round(self) -> Self {
-        Self {
-            x: self.x.round(),
-            y: self.y.round(),
-            z: self.z.round(),
-        }
+        let Self(x, y, z) = self;
+        Self(x.round(), y.round(), z.round())
     }
 }
 
 #[cfg(test)]
 mod vec2_tests {
-    use crate::{
-        matrices::Mat3,
-        vectors::{vec2, Vector2D},
-    };
+    use crate::{matrices::Mat3, vectors::Vec2};
     use std::vec;
     #[test]
     fn test_default() {
-        let v = Vector2D::default();
-        assert_eq!(v, vec2(0.0, 0.0));
+        let v = Vec2::default();
+        assert_eq!(v, Vec2(0.0, 0.0));
     }
     #[test]
     fn test_equal() {
-        let v1 = vec2(3.0, 4.0);
-        let v2 = vec2(3.0, 4.0);
+        let v1 = Vec2(3.0, 4.0);
+        let v2 = Vec2(3.0, 4.0);
         assert_eq!(v1, v2);
     }
     #[test]
-    fn test_vec2() {
-        let expect = Vector2D { x: 3.0, y: 4.0 };
-        let result = vec2(3.0, 4.0);
-        assert_eq!(result, expect);
-    }
-    #[test]
     fn test_add() {
-        let expect = vec2(5.0, 7.0);
-        let result = vec2(3.0, 4.0) + vec2(1.0, 2.0) + vec2(1.0, 1.0);
+        let expect = Vec2(5.0, 7.0);
+        let result = Vec2(3.0, 4.0) + Vec2(1.0, 2.0) + Vec2(1.0, 1.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_sub() {
-        let expect = vec2(2.0, 2.0);
-        let result = vec2(3.0, 4.0) - vec2(1.0, 2.0);
+        let expect = Vec2(2.0, 2.0);
+        let result = Vec2(3.0, 4.0) - Vec2(1.0, 2.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_mul() {
-        let expect = vec2(6.0, 8.0);
-        let result = vec2(3.0, 4.0) * 2_f64;
+        let expect = Vec2(6.0, 8.0);
+        let result = Vec2(3.0, 4.0) * 2.0;
         assert_eq!(expect, result);
-        let result = 2_f64 * vec2(3.0, 4.0);
+        let result = 2.0 * Vec2(3.0, 4.0);
         assert_eq!(expect, result);
     }
     #[test]
     fn test_neg() {
-        let expect = vec2(3.0, -4.0);
-        let result = -vec2(-3.0, 4.0);
+        let expect = Vec2(3.0, -4.0);
+        let result = -Vec2(-3.0, 4.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_abs() {
-        let expect = vec2(3.0, 4.0);
-        let result = vec2(-3.0, -4.0).abs();
+        let expect = Vec2(3.0, 4.0);
+        let result = Vec2(-3.0, -4.0).abs();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_invert() {
-        let expect = vec2(0.5, 0.5);
-        let result = vec2(2.0, 2.0).invert();
+        let expect = Vec2(0.5, 0.5);
+        let result = Vec2(2.0, 2.0).invert();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_sum() {
-        let array_expect = vec2(6.0, 8.0);
-        let array_result: Vector2D = [vec2(1.0, 2.0), vec2(3.0, 4.0), vec2(2.0, 2.0)]
+        let array_expect = Vec2(6.0, 8.0);
+        let array_result: Vec2 = [Vec2(1.0, 2.0), Vec2(3.0, 4.0), Vec2(2.0, 2.0)]
             .iter()
             .sum();
         assert_eq!(array_result, array_expect);
-        let vector_expect = vec2(6.0, 8.0);
-        let vector_result: Vector2D = vec![vec2(1.0, 2.0), vec2(3.0, 4.0), vec2(2.0, 2.0)]
+        let vector_expect = Vec2(6.0, 8.0);
+        let vector_result: Vec2 = vec![Vec2(1.0, 2.0), Vec2(3.0, 4.0), Vec2(2.0, 2.0)]
             .iter()
             .sum();
         assert_eq!(vector_result, vector_expect);
@@ -350,134 +283,124 @@ mod vec2_tests {
     #[test]
     fn test_mag() {
         let expect = 5.0;
-        let result = vec2(3.0, 4.0).mag();
+        let result = Vec2(3.0, 4.0).mag();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_scale() {
-        let expect = vec2(6.0, 8.0);
-        let result = vec2(3.0, 4.0).scale(2.0);
+        let expect = Vec2(6.0, 8.0);
+        let result = Vec2(3.0, 4.0).scale(2.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_dot() {
-        let v1 = vec2(1.0, 2.0);
-        let v2 = vec2(3.0, 4.0);
+        let v1 = Vec2(1.0, 2.0);
+        let v2 = Vec2(3.0, 4.0);
         let result = v1.dot(v2);
         assert_eq!(result, 11.0);
     }
     #[test]
     fn test_distance() {
         let expect = 7.0;
-        let result = vec2(8.0, 0.0).distance(vec2(1.0, 0.0));
+        let result = Vec2(8.0, 0.0).distance(Vec2(1.0, 0.0));
         assert_eq!(result, expect);
     }
     #[test]
     fn test_lerp() {
-        let expect = vec2(2.0, 1.5);
-        let result = vec2(1.0, 1.0).lerp(vec2(3.0, 2.0), 0.5);
+        let expect = Vec2(2.0, 1.5);
+        let result = Vec2(1.0, 1.0).lerp(Vec2(3.0, 2.0), 0.5);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_normalize() {
-        let expect = vec2(0.6, 0.8);
-        let result = vec2(3.0, 4.0).normalize();
+        let expect = Vec2(0.6, 0.8);
+        let result = Vec2(3.0, 4.0).normalize();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_round() {
-        let expect = vec2(0.0, 7.0);
-        let result = vec2(0.25, 6.73).round();
+        let expect = Vec2(0.0, 7.0);
+        let result = Vec2(0.25, 6.73).round();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_transform_by() {
         let mat = Mat3::identity().scale(10.0, 10.0);
-        let expect = vec2(30.0, 40.0);
-        let result = vec2(3.0, 4.0).transform_by(mat);
+        let expect = Vec2(30.0, 40.0);
+        let result = Vec2(3.0, 4.0).transform_by(mat);
         assert_eq!(result, expect);
     }
 }
 
 #[cfg(test)]
 mod vec3_tests {
-    use crate::vectors::{vec3, Vector3D};
+    use crate::vectors::Vec3;
 
     #[test]
     fn test_default() {
-        let v = Vector3D::default();
-        assert_eq!(v, vec3(0.0, 0.0, 0.0));
+        let v = Vec3::default();
+        assert_eq!(v, Vec3(0.0, 0.0, 0.0));
     }
     #[test]
     fn test_equal() {
-        let v1 = vec3(3.0, 4.0, 5.0);
-        let v2 = vec3(3.0, 4.0, 5.0);
+        let v1 = Vec3(3.0, 4.0, 5.0);
+        let v2 = Vec3(3.0, 4.0, 5.0);
         assert_eq!(v1, v2);
     }
     #[test]
-    fn test_vec3() {
-        let expect = Vector3D {
-            x: 3.0,
-            y: 4.0,
-            z: 5.0,
-        };
-        let result = vec3(3.0, 4.0, 5.0);
-        assert_eq!(result, expect);
-    }
-    #[test]
     fn test_add() {
-        let expect = vec3(5.0, 7.0, 7.0);
-        let result = vec3(3.0, 4.0, 2.0) + vec3(1.0, 2.0, 2.0) + vec3(1.0, 1.0, 3.0);
+        let expect = Vec3(5.0, 7.0, 7.0);
+        let result = Vec3(3.0, 4.0, 2.0) + Vec3(1.0, 2.0, 2.0) + Vec3(1.0, 1.0, 3.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_sub() {
-        let expect = vec3(2.0, 2.0, 2.0);
-        let result = vec3(3.0, 4.0, 5.0) - vec3(1.0, 2.0, 3.0);
+        let expect = Vec3(2.0, 2.0, 2.0);
+        let result = Vec3(3.0, 4.0, 5.0) - Vec3(1.0, 2.0, 3.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_mul() {
-        let expect = vec3(10.0, 8.0, 6.0);
-        let result = vec3(5.0, 4.0, 3.0) * 2_f64;
+        let expect = Vec3(10.0, 8.0, 6.0);
+        let result = Vec3(5.0, 4.0, 3.0) * 2.0;
         assert_eq!(expect, result);
-        let result = 2_f64 * vec3(5.0, 4.0, 3.0);
+        let result = 2.0 * Vec3(5.0, 4.0, 3.0);
         assert_eq!(expect, result);
     }
     #[test]
     fn test_neg() {
-        let expect = vec3(3.0, -4.0, 5.0);
-        let result = -vec3(-3.0, 4.0, -5.0);
+        let expect = Vec3(3.0, -4.0, 5.0);
+        let result = -Vec3(-3.0, 4.0, -5.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_abs() {
-        let expect = vec3(3.0, 4.0, 5.0);
-        let result = vec3(-3.0, 4.0, -5.0).abs();
+        let expect = Vec3(3.0, 4.0, 5.0);
+        let result = Vec3(-3.0, 4.0, -5.0).abs();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_invert() {
-        let expect = vec3(0.5, 0.5, 0.25);
-        let result = vec3(2.0, 2.0, 4.0).invert();
+        let expect = Vec3(0.5, 0.5, 0.25);
+        let result = Vec3(2.0, 2.0, 4.0).invert();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_sum() {
-        let array_expect = vec3(6.0, 8.0, 3.0);
-        let array_result: Vector3D = [
-            vec3(1.0, 2.0, 1.0),
-            vec3(3.0, 4.0, 1.0),
-            vec3(2.0, 2.0, 1.0),
+        let array_expect = Vec3(6.0, 8.0, 3.0);
+        let array_result: Vec3 = [
+            Vec3(1.0, 2.0, 1.0),
+            Vec3(3.0, 4.0, 1.0),
+            Vec3(2.0, 2.0, 1.0),
         ]
         .iter()
         .sum();
         assert_eq!(array_result, array_expect);
-        let vector_expect = vec3(6.0, 8.0, 15.0);
-        let vector_result: Vector3D = vec![
-            vec3(1.0, 2.0, 5.0),
-            vec3(3.0, 4.0, 5.0),
-            vec3(2.0, 2.0, 5.0),
+        let vector_expect = Vec3(6.0, 8.0, 15.0);
+        let vector_result: Vec3 = vec![
+            Vec3(1.0, 2.0, 5.0),
+            Vec3(3.0, 4.0, 5.0),
+            Vec3(2.0, 2.0, 5.0),
         ]
         .iter()
         .sum();
@@ -486,44 +409,44 @@ mod vec3_tests {
     #[test]
     fn test_mag() {
         let expect = 7.0710678118654755;
-        let result = vec3(3.0, 4.0, 5.0).mag();
+        let result = Vec3(3.0, 4.0, 5.0).mag();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_scale() {
-        let expect = vec3(6.0, 8.0, 10.0);
-        let result = vec3(3.0, 4.0, 5.0).scale(2.0);
+        let expect = Vec3(6.0, 8.0, 10.0);
+        let result = Vec3(3.0, 4.0, 5.0).scale(2.0);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_dot() {
-        let v1 = vec3(1.0, 2.0, 3.0);
-        let v2 = vec3(3.0, 4.0, 5.0);
+        let v1 = Vec3(1.0, 2.0, 3.0);
+        let v2 = Vec3(3.0, 4.0, 5.0);
         let result = v1.dot(v2);
         assert_eq!(result, 26.0);
     }
     #[test]
     fn test_distance() {
         let expect = 7.0;
-        let result = vec3(8.0, 0.0, 0.0).distance(vec3(1.0, 0.0, 0.0));
+        let result = Vec3(8.0, 0.0, 0.0).distance(Vec3(1.0, 0.0, 0.0));
         assert_eq!(result, expect);
     }
     #[test]
     fn test_lerp() {
-        let expect = vec3(2.0, 1.5, 1.0);
-        let result = vec3(1.0, 1.0, 1.0).lerp(vec3(3.0, 2.0, 1.0), 0.5);
+        let expect = Vec3(2.0, 1.5, 1.0);
+        let result = Vec3(1.0, 1.0, 1.0).lerp(Vec3(3.0, 2.0, 1.0), 0.5);
         assert_eq!(result, expect);
     }
     #[test]
     fn test_normalize() {
-        let expect = vec3(0.4242640687119285, 0.565685424949238, 0.7071067811865475);
-        let result = vec3(3.0, 4.0, 5.0).normalize();
+        let expect = Vec3(0.4242640687119285, 0.565685424949238, 0.7071067811865475);
+        let result = Vec3(3.0, 4.0, 5.0).normalize();
         assert_eq!(result, expect);
     }
     #[test]
     fn test_round() {
-        let result = vec3(0.25, 6.73, 0.5).round();
-        let expect = vec3(0.0, 7.0, 1.0);
+        let result = Vec3(0.25, 6.73, 0.5).round();
+        let expect = Vec3(0.0, 7.0, 1.0);
         assert_eq!(result, expect);
     }
 }
