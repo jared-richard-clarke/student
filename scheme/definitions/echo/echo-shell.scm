ae360f2d51cd789413f682ed4a8d9d6faf908f34
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-30 11:59:49 -0800

    Create echo-shell.scm

---
+++ scheme/definitions/echo/echo-shell.scm
@@ -0,0 +1,14 @@
+#! /usr/bin/scheme --script
+
+;; Program treated as a shell script.
+
+(let ([args (cdr (command-line))])
+  (unless (null? args)
+    (let-values ([(newline? args)
+                  (if (equal? (car args) "-n")
+                      (values #f (cdr args))
+                      (values #t args))])
+      (do ([args args (cdr args)] [sep "" " "])
+          ((null? args))
+        (printf "~a~a" sep (car args)))
+      (when newline? (newline)))))
