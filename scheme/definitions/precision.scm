1e5c44d32a09bf63cf9e1737ba57ff9a66586b50
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-03 22:35:42 -0800

    Add brackets for clarity

--- scheme/definitions/precision.scm
+++ scheme/definitions/precision.scm
@@ -4,6 +4,6 @@
 ;; — R6RS Specification
 
 (define (precision)
-  (do ((n 0 (+ n 1))
-       (x 1.0 (/ x 2.0)))
-    ((= 1.0 (+ 1.0 x)) n)))
+  (do ([n 0 (+ n 1)]
+       [x 1.0 (/ x 2.0)])
+    [(= 1.0 (+ 1.0 x)) n]))
