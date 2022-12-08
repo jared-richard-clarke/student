b5b2bdc66047d075ba53374a1cc1a488d39ce47c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-08 14:33:22 -0800

    Rename iter, loop

--- scheme/macros/factorial/factorial-def.scm
+++ scheme/macros/factorial/factorial-def.scm
@@ -1,42 +1,42 @@
 (define (factorial n)
-  (define (iter product operand)
+  (define (loop product operand)
     (if (< operand 1)
         product
-        (iter (* product operand)
+        (loop (* product operand)
               (- operand 1))))
-  (iter 1 n))
+  (loop 1 n))
 
 ; === letrec* expansion ===
 (define factorial-letrec*
   (lambda (n)
-    (letrec* ([iter (lambda (product operand)
+    (letrec* ([loop (lambda (product operand)
                       (if (< operand 1)
                           product
-                          (iter (* product operand)
+                          (loop (* product operand)
                                 (- operand 1))))])
-             (iter 1 n))))
+             (loop 1 n))))
 
 ; === let expansion ===
 (define factorial-let
   (lambda (n)
-    (let ([iter #f])
-      (set! iter (lambda (product operand)
+    (let ([loop #f])
+      (set! loop (lambda (product operand)
                    (if (< operand 1)
                        product
-                       (iter (* product operand)
+                       (loop (* product operand)
                              (- operand 1)))))
       (let ()
-        (iter 1 n)))))
+        (loop 1 n)))))
 
 ; === lambda expansion ===
 (define factorial-lambda
   (lambda (n)
-    ((lambda (iter)
-       (set! iter (lambda (product operand)
+    ((lambda (loop)
+       (set! loop (lambda (product operand)
                     (if (< operand 1)
                         product
-                        (iter (* product operand)
+                        (loop (* product operand)
                               (- operand 1)))))
        ((lambda ()
-          (iter 1 n))))
+          (loop 1 n))))
      #f)))
