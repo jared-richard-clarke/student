8cb08f36e5e0d5a4b61529db1da2190e7b541e55
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-08 22:27:02 -0800

    Prune

--- scheme/definitions/assertions.scm
+++ scheme/definitions/assertions.scm
@@ -1,17 +1,3 @@
-;; (assert-eq expression value) -> boolean
-;; Asserts expression evaluates to value.
-;; (assert-eq (list 1 2 3) '(1 2 3)) -> true
-
-(define (assert-eq expression value)
-  (equal? expression value))
-
-;; (assert-ne expression value) -> boolean
-;; Asserts expression does not evaluate to value.
-;; (assert-ne '(1 (2 3)) '(1 2 3)) -> true
-
-(define (assert-ne expression value)
-  (not (equal? expression value)))
-
 ;; (assert-equal expression value) -> current-output-port
 ;; If expression does not evaluate to value, macro prints failed test to current-output-port.
 ;; (assert-equal (vec2 4 4) #(3 4)) ->
