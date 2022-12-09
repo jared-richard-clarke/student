16dacc0da763f42063932d056b6a8bcc82b2eabe
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-08 22:25:35 -0800

    Revise documentation, use fold-left

--- scheme/definitions/monoid.scm
+++ scheme/definitions/monoid.scm
@@ -1,11 +1,12 @@
 ;; monoid: a set equipped with an associative binary operation and an identity element
 
+;; (monoid function any) -> function
+;; Intended as a factory function for producing monoid functions.
+;; (define sum (monoid + 0)) -> (sum 1 2 3 4 5) -> 15
+
 (define (monoid operation identity)
   (lambda operands
-    (foldl (lambda (operand total)
-             (operation total operand))
-           identity
-           operands)))
-
-(define sum (monoid + 0))
-(define product (monoid * 1))
+    (fold-left (lambda (total operand)
+                 (operation total operand))
+               identity
+               operands)))
