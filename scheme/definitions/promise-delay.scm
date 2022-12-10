cb0c60621eafbe799399fb2e7c6448ecda3594df
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-09 19:54:22 -0800

    Create promise-delay.scm

---
+++ scheme/definitions/promise-delay.scm
@@ -0,0 +1,26 @@
+;; "The syntactic form 'delay' and the procedure 'force' may be used in combination
+;;  to implement lazy evaluation. An expression subject to lazy evaluation is not
+;;  evaluated until its value is required and, once evaluated, is never reevaluated."
+;;
+;; — The Scheme Programming Language, chapter 5.7, by R. Kent Dybvig
+
+;; Side Note: These are example implementations. 'promise' and 'delay'
+;; are most likely implemented differently.
+
+(define-syntax delay
+  (syntax-rules ()
+    [(_ expr) (promise (lambda () expr))]))
+
+(define (promise fn)
+  (let ([value #f] [set? #f])
+    (lambda ()
+      (unless set?
+        (let ([x (fn)])
+          (unless set?
+            (set! value x)
+            (set! set? #t))))
+      value)))
+
+(define force
+  (lambda (promise)
+    (promise)))
