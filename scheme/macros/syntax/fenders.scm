217aec3c3119a3a5f5bef4151f81f0179618c13d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-11 20:52:14 -0800

    Update fenders.scm

--- scheme/macros/syntax/fenders.scm
+++ scheme/macros/syntax/fenders.scm
@@ -1,14 +1,11 @@
-;; This syntax definition of 'let' uses the fender 'ids?'
-;; to ensure each binding uses a proper identifier.
+;; This syntax definition of 'let' uses the fender 'identifier?'
+;; the named 'let' uses a proper identifier.
 
 (define-syntax let
   (lambda (x)
-    (define ids?
-      (lambda (lst)
-        (or (null? lst)
-            (and (identifier? (car lst))
-                 (ids? (cdr lst))))))
     (syntax-case x ()
-      [(_ ((i e) ...) b1 b2 ...)
-       (ids? #'(i ...))
-       #'((lambda (i ...) b1 b2 ...) e ...)])))
+      [(_ ((x e) ...) b1 b2 ...)
+       #'((lambda (x ...) b1 b2 ...) e ...)]
+      [(_ f ((x e) ...) b1 b2 ...)
+       (identifier? #'f)
+       #'((rec f (lambda (x ...) b1 b2 ...)) e ...)])))
