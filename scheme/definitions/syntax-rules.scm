15e6fcb403c92098c17a856f9fe218eac25fab40
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-11 20:35:27 -0800

    Delete syntax-rules.scm

--- scheme/definitions/syntax-rules.scm
+++
@@ -1,9 +0,0 @@
-;; syntax-rules defined in terms of syntax-case.
-
-(define-syntax syntax-rules
-  (lambda (x)
-    (syntax-case x ()
-      [(_ (i ...) ((keyword . pattern) template) ...)
-       #'(lambda (x)
-           (syntax-case x (i ...)
-             [(_ . pattern) #'template] ...))])))
