f6a13cb92eae4bd96facd276a2e439af280fdf26
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-11 20:23:21 -0800

    Create syntax-rules.scm

---
+++ scheme/macros/or-syntax/syntax-rules.scm
@@ -0,0 +1,9 @@
+;; The 'or' macro as defined by 'syntax-rules'
+
+(define-syntax or
+  (syntax-rules ()
+    [(_) #f]
+    [(_ e) e]
+    [(_ e1 e2 e3 ...)
+     (let ([t e1])
+       (if t t (or e2 e3 ...)))]))
