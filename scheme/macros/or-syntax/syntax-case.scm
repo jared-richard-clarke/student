553980d1b6feac6f541ce89170225010edae6823
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-12 16:06:14 -0800

    Replace '#, with syntax

--- scheme/macros/or-syntax/syntax-case.scm
+++ scheme/macros/or-syntax/syntax-case.scm
@@ -1,12 +1,12 @@
-;; The 'or' macro as defined by 'syntax-case' and 'syntax' (#')
+;; The 'or' macro as defined by 'syntax-case' and 'syntax'
 
 (define-syntax or
   (lambda (x)
     (syntax-case x ()
-      [(_) #'#f]
-      [(_ e) #'e]
+      [(_) (syntax #f)]
+      [(_ e) (syntax e)]
       [(_ e1 e2 e3 ...)
-       #'(let ([t e1]) 
-           (if t 
-               t 
-               (or e2 e3 ...)))])))
+       (syntax (let ([t e1]) 
+                 (if t 
+                     t 
+                     (or e2 e3 ...))))])))
