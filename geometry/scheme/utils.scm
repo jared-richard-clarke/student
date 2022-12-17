8d1f700c8d3dc65db8000735973ac940f3773019
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-16 18:31:42 -0800

    Rename EPSILON, TOLERANCE

--- geometry/scheme/utils.scm
+++ geometry/scheme/utils.scm
@@ -39,14 +39,14 @@
 
          ;; (approx-eq? number number) -> boolean
          ;; Tests for approximate equality between two floating-point numbers within an absolute
-         ;; or relative tolerance of EPSILON. An absolute tolerance is used for values
+         ;; or relative tolerance of TOLERANCE. An absolute tolerance is used for values
          ;; less than or equal to 1.0. A relative tolerance is used for larger values.
          ;; (approx-eq? 0.2 0.19999999) -> #t
 
          (define (approx-eq? x y)
-           (let ([EPSILON 1e-7]) ;; <- arbitrary maximum allowable difference
+           (let ([TOLERANCE 1e-7]) ;; <- arbitrary maximum allowable difference
              (<= (abs (- x y))
-                 (* EPSILON 
+                 (* TOLERANCE 
                     (max 1.0 
                          (abs x)
                          (abs y))))))
