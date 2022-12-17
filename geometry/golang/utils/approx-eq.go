559c5780d90cf12f6b3f683fde276c500868d75c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-16 18:08:00 -0800

    Revise Epsilon constant

--- geometry/golang/utils/approx-eq.go
+++ geometry/golang/utils/approx-eq.go
@@ -2,12 +2,12 @@ package utils
 
 import "math"
 
-// The maximum allowable difference in precision between floating-point numbers.
-const epsilon = 0.000001
+// An arbitrary maximum allowable difference in precision between floating-point numbers.
+const Epsilon = 1e-07
 
 // Tests for approximate equality between two 64-bit, floating-point numbers within an absolute
 // or relative tolerance of Epsilon. An absolute tolerance is used for values
 // less than or equal to 1.0. A relative tolerance is used for larger values.
 func ApproxEq(x, y float64) bool {
-	return math.Abs(x-y) <= epsilon*math.Max(1.0, math.Max(math.Abs(x), math.Abs(y)))
+	return math.Abs(x-y) <= Epsilon*math.Max(1.0, math.Max(math.Abs(x), math.Abs(y)))
 }
