15bbb7df86bec22eda77c750417171302d7afe90
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-16 18:17:44 -0800

    Rename EPSILON, TOLERANCE

--- geometry/python/utils.py
+++ geometry/python/utils.py
@@ -1,11 +1,11 @@
-EPSILON = 1e-7
+TOLERANCE = 1e-7
 """An arbitrary maximum allowable difference in precision between floating-point numbers."""
 
 
 def equals(x, y):
     """
     Tests for approximate equality between two floating-point numbers within an absolute
-    or relative tolerance of EPSILON. An absolute tolerance is used for values
+    or relative tolerance of TOLERANCE. An absolute tolerance is used for values
     less than or equal to 1.0. A relative tolerance is used for larger values.
     """
-    return abs(x - y) <= (EPSILON * max(1.0, abs(x), abs(y)))
+    return abs(x - y) <= (TOLERANCE * max(1.0, abs(x), abs(y)))
