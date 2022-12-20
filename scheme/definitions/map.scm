4d601d1c1e767c3bfd198e5c86481efa80d109fc
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-19 20:05:23 -0800

    Rename inner functions

--- scheme/definitions/map.scm
+++ scheme/definitions/map.scm
@@ -2,23 +2,18 @@
 ;; Unlike the map-simple definition below, this implementation can iterate over multiple lists.
 ;; Both definitions omit error handling for simplificity.
 
-(define map
-  (lambda (action seq . more)
-    (if (null? more)
-        (let map-1 ([seq seq])
-          (if (null? seq)
-              '()
-              (cons (action (car seq))
-                    (map-1 (cdr seq)))))
-        (let map-more ([seq seq]
-                       [more more])
-          (if (null? seq)
-              '()
-              (cons (apply action
-                           (car seq)
-                           (map car more))
-                    (map-more (cdr seq)
-                              (map cdr more))))))))
+(define (map action seq . more)
+  (if (null? more)
+      (let map-seq ([seq seq])
+        (if (null? seq)
+            '()
+            (cons (action (car seq)) (map-seq (cdr seq)))))
+      (let map-more ([seq seq]
+                     [more more])
+        (if (null? seq)
+            '()
+            (cons (apply action (car seq) (map car more))
+                  (map-more (cdr seq) (map cdr more)))))))
 
 ;; (map-simple function list) -> list
 ;; Constructs a new sequence populated with the results of calling 
@@ -27,6 +22,6 @@
 
 (define (map-simple action sequence)
   (if (null? sequence)
-      null
+      '()
       (cons (action (car sequence))
             (map-simple action (cdr sequence)))))
