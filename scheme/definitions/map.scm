9439af1e7e919496ccf3d2ff2c9a86cf0313feea
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-19 20:17:48 -0800

    Rename formals

--- scheme/definitions/map.scm
+++ scheme/definitions/map.scm
@@ -2,26 +2,26 @@
 ;; Unlike the map-simple definition below, this implementation can iterate over multiple lists.
 ;; Both definitions omit error handling for simplificity.
 
-(define (map action seq . more)
-  (if (null? more)
-      (let map-seq ([seq seq])
-        (if (null? seq)
+(define (map fun x . xs)
+  (if (null? xs)
+      (let map-x ([x x])
+        (if (null? x)
             '()
-            (cons (action (car seq)) (map-seq (cdr seq)))))
-      (let map-more ([seq seq]
-                     [more more])
-        (if (null? seq)
+            (cons (fun (car x)) (map-x (cdr x)))))
+      (let map-xs ([x x]
+                   [xs xs])
+        (if (null? x)
             '()
-            (cons (apply action (car seq) (map car more))
-                  (map-more (cdr seq) (map cdr more)))))))
+            (cons (apply fun (car x) (map car xs))
+                  (map-xs (cdr x) (map cdr xs)))))))
 
 ;; (map-simple function list) -> list
 ;; Constructs a new sequence populated with the results of calling 
 ;; the provided function on each element of the provided sequence.
 ;; (map-simple add-1 '(1 2 3)) -> '(2 3 4)
 
-(define (map-simple action sequence)
-  (if (null? sequence)
+(define (map-simple fun x)
+  (if (null? x)
       '()
-      (cons (action (car sequence))
-            (map-simple action (cdr sequence)))))
+      (cons (fun (car x))
+            (map-simple fun (cdr x)))))
