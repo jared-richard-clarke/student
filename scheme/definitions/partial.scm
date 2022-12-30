aabb46657cbe039c4e958cbef51c070efe47276b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-29 21:26:01 -0800

    Revise doc comment

--- scheme/definitions/partial.scm
+++ scheme/definitions/partial.scm
@@ -1,7 +1,7 @@
 ;; (partial function values ...) -> (function values ...) -> value
 ;; Fixes a number of arguments to a function, producing another function with a smaller arity.
 ;; Applies function to combined arguments.
-;; (define add1 (partial + 1)) -> (add1 10) -> 11
+;; (define add1 (partial + 1)) -> (add1 10 10) -> 21
 
 (define (partial fn . x)
   (lambda y
