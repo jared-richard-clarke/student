1947348d603d24b8f61805fcb1ff586eb717f973
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-29 21:00:03 -0800

    Use more explicit syntax

--- scheme/definitions/quicksort.scm
+++ scheme/definitions/quicksort.scm
@@ -2,11 +2,13 @@
 ;; Sorts a list of numbers from least to greatest using the quick-sort algorithm.
 ;; (quick-sort '(3 -4 11 2 11)) -> '(-4 2 3 11 11)
 
-(define (le x)
-  (lambda (y) (<= y x)))
+(define le
+  (lambda (x)
+    (lambda (y) (<= y x))))
 
-(define (gt x)
-  (lambda (y) (> y x)))
+(define gt
+  (lambda (x)
+    (lambda (y) (> y x))))
 
 (define (quick-sort lst)
   (if (null? lst)
