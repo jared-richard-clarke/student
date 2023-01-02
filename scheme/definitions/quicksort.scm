c171aec4bc5bbdab64da76d9808a62d10c08efce
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-01-02 11:53:51 -0800

    Parameterize comparison

--- scheme/definitions/quicksort.scm
+++ scheme/definitions/quicksort.scm
@@ -1,12 +1,16 @@
-;; (quick-sort (list number ...)) -> (list number ...)
-;; Sorts a list of numbers from least to greatest using the quick-sort algorithm.
-;; (quick-sort '(3 -4 11 2 11)) -> '(-4 2 3 11 11)
+;; (quick-sort function (list any ...)) -> (list any ...)
+;; Sorts a list of comparable items using the quick-sort algorithm.
+;; The "compare" function sorts items to the left of the pivot, whereas
+;; the the inverse of "compare" sorts to the right. Thereby "compare"
+;; determines ascending or descending order for elements in the list.
+;; (quick-sort < '(3 -4 11 2 11)) ---------> '(-4 2 3 11 11)
+;; (quick-sort char<? '(#\b #\c #\a #\d)) -> '(#\a #\b #\c #\d)
 
-(define (quick-sort lst)
+(define (quick-sort compare lst)
   (if (null? lst)
       '()
       (let ([x  (car lst)]
             [xs (cdr lst)])
-        (let ([left  (quick-sort (filter (lambda (y) (<  y x)) xs))]
-              [right (quick-sort (filter (lambda (y) (>= y x)) xs))])
+        (let ([left  (quick-sort compare (filter (lambda (y) (compare y x)) xs))]
+              [right (quick-sort compare (filter (lambda (y) (not (compare y x))) xs))])
           (append left (list x) right)))))
