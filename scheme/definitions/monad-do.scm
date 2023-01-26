1604fb418e6ba42fa67cecff255dab32837fe00d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-01-25 22:59:18 -0800

    Create monad-do.scm

---
+++ scheme/definitions/monad-do.scm
@@ -0,0 +1,25 @@
+;; The Haskell "do" syntax (simplified). Makes monads readable.
+;; "bind" must be defined in order for this macro to work.
+;;
+;; (define and-then
+;;   (lambda (px py)
+;;     (do (x <- px)
+;;         (y <- py)
+;;        (return (cons x y)))))
+;;
+;; expands ->
+;;
+;; (define and-then
+;;   (lambda (px py)
+;;     (bind px (lambda (x)
+;;                (bind py (lambda (y)
+;;                           (return (cons x y))))))))
+
+(define-syntax do
+  (lambda (stx)
+    (syntax-case stx (<-)
+      [(_ expression)
+       (syntax expression)]
+      [(_ (x <- px) expression ...)
+       (syntax (bind px (lambda (x) 
+                          (do expression ...))))])))
