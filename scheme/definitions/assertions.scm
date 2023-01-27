8d5dd0f6173b814ce05625e38c032d79cc5689e8
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-01-26 18:38:26 -0800

    Add "assert-left-right"

--- scheme/definitions/assertions.scm
+++ scheme/definitions/assertions.scm
@@ -1,8 +1,8 @@
-;; (assert-test predicate expression value) -> current-output-port
-;; If expression does not evaluate to value, macro prints failed test to current-output-port.
+;; (assert-test predicate expression value) -> current-output-port | (void)
+;; If expression does not evaluate to value, macro prints failed test to the current-output-port.
 ;; (assert-test equal? (vec2 4 4) #(3 4)) ->
-;; Test: (vec2 4 4)
-;; Expect: #(3 4), Got: #(4 4)
+;; "Test: (vec2 4 4)
+;;  Expect: #(3 4), Got: #(4 4)"
 
 (define-syntax assert-test
   (lambda (stx)
@@ -14,3 +14,23 @@
                            (quote expression) ;; <---- returns expression prior to evaluation
                            value
                            computed-expr))))])))
+
+;; (assert-left-right predicate expression expression) -> current-output-port | (void)
+;; If the left-hand-expression does not equal the right-hand-expression,
+;; macro prints failed test to the current-output port.
+;; (assert-left-right equal? (+ 1 6) (- 1 6)) ->
+;; "Test failed:
+;;  rhs: (+ 1 6) -> 7, lhs: (- 1 6) -> -5"
+
+(define-syntax assert-left-right
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ compare x y)
+       (syntax (let ([computed-x x]
+                     [computed-y y])
+                 (unless (compare computed-x computed-y)
+                   (printf "Test failed:\nrhs: ~a -> ~a, lhs: ~a -> ~a\n"
+                           (quote x)
+                           x
+                           (quote y)
+                           y))))])))
