2f77395ad81c9e4abea0233f870299435634fb91
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-02 13:39:15 -0800

    Add span

--- scheme/definitions/take-drop.scm
+++ scheme/definitions/take-drop.scm
@@ -9,17 +9,6 @@
         '()
         (cons (car xs) (take (- n 1) (cdr xs))))))
 
-;; (take-while function (list any)) -> (list any)
-;; Returns the longest possible prefix list of list "xs"
-;; that satisfy predicate "test".
-;; (take-while odd? '(1 3 5 2 4)) -> '(1 3 5)
-
-(define take-while
-  (lambda (test xs)
-    (if (or (null? xs) (not (test (car xs))))
-        '()
-        (cons (car xs) (take-while test (cdr xs))))))
-
 ;; (drop number (list any)) -> (list any)
 ;; Drops the first "n" elements of list "xs". Drops the
 ;; whole list if "n" is greater than the length of "xs".
@@ -30,6 +19,26 @@
     (if (or (<= n 0) (null? xs))
         xs
         (drop (- n 1) (cdr xs)))))
+
+;; (split-at number (list any)) -> (list (list any) (list any))
+;; Returns a list of two lists where the first list is "xs" prefix
+;; of length "n" and the second list is the remainder.
+;; (split-at 2 '(1 2 3 4)) -> '((1 2) (3 4))
+
+(define split-at
+  (lambda (n xs)
+    (list (take n xs) (drop n xs))))
+
+;; (take-while function (list any)) -> (list any)
+;; Returns the longest possible prefix list of list "xs"
+;; that satisfy predicate "test".
+;; (take-while odd? '(1 3 5 2 4)) -> '(1 3 5)
+
+(define take-while
+  (lambda (test xs)
+    (if (or (null? xs) (not (test (car xs))))
+        '()
+        (cons (car xs) (take-while test (cdr xs))))))
 
 ;; (drop-while function (list any)) -> (list any)
 ;; Returns the remaining list of list "xs" that don't
@@ -42,11 +51,12 @@
         xs
         (drop-while test (cdr xs)))))
 
-;; (split-at number (list any)) -> (list (list any) (list any))
-;; Returns a list of two lists where the first list is "xs" prefix
-;; of length "n" and the second list is the remainder.
-;; (split-at 2 '(1 2 3 4)) -> '((1 2) (3 4))
+;; (span function (list any)) -> (list any)
+;; Returns a list of two lists where the first is the longest
+;; list of elements of "xs" that satisfy "test" and the second
+;; is the remainder.
+;; (span odd? '(1 3 5 2 4)) -> '((1 3 5) (2 4))
 
-(define split-at
-  (lambda (n xs)
-    (list (take n xs) (drop n xs))))
+(define span
+  (lambda (test xs)
+    (list (take-while test xs) (drop-while test xs))))
