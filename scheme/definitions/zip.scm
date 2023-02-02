10a194f1d7d1aba6274102cc245898c82844bcd5
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-02 13:52:52 -0800

    Add zip-with

--- scheme/definitions/zip.scm
+++ scheme/definitions/zip.scm
@@ -1,10 +1,19 @@
-;; (zip function list list) -> list
-;; Combines two lists, element by element. Returns when shortest list is exhausted.
-;; (zip cons '(one two three) '(1 2 3)) -> '((one . 1) (two . 2) (three . 3))
-;; (zip + '(1 2 3 4) '(1 2 3)) -> '(2 4 6)
+;; (zip (list any) (list any)) -> (list any)
+;; Combines two lists pairwise. Returns when the shortest list is exhausted.
+;; (zip '(one two three) '(1 2 3)) -> '((one . 1) (two . 2) (three . 3))
 
-(define (zip fn l1 l2)
-  (if (or (null? l1) (null? l2))
-      '()
-      (cons (fn (car l1) (car l2))
-            (zip fn (cdr l1) (cdr l2)))))
+(define zip
+  (lambda (xs ys)
+    (zip-with cons xs ys)))
+
+;; (zip-with function (list any) (list any)) -> (list any)
+;; Combines two lists pairwise, applying a function to each element pair.
+;; Returns when shortest list is exhausted.
+;; (zip-with + '(1 2 3 4) '(1 2 3)) -> '(2 4 6)
+
+(define zip-with
+  (lambda (fn xs ys)
+    (if (or (null? xs) (null? ys))
+        '()
+        (cons (fn (car xs) (car ys))
+              (zip-with fn (cdr xs) (cdr ys))))))
