4a7def950fa6acc59a6e9ada0c0d7a1b2342ca85
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-09 14:54:22 -0800

    Fix sum type signature, reformat

--- geometry/haskell/src/Vectors.hs
+++ geometry/haskell/src/Vectors.hs
@@ -26,60 +26,60 @@ import Prelude hiding (abs, negate, sum)
 -- definitions for "abs", "negate", and "sum"
 
 -- utils
-hypot :: Floating a => a -> a -> a
+hypot :: Floating t => t -> t -> t
 hypot x y = sqrt $ x ** 2 + y ** 2
 
 -- 2D vector
-data Vec2 a = Vec2 a a
+data Vec2 t = Vec2 t t
   deriving (Eq, Show, Read)
 
-add :: Num a => Vec2 a -> Vec2 a -> Vec2 a
+add :: Num t => Vec2 t -> Vec2 t -> Vec2 t
 add (Vec2 a b) (Vec2 c d) = Vec2 (a + c) (b + d)
 
-sub :: Num a => Vec2 a -> Vec2 a -> Vec2 a
+sub :: Num t => Vec2 t -> Vec2 t -> Vec2 t
 sub (Vec2 a b) (Vec2 c d) = Vec2 (a - c) (b - d)
 
-negate :: Num a => Vec2 a -> Vec2 a
+negate :: Num t => Vec2 t -> Vec2 t
 negate (Vec2 x y) = Vec2 (0 - x) (0 - y)
 
-abs :: (Num a, Ord a) => Vec2 a -> Vec2 a
+abs :: (Num t, Ord t) => Vec2 t -> Vec2 t
 abs (Vec2 x y) = Vec2 (absolute x) (absolute y)
   where
     absolute n = if n < 0 then 0 - n else n
 
-invert :: Fractional a => Vec2 a -> Vec2 a
+invert :: Fractional t => Vec2 t -> Vec2 t
 invert (Vec2 x y) = Vec2 (1 / x) (1 / y)
 
-sum :: (Foldable t, Num a) => p -> t (Vec2 a) -> Vec2 a
-sum xs = foldr1 add
+sum :: (Foldable s, Num t) => s (Vec2 t) -> Vec2 t
+sum = foldr1 add
 
-magnitude :: Floating a => Vec2 a -> a
+magnitude :: Floating t => Vec2 t -> t
 magnitude (Vec2 x y) = hypot x y
 
-scale :: Num a => Vec2 a -> a -> Vec2 a
+scale :: Num t => Vec2 t -> t -> Vec2 t
 scale (Vec2 x y) n = Vec2 (n * x) (n * y)
 
-dot :: Num a => Vec2 a -> Vec2 a -> a
+dot :: Num t => Vec2 t -> Vec2 t -> t
 dot (Vec2 a b) (Vec2 c d) = a * c + b * d
 
-distance :: Floating a => Vec2 a -> Vec2 a -> a
+distance :: Floating t => Vec2 t -> Vec2 t -> t
 distance (Vec2 a b) (Vec2 c d) =
   let x = c - a
       y = d - b
    in hypot x y
 
-lerp :: Num a => Vec2 a -> Vec2 a -> a -> Vec2 a
+lerp :: Num t => Vec2 t -> Vec2 t -> t -> Vec2 t
 lerp (Vec2 a b) (Vec2 c d) n = Vec2 (interp a c n) (interp b d n)
   where
     interp x y t = (y - x) * t + x
 
-normalize :: Floating a => Vec2 a -> Vec2 a
+normalize :: Floating t => Vec2 t -> Vec2 t
 normalize v =
   let Vec2 x y = v
       m = magnitude v
    in Vec2 (x / m) (y / m)
 
-transform :: Num a => Vec2 a -> Matrix a -> Vec2 a
+transform :: Num t => Vec2 t -> Matrix t -> Vec2 t
 transform v m =
   let Vec2 x y = v
       Mat3
