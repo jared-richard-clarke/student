a4b91450483f27f065f93e25359e8ed92c5847cf
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-09 14:37:24 -0800

    Generalize types

--- geometry/haskell/src/Vectors.hs
+++ geometry/haskell/src/Vectors.hs
@@ -1,93 +1,108 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use negate" #-}
-module Vectors
-  ( Vector (..),
-    add,
-    sub,
-    negate,
-    abs,
-    invert,
-    sum,
-    magnitude,
+module Matrices
+  ( Matrix (..),
+    multiply,
+    identity,
+    translate,
     scale,
-    dot,
-    distance,
-    lerp,
-    normalize,
-    transform,
+    rotate,
+    shear,
+    compose,
   )
 where
 
-import Matrices (Matrix (..))
-import Prelude hiding (abs, negate, sum)
-
--- There's probably a more elegant solution to creating my own
--- definitions for "abs", "negate", and "sum"
-
--- utils
-hypot :: Double -> Double -> Double
-hypot x y = sqrt $ x ** 2 + y ** 2
-
--- vectors
-data Vector = Vec2 Double Double
+data Matrix t = Mat3
+  { a :: t,
+    b :: t,
+    c :: t,
+    d :: t,
+    e :: t,
+    f :: t
+  }
   deriving (Eq, Show, Read)
 
-add :: Vector -> Vector -> Vector
-add (Vec2 a b) (Vec2 c d) = Vec2 (a + c) (b + d)
-
-sub :: Vector -> Vector -> Vector
-sub (Vec2 a b) (Vec2 c d) = Vec2 (a - c) (b - d)
-
-negate :: Vector -> Vector
-negate (Vec2 x y) = Vec2 (0 - x) (0 - y)
-
-abs :: Vector -> Vector
-abs (Vec2 x y) = Vec2 (absolute x) (absolute y)
-  where
-    absolute n = if n < 0 then 0 - n else n
-
-invert :: Vector -> Vector
-invert (Vec2 x y) = Vec2 (1 / x) (1 / y)
-
-sum :: [Vector] -> Vector
-sum = foldr1 add
-
-magnitude :: Vector -> Double
-magnitude (Vec2 x y) = hypot x y
-
-scale :: Vector -> Double -> Vector
-scale (Vec2 x y) n = Vec2 (n * x) (n * y)
-
-dot :: Vector -> Vector -> Double
-dot (Vec2 a b) (Vec2 c d) = a * c + b * d
-
-distance :: Vector -> Vector -> Double
-distance (Vec2 a b) (Vec2 c d) =
-  let x = c - a
-      y = d - b
-   in hypot x y
-
-lerp :: Vector -> Vector -> Double -> Vector
-lerp (Vec2 a b) (Vec2 c d) n = Vec2 (interp a c n) (interp b d n)
-  where
-    interp x y t = (y - x) * t + x
-
-normalize :: Vector -> Vector
-normalize v =
-  let Vec2 x y = v
-      m = magnitude v
-   in Vec2 (x / m) (y / m)
-
-transform :: Vector -> Matrix -> Vector
-transform v m =
-  let Vec2 x y = v
-      Mat3
-        { a = a,
-          b = b,
-          c = c,
-          d = d,
-          e = _,
-          f = _
+multiply :: Num t => Matrix t -> Matrix t -> Matrix t
+multiply m n =
+  let Mat3
+        { a = a1,
+          b = b1,
+          c = c1,
+          d = d1,
+          e = e1,
+          f = f1
         } = m
-   in Vec2 (a * x + c * y) (b * x + d * y)
+      Mat3
+        { a = a2,
+          b = b2,
+          c = c2,
+          d = d2,
+          e = e2,
+          f = f2
+        } = n
+   in Mat3
+        { a = a1 * a2 + b1 * c2,
+          b = a1 * b2 + b1 * d2,
+          c = c1 * a2 + d1 * c2,
+          d = c1 * b2 + d1 * d2,
+          e = e1 * a2 + f1 * c2 + e2,
+          f = e1 * b2 + f1 * d2 + f2
+        }
+
+identity :: Num t => Matrix t
+identity =
+  Mat3
+    { a = 1,
+      b = 0,
+      c = 0,
+      d = 1,
+      e = 0,
+      f = 0
+    }
+
+translate :: Num t => t -> t -> Matrix t
+translate x y =
+  Mat3
+    { a = 1,
+      b = 0,
+      c = 0,
+      d = 1,
+      e = x,
+      f = y
+    }
+
+scale :: Num t => t -> t -> Matrix t
+scale x y =
+  Mat3
+    { a = x,
+      b = 0,
+      c = 0,
+      d = y,
+      e = 0,
+      f = 0
+    }
+
+rotate :: Floating t => t -> Matrix t
+rotate angle =
+  let x = cos angle
+      y = sin angle
+   in Mat3
+        { a = x,
+          b = y,
+          c = negate x,
+          d = y,
+          e = 0,
+          f = 0
+        }
+
+shear :: Num t => t -> t -> Matrix t
+shear x y =
+  Mat3
+    { a = 1,
+      b = y,
+      c = x,
+      d = 1,
+      e = 0,
+      f = 0
+    }
+
+compose :: (Foldable t, Num a) => p -> t (Matrix a) -> Matrix a
+compose xs = foldr multiply identity
