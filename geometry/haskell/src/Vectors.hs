0c22a5b6bf516a03a2b379697b427a6d184f53a4
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-10 00:58:40 -0800

    Implement Vec2 as an Applicative

--- geometry/haskell/src/Vectors.hs
+++ geometry/haskell/src/Vectors.hs
@@ -20,6 +20,7 @@ module Vectors
   )
 where
 
+import Control.Applicative (liftA2)
 import Matrices (Matrix (..))
 import Prelude hiding (abs, negate, sum)
 
@@ -38,11 +39,19 @@ instance Functor Vec2 where
   fmap :: (a -> b) -> Vec2 a -> Vec2 b
   fmap fn (Vec2 x y) = Vec2 (fn x) (fn y)
 
+instance Applicative Vec2 where
+  pure :: a -> Vec2 a
+  pure x = Vec2 x x
+  (<*>) :: Vec2 (a -> b) -> Vec2 a -> Vec2 b
+  (<*>) = liftA2 id
+  liftA2 :: (a -> b -> c) -> Vec2 a -> Vec2 b -> Vec2 c
+  liftA2 fn v1 v2 = fmap fn v1 <*> v2
+
 add :: Num t => Vec2 t -> Vec2 t -> Vec2 t
-add (Vec2 x1 y1) (Vec2 x2 y2) = Vec2 (x1 + x2) (y1 + y2)
+add = liftA2 (+)
 
 sub :: Num t => Vec2 t -> Vec2 t -> Vec2 t
-sub (Vec2 x1 y1) (Vec2 x2 y2) = Vec2 (x1 - x2) (y1 - y2)
+sub = liftA2 (-)
 
 negate :: Num t => Vec2 t -> Vec2 t
 negate = fmap (0 -)
@@ -56,7 +65,7 @@ invert :: Fractional t => Vec2 t -> Vec2 t
 invert = fmap (1 /)
 
 sum :: (Foldable s, Num t) => s (Vec2 t) -> Vec2 t
-sum = foldr1 add
+sum = foldr add $ Vec2 0 0
 
 magnitude :: Floating t => Vec2 t -> t
 magnitude (Vec2 x y) = hypot x y
@@ -73,15 +82,13 @@ distance (Vec2 x1 y1) (Vec2 x2 y2) =
       y = y2 - y1
    in hypot x y
 
-lerp :: Num t => Vec2 t -> Vec2 t -> t -> Vec2 t
-lerp (Vec2 x1 y1) (Vec2 x2 y2) n = Vec2 (interp x1 x2 n) (interp y1 y2 n)
+lerp :: Num t => t -> Vec2 t -> Vec2 t -> Vec2 t
+lerp n = liftA2 (interp n)
   where
-    interp x y t = (y - x) * t + x
+    interp t x y = (y - x) * t + x
 
 normalize :: Floating t => Vec2 t -> Vec2 t
-normalize v =
-  let m = magnitude v
-   in fmap (/ m) v
+normalize v = fmap (/ m) v where m = magnitude v
 
 transform :: Num t => Vec2 t -> Matrix t -> Vec2 t
 transform v m =
