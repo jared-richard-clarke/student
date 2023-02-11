c0f2449dceaa86db075efe1bea9135bf546129f2
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-10 18:23:44 -0800

    Create Vectors.Vec2 and Vectors.Vec3

--- geometry/haskell/src/Euclid.hs
+++ geometry/haskell/src/Euclid.hs
@@ -1,8 +1,10 @@
 module Euclid
   ( module Matrices,
-    module Vectors,
+    module Vectors.Vec2,
+    module Vectors.Vec3,
   )
 where
 
 import qualified Matrices
-import qualified Vectors
+import qualified Vectors.Vec2
+import qualified Vectors.Vec3
