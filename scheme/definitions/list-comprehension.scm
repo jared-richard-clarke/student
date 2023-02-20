b7eed3010b6fdc61775881d2ad68e1a0a7250c8e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-20 13:32:05 -0800

    Create list-comprehension.scm

---
+++ scheme/definitions/list-comprehension.scm
@@ -0,0 +1,31 @@
+;; (for (+ x y) [(x <- '(1 2 3)) (y <- '(4 5 6))])
+;;
+;; expands ->
+;;
+;; (bind '(1 2 3) (lambda (x)
+;;                 (bind '(4 5 6) (lambda (y)
+;;                                  (return (+ x y))))))
+;;
+;; returns ->
+;;
+;; '(5 6 7 6 7 8 7 8 9)
+
+(define-syntax for
+  (syntax-rules (<-)
+    [(_ expression [(x <- mx)])
+     (bind mx (lambda (x)
+                (return expression)))]
+    [(_ expression [(x <- mx) (y <- my) ...])
+     (bind mx (lambda (x)
+                (for expression [(y <- my) ...])))]))
+
+(define return
+  (lambda (x) (list x)))
+
+(define bind
+  (lambda (xs f)
+    (concat (map f xs))))
+
+(define concat
+  (lambda (xs)
+    (fold-right append '() xs)))
