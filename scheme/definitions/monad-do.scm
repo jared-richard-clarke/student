70671487e4fd085621bd1abdd17932ffa537fd0c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-21 13:40:30 -0800

    Expand macro to ignore values

--- scheme/definitions/monad-do.scm
+++ scheme/definitions/monad-do.scm
@@ -1,6 +1,21 @@
 ;; The Haskell "do" syntax (simplified). Makes monads readable.
 ;; "bind" must be defined in order for this macro to work.
 ;;
+
+(define-syntax monad-do
+  (lambda (stx)
+    (syntax-case stx (<-)
+      [(_ expression)
+       (syntax expression)]
+      [(_ (x <- mx) expression ...)
+       (syntax (bind mx (lambda (x) 
+                          (monad-do expression ...))))]
+      [(_ mx expression ...)
+       (syntax (bind mx (lambda ()
+                          (monad-do expression ...))))])))
+
+;; === EXAMPLES ===
+;;
 ;; (define and-then
 ;;   (lambda (px py)
 ;;     (monad-do (x <- px)
@@ -14,12 +29,19 @@
 ;;     (bind px (lambda (x)
 ;;                (bind py (lambda (y)
 ;;                           (return (cons x y))))))))
-
-(define-syntax monad-do
-  (lambda (stx)
-    (syntax-case stx (<-)
-      [(_ expression)
-       (syntax expression)]
-      [(_ (x <- mx) expression ...)
-       (syntax (bind mx (lambda (x) 
-                          (monad-do expression ...))))])))
+;;
+;; (define between
+;;   (lambda (px py pz)
+;;     (monad-do px
+;;               (y <- py)
+;;               pz
+;;               (return y))))
+;;
+;; expands ->
+;;
+;; (define between
+;;   (lambda (px py pz)
+;;     (bind px (lambda ()
+;;                (bind py (lambda (y)
+;;                           (bind pz (lambda () (return y)))))))))
+;;
