a97f6d44152e4b8f6d3c272c210873fcba52682b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-22 11:35:33 -0800

    Rename "for", "for-list"

--- scheme/definitions/list-comprehension.scm
+++ scheme/definitions/list-comprehension.scm
@@ -1,4 +1,4 @@
-;; (for (+ x y) [(x <- '(1 2 3)) (y <- '(4 5 6))])
+;; (for-list (+ x y) [(x <- '(1 2 3)) (y <- '(4 5 6))])
 ;;
 ;; expands ->
 ;;
@@ -10,14 +10,14 @@
 ;;
 ;; '(5 6 7 6 7 8 7 8 9)
 
-(define-syntax for
+(define-syntax for-list
   (syntax-rules (<-)
     [(_ expression [(x <- mx)])
      (bind mx (lambda (x)
                 (return expression)))]
     [(_ expression [(x <- mx) (y <- my) ...])
      (bind mx (lambda (x)
-                (for expression [(y <- my) ...])))]))
+                (for-list expression [(y <- my) ...])))]))
 
 (define return
   (lambda (x) (list x)))
