e2d9614209a61066bd5ac1f872b016865ce02fa1
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-22 05:03:13 -0800

    Replace "syntax-case" with "syntax-rules"

--- scheme/definitions/monad-do.scm
+++ scheme/definitions/monad-do.scm
@@ -2,18 +2,13 @@
 ;; "bind" must be defined in order for this macro to work.
 
 (define-syntax monad-do
-  (lambda (stx)
-    (syntax-case stx (<-)
-      [(_ expression)
-       (syntax expression)]
-      [(_ (x <- mx) expression ...)
-       (syntax (bind mx (lambda (x) 
-                          (monad-do expression ...))))]
-      [(_ mx expression ...)
-       (syntax (bind mx (lambda ()
-                          (monad-do expression ...))))])))
+  (syntax-rules (<-)
+    [(_ expression) expression]
+    [(_ (x <- mx) expression ...)
+     (bind mx (lambda (x) 
+                (monad-do expression ...)))]))
 
-;; === EXAMPLES ===
+;; === EXAMPLE ===
 ;;
 ;; (define and-then
 ;;   (lambda (px py)
@@ -28,19 +23,3 @@
 ;;     (bind px (lambda (x)
 ;;                (bind py (lambda (y)
 ;;                           (return (cons x y))))))))
-;;
-;; (define between
-;;   (lambda (px py pz)
-;;     (monad-do px
-;;               (y <- py)
-;;               pz
-;;               (return y))))
-;;
-;; expands ->
-;;
-;; (define between
-;;   (lambda (px py pz)
-;;     (bind px (lambda ()
-;;                (bind py (lambda (y)
-;;                           (bind pz (lambda () (return y)))))))))
-;;
