705b457194e0c10d353682505f1a266b0107ae20
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-22 15:28:30 -0800

    Add another example

--- scheme/definitions/list-comprehension.scm
+++ scheme/definitions/list-comprehension.scm
@@ -1,15 +1,3 @@
-;; (for-list (+ x y) [(x <- '(1 2 3)) (y <- '(4 5 6))])
-;;
-;; expands ->
-;;
-;; (bind '(1 2 3) (lambda (x)
-;;                  (bind '(4 5 6) (lambda (y)
-;;                                   (return (+ x y))))))
-;;
-;; returns ->
-;;
-;; '(5 6 7 6 7 8 7 8 9)
-
 (define-syntax for-list
   (syntax-rules (<-)
     [(_ expression [(x <- mx)])
@@ -29,3 +17,31 @@
 (define concat
   (lambda (xs)
     (fold-right append '() xs)))
+
+;; === example 1 ===
+;;
+;; (for-list (+ x y) [(x <- '(1 2 3)) (y <- '(4 5 6))])
+;;
+;; expands ->
+;;
+;; (bind '(1 2 3) (lambda (x)
+;;                  (bind '(4 5 6) (lambda (y)
+;;                                   (return (+ x y))))))
+;;
+;; returns ->
+;;
+;; '(5 6 7 6 7 8 7 8 9)
+;;
+;; === example 2 ===
+;;
+;; (for-list (cons x y) [(x <- '(a b)) (y <- '(1 2))])
+;;
+;; expands ->
+;;
+;; (bind '(a b) (lambda (x)
+;;                (bind '(1 2) (lambda (y)
+;;                               (return (cons x y))))))
+;;
+;; evaluates ->
+;;
+;; '((a . 1) (a . 2) (b . 1) (b . 2))
