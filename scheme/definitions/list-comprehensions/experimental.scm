7ff1e21c389850547949a10d65140975d358867d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-23 12:19:22 -0800

    Add failure context

--- scheme/definitions/list-comprehensions/experimental.scm
+++ scheme/definitions/list-comprehensions/experimental.scm
@@ -1,3 +1,10 @@
+
+;; (for-list x [(x <- '(1 2 3 4 5 6 7 8 9 10))] (even? x))
+;;
+;; evaluates ->
+;;
+;; '(2 4 6 8 10)
+
 (define-syntax for-list
   (syntax-rules (<-)
     [(_ expression [(x <- mx)])
@@ -10,25 +17,25 @@
      (bind mx (lambda (x)
                 (if predicate
                     (return expression)
-                    (return))))]
+                    (fail))))]
     [(_ expression [(x <- mx) (y <- my) ...] predicate)
      (bind mx (lambda (x)
                 (for-list expression [(y <- my) ...] predicate)))]))
 
+;; === monad ===
+
 (define return
-  (case-lambda [(x) (list x)]
-               [()  (list)]))
+  (lambda (x) (list x)))
 
 (define bind
   (lambda (xs f)
     (concat (map f xs))))
 
+(define fail
+  (lambda () (list)))
+
+;; === utils ===
+
 (define concat
   (lambda (xs)
     (fold-right append '() xs)))
-
-;; (for-list x [(x <- '(1 2 3 4 5 6 7 8 9 10))] (even? x))
-;;
-;; evaluates ->
-;;
-;; '(2 4 6 8 10)
