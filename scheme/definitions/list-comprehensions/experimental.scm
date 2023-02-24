1b8bb078803e759a0e6d4d6987c49d56f815778c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-23 16:20:01 -0800

    Add tests for monadic laws

--- scheme/definitions/list-comprehensions/experimental.scm
+++ scheme/definitions/list-comprehensions/experimental.scm
@@ -38,3 +38,37 @@
 (define concat
   (lambda (xs)
     (fold-right append '() xs)))
+
+;; === test utils ===
+
+(define-syntax assert
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ compare x y)
+       (syntax (let ([computed-x x]
+                     [computed-y y])
+                 (unless (compare computed-x computed-y)
+                   (printf "Test failed:\nlhs: ~a -> ~a, rhs: ~a -> ~a\n"
+                           (quote x)
+                           x
+                           (quote y)
+                           y))))])))
+
+;; === monad laws ===
+
+;; left identity: return x >>= f = f x
+
+(let ([add1 (lambda (x) (list (add1 x)))])
+  (assert equal? (bind (return 6) add1) (add1 6)))
+
+;; right identity: m >>= return = m
+
+(assert equal? (bind (list 7) return) (list 7))
+
+;; associativity: (m >>= f) >>= g = m >>= (\x -> f x >>= g)
+
+(let ([sub1 (lambda (x) (list (sub1 x)))]
+      [add2 (lambda (x) (list (add1 x)))])
+  (assert equal?
+          (bind (bind (list 7) sub1) add2)
+          (bind (list 7) (lambda (x) (bind (sub1 x) add2)))))
