3fbb147bfb697e92df3c58146f07b45423bd4a01
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-02-25 10:19:39 -0800

    Update and rename experimental.scm to list-comprehension.scm

--- scheme/definitions/list-comprehensions/list-comprehension.scm
+++ scheme/definitions/list-comprehensions/list-comprehension.scm
@@ -68,7 +68,7 @@
 ;; associativity: (m >>= f) >>= g = m >>= (\x -> f x >>= g)
 
 (let ([sub1 (lambda (x) (list (sub1 x)))]
-      [add2 (lambda (x) (list (add1 x)))])
+      [add1 (lambda (x) (list (add1 x)))])
   (assert equal?
-          (bind (bind (list 7) sub1) add2)
-          (bind (list 7) (lambda (x) (bind (sub1 x) add2)))))
+          (bind (bind (list 7) sub1) add1)
+          (bind (list 7) (lambda (x) (bind (sub1 x) add1)))))
