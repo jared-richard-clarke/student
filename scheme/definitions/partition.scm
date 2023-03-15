c1eb62af1ee773a22e5f0907021f228b7ccdfe50
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-03-15 11:45:24 -0700

    Replace list with values form

--- scheme/definitions/partition.scm
+++ scheme/definitions/partition.scm
@@ -1,10 +1,10 @@
-;; (partition function (list any)) -> (list (list any) (list any))
-;; Inputs a list and predicate and returns a pair of lists wherein 
+;; (partition function (list any)) -> (list any) (list any)
+;; Inputs a list and predicate and returns two lists wherein 
 ;; the first list contains all the elements that satisfy the predicate
 ;; and the second list contains all the elements that don't.
-;; (partition (lambda (x) (< x 5)) '(1 2 3 4 5 6 7 8 9 10)) -> '((1 2 3 4) (5 6 7 8 9 10))
+;; (partition even? '(1 2 3 4 5 6 7 8 9 10)) -> (2 4 6 8 10) (1 3 5 7 9)
 
-(define (partition pred xs)
-  (let ([yes (filter pred xs)]
-        [no  (filter (lambda (x) (not (pred x))) xs)])
-    (list yes no)))
+(define (partition test xs)
+  (let ([yes (filter test xs)]
+        [no  (filter (lambda (x) (not (test x))) xs)])
+    (values yes no)))
