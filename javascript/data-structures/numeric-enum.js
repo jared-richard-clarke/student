5706de884fe1420d125532b028adbe79a61cd7c4
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-03-29 14:45:47 -0700

    Revise internals

--- javascript/data-structures/numeric-enum.js
+++ javascript/data-structures/numeric-enum.js
@@ -1,23 +1,24 @@
-// enumerate(...values) -> Object { string(number): value | number }
+// enumerate(...values) -> Object { string | string(number): value | number }
 // Takes a variable number of arguments and returns an immutable, enumerated object.
-// Values are associated with a number from a series. The series starts at 0 and is incremented by 1.
+// Values are associated with a number from a series. The series starts at 1 and is incremented by 1.
 // enumerate("green", "yellow", "red") ->
 // {
-//   0: "green",
-//   1: "yellow",
-//   2: "red",
-//   green: 0,
-//   yellow: 1,
-//   red: 2,
+//   1: "green",
+//   2: "yellow",
+//   3: "red",
+//   green: 1,
+//   yellow: 2,
+//   red: 3,
 // }
 
-const enumerate = function (...values) {
-    const result = Object.create(null);
-    let number = 0;
-    values.forEach(function (value) {
-        result[value] = number;
-        result[number] = value;
-        number += 1;
-    });
-    return Object.freeze(result);
-};
+function enumerate(...values) {
+    let number = 1;
+    return Object.freeze(
+        values.reduce(function (accum, value) {
+            accum[value] = number;
+            accum[number] = value;
+            number += 1;
+            return accum;
+        }, Object.create(null))
+    );
+}
