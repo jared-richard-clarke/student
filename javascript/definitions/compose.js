1975f07afefc46e9df01c151f2236cfa0b3f0745
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-03-29 14:24:56 -0700

    Create compose.js

---
+++ javascript/definitions/compose.js
@@ -0,0 +1,25 @@
+// pipe(value, ...function) -> value
+// Transforms a value through a series of single-argument functions.
+// Evaluates left to right.
+// pipe("  hEllO, worLd   ", (text) => text.toLowerCase(), (text) => text.trim()) -> "hello, world"
+
+function pipe(input, ...actions) {
+    return actions.reduce(
+        (accum, action) => action(accum),
+        input
+    );
+}
+
+// compose(...function) -> function -> value
+// Composes a series of single-argument functions into a single function expression.
+// Evaluates right to left.
+// const edit = compose((text) => text.toLowerCase(), (text) => text.trim()) -> edit("  hEllO, worLd   ") -> "hello, world"
+
+function compose(...actions) {
+    return function (input) {
+        return actions.reduceRight(
+            (accum, action) => action(accum), 
+            input
+        );
+    };
+}
