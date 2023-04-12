d02a7f66ff280e58225052f6054c70e390b78ae0
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-04-11 17:45:51 -0700

    Create chunk.js

---
+++ javascript/definitions/chunk.js
@@ -0,0 +1,21 @@
+// chunk(xs: [any ...], size: number) -> [[any ...] ...]
+// Splits the given array into an array of arrays of the given size.
+// chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4); -> [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10]]
+
+function chunk(xs, size) {
+    if (size <= 0 || !Number.isInteger(size) || xs.length === 0) {
+        return xs.slice();
+    }
+
+    const chunks = [];
+    let read = 0;
+    let write = 0;
+
+    while (read < xs.length) {
+        chunks[write] = xs.slice(read, read + size);
+        write += 1;
+        read += size;
+    }
+
+    return chunks;
+}
