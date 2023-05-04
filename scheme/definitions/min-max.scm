6054d9ae8d4c5cb89d53488028284f414b3f0dad
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-04 13:06:08 -0700

    Rename compare, order

--- scheme/definitions/min-max.scm
+++ scheme/definitions/min-max.scm
@@ -1,21 +1,21 @@
-(define compare
-  (lambda (op id)
+(define order
+  (lambda (compare identity)
     (lambda (xs)
       (fold-left (lambda (old new) 
-                   (if (op old new) 
+                   (if (compare old new) 
                        old 
                        new))
-                 id
+                 identity
                  xs))))
 
 ;; (max (list number)) -> number
 ;; Returns the largest number in a list.
 ;; (max '(1/3 2.0 -3)) -> 2.0
 
-(define max (compare > -inf.0))
+(define max (order > -inf.0))
 
 ;; (min (list number)) -> number
 ;; Returns the smallest number in a list.
 ;; (min '(1/3 2.0 -3)) -> -3
 
-(define min (compare < +inf.0))
+(define min (order < +inf.0))
