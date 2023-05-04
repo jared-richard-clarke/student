b05e16ded83c1145827392c9d261c5299e983d3a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-04 00:41:05 -0700

    Revise documentation

--- scheme/definitions/min-max.scm
+++ scheme/definitions/min-max.scm
@@ -6,13 +6,13 @@
                  xs))))
 
 ;; (max (list number)) -> number
-;; Returns the maximum number of a list.
+;; Returns the largest number in a list.
 ;; (max '(1/3 2.0 -3)) -> 2.0
 
 (define max (compare > -inf.0))
 
 ;; (min (list number)) -> number
-;; Returns the minimum number of a list.
+;; Returns the smallest number in a list.
 ;; (min '(1/3 2.0 -3)) -> -3
 
 (define min (compare < +inf.0))
