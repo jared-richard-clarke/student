d678cd14f81ecc46501db75ab6de48e6d9896d2d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-04 00:45:56 -0700

    Update min-max.scm

--- scheme/definitions/min-max.scm
+++ scheme/definitions/min-max.scm
@@ -1,21 +1,21 @@
-(define comparison
-  (lambda (operation identity)
+(define compare
+  (lambda (op id)
     (lambda (xs)
       (fold-left (lambda (old new) 
-                   (if (operation old new) 
+                   (if (op old new) 
                        old 
                        new))
-                 identity
+                 id
                  xs))))
 
 ;; (max (list number)) -> number
 ;; Returns the largest number in a list.
 ;; (max '(1/3 2.0 -3)) -> 2.0
 
-(define max (comparison > -inf.0))
+(define max (compare > -inf.0))
 
 ;; (min (list number)) -> number
 ;; Returns the smallest number in a list.
 ;; (min '(1/3 2.0 -3)) -> -3
 
-(define min (comparison < +inf.0))
+(define min (compare < +inf.0))
