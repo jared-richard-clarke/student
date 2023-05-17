750fad31745c54b0e69752836ec64a890d9d7e4c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-17 16:03:29 -0700

    Rename order, bound

--- scheme/definitions/min-max.scm
+++ scheme/definitions/min-max.scm
@@ -1,4 +1,4 @@
-(define order
+(define bound
   (lambda (compare identity)
     (lambda (xs)
       (fold-left (lambda (old new) 
@@ -12,10 +12,10 @@
 ;; Returns the largest number in a list.
 ;; (max '(1/3 2.0 -3)) -> 2.0
 
-(define max (order > -inf.0))
+(define max (bound > -inf.0))
 
 ;; (min (list number)) -> number
 ;; Returns the smallest number in a list.
 ;; (min '(1/3 2.0 -3)) -> -3
 
-(define min (order < +inf.0))
+(define min (bound < +inf.0))
