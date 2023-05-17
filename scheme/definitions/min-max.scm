fe5cf7489ddb001b83666a6ff3f23aaffb33598d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-17 16:30:08 -0700

    Rename bound, bounded

--- scheme/definitions/min-max.scm
+++ scheme/definitions/min-max.scm
@@ -1,4 +1,9 @@
-(define bound
+;; bounded = abs(f(x)) <= m where m = real number
+
+;; Side Note: `max` and `min` are not truly bounded. Neither negative 
+;; nor positive infinity are real numbers.
+
+(define bounded
   (lambda (compare identity)
     (lambda (xs)
       (fold-left (lambda (old new) 
@@ -12,10 +17,10 @@
 ;; Returns the largest number in a list.
 ;; (max '(1/3 2.0 -3)) -> 2.0
 
-(define max (bound > -inf.0))
+(define max (bounded > -inf.0))
 
 ;; (min (list number)) -> number
 ;; Returns the smallest number in a list.
 ;; (min '(1/3 2.0 -3)) -> -3
 
-(define min (bound < +inf.0))
+(define min (bounded < +inf.0))
