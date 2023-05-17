2ad8a57b847c6a4a1e1ec14269e9bab295d7aec6
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-17 15:43:51 -0700

    Add rnrs-compliant version

--- scheme/definitions/assertions.scm
+++ scheme/definitions/assertions.scm
@@ -1,28 +1,11 @@
-;; (assert-test predicate expression value) -> current-output-port | (void)
-;; If expression does not evaluate to value, macro prints failed test to the current-output-port.
-;; (assert-test equal? (vec2 4 4) #(3 4)) ->
-;; "Test: (vec2 4 4)
-;;  Expect: #(3 4), Got: #(4 4)"
-
-(define-syntax assert-test
-  (lambda (stx)
-    (syntax-case stx ()
-      [(_ compare expression value)
-       (syntax (let ([computed-expr expression]) ;; <- prevents redundant computation
-                 (unless (compare computed-expr value)
-                   (printf "Test: ~a\nExpect: ~a, Got: ~a\n"
-                           (quote expression) ;; <---- returns expression prior to evaluation
-                           value
-                           computed-expr))))])))
-
-;; (assert-left-right predicate expression expression) -> current-output-port | (void)
-;; If the left-hand-expression does not equal the right-hand-expression,
-;; macro prints failed test to the current-output port.
-;; (assert-left-right equal? (+ 1 6) (- 1 6)) ->
+;; (assert predicate expression expression) -> current-output-port
+;; If the left-hand-expression does not satisfy the predicate comparing it to the right-hand-expression,
+;; `assert` prints the failed test to the current-output port.
+;; (assert equal? (+ 1 6) (- 1 6)) ->
 ;; "Test failed:
-;;  lhs: (+ 1 6) -> 7, rhs: (- 1 6) -> -5"
+;; lhs: (+ 1 6) -> 7, rhs: (- 1 6) -> -5"
 
-(define-syntax assert-left-right
+(define-syntax assert
   (syntax-rules ()
     [(_ compare x y)
      (let ([computed-x x]
@@ -33,3 +16,17 @@
                  computed-x
                  (quote y)
                  computed-y)))]))
+
+;; Version of `assert` compatible with `rnrs`
+
+(define-syntax assert
+  (syntax-rules ()
+    [(_ compare x y)
+     (let ([computed-x x]
+           [computed-y y])
+       (unless (compare computed-x computed-y)
+         (begin (display "Test failed:")
+                (newline)
+                (display "lhs: ") (display (quote x)) (display " -> ") (display computed-x) (display ", ")
+                (display "rhs: ") (display (quote y)) (display " -> ") (display computed-y)
+                (newline))))]))
