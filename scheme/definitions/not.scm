d99d9070429ea90d50839d90df2baa4426f54a7f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-30 16:06:42 -0700

    Revise arguments and documentation

--- scheme/definitions/not.scm
+++ scheme/definitions/not.scm
@@ -1,6 +1,7 @@
-;; (not obj) -> boolean
-;; Returns #t if value is false. Returns #f otherwise.
+;; (not any) -> boolean
+;; Outputs #t if input evaluates false. Returns #f otherwise.
+;; Follows Scheme implementation for evaluation of non-boolean expressions.
 ;; (not #f) -> #t
 
-(define (not value)
-  (if value #f #t))
+(define (not x)
+  (if x #f #t))
