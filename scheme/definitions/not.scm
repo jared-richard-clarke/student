11200d81a862e76901064dc06429e3516c9089a9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-30 16:09:38 -0700

    Revise documentation

--- scheme/definitions/not.scm
+++ scheme/definitions/not.scm
@@ -1,7 +1,9 @@
 ;; (not any) -> boolean
 ;; Outputs #t if input evaluates false. Returns #f otherwise.
 ;; Follows Scheme implementation for evaluation of non-boolean expressions.
+;; Generally, any expression that is not #f is equivalent to #t.
 ;; (not #f) -> #t
+;; (not 7)  -> #f
 
 (define (not x)
   (if x #f #t))
