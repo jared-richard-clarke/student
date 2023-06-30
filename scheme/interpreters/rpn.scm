3d34a7591c7005aa9ef06b67e73d579579402e0c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-30 02:42:41 -0700

    Create rpn.scm

---
+++ scheme/interpreters/rpn.scm
@@ -0,0 +1,21 @@
+;; Reverse Polish Notation calculator.
+;; (rpn '(2 3 * 1 + -)) -> '(-7)
+
+(define rpn
+  (let ([stack '()]
+        [environment (list (cons '+ +)
+                           (cons '- -)
+                           (cons '* *)
+                           (cons '/ /))]
+        [lookup (lambda (var env)
+                  (let ([result (assq var env)])
+                    (if result
+                        (cdr result)
+                        (error "undefined operator:" var))))])
+    (lambda (expression)
+      (fold-left (lambda (x total)
+                   (if (number? x)
+                       (cons x total)
+                       (cons (apply (lookup x environment) (reverse total)) stack)))
+                 stack
+                 expression))))
