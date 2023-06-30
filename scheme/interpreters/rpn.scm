70e488908d25e86240d3110710b8e02cddea8820
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-30 14:30:52 -0700

    Add "return" to environment

--- scheme/interpreters/rpn.scm
+++ scheme/interpreters/rpn.scm
@@ -2,24 +2,30 @@
 ;; (rpn '(2 3 × 1 + -)) -> '(-7)
 
 (define rpn
+  ;; === calculator environment ===
   (let ([stack '()]
-        [environment (list (cons '+ +)
-                           (cons '- -)
-                           (cons '* *)
-                           (cons '× *)
-                           (cons '/ /)
-                           (cons '÷ /))]
+        [table (list (cons '+ +)
+                     (cons '- -)
+                     (cons '* *)
+                     (cons '× *)
+                     (cons '/ /)
+                     (cons '÷ /))]
         [lookup (lambda (var env)
                   (let ([result (assq var env)])
                     (if result
                         (cdr result)
-                        (error "undefined operator:" var))))])
+                        (error "undefined operator:" var))))]
+        [return (lambda (stack)
+                  (if (= (length stack) 1)
+                      (car stack)
+                      (error "incomplete expression:" (reverse stack))))])
+    ;; === calculator function ===
     (lambda (expression)
-      (fold-left (lambda (x total)
-                   (if (number? x)
-                       (cons x total)
-                       (cons (apply (lookup x environment)
-                                    (reverse total))
-                             stack)))
-                 stack
-                 expression))))
+      (return (fold-left (lambda (x total)
+                           (if (number? x)
+                               (cons x total)
+                               (cons (apply (lookup x table)
+                                            (reverse total))
+                                     stack)))
+                         stack
+                         expression)))))
