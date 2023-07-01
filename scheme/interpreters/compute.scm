eb45262e14ba288b96fa70a5956415ee0ae9d6ac
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-30 18:12:54 -0700

    Replace external definitions with "let"

--- scheme/interpreters/compute.scm
+++ scheme/interpreters/compute.scm
@@ -1,52 +1,29 @@
-;; (compute list) -> number
-;; Evaluates arithmetic expressions in Scheme.
+;; Evaluates Scheme arithmetic expressions.
 ;; (compute '(+ 1 2 3 4 5 6 7 8 9 10)) -> 55
 
-(define env
-  (list [cons '+ +]
-        [cons '- -]
-        [cons '* *]
-        [cons '× *]
-        [cons '/ /]
-        [cons '÷ /]
-        [cons '^ expt]
-        [cons '√ sqrt]
-        [cons 'π 3.141592653589793]
-        [cons 'e 2.718281828459045]
-        [cons 'φ 1.618033988749894]))
-
-(define (lookup var env)
-  (let ([result (assq var env)])
-    (if result
-        (cdr result)
-        (error "unbound operator or constant: " var))))
-
-(define (compute expr)
-  (cond
-    [(symbol? expr) (lookup expr env)]
-    [(number? expr) expr]
-    [(pair? expr)
-     (apply (compute (car expr))
-            (map (lambda (x) (compute x))
-                 (cdr expr)))]
-    [else (error "invalid expression: " expr)]))
-
-;; === examples ===
-
-(define expression-1
-  '(+ 1
-      (× 2
-         3)))
-
-(define expression-2
-  '(+ (+ 1
-         2)
-      4))
-
-(define expression-3
-  '(+ (√ 25)
-      2))
-
-(compute expression-1) ; -> 7
-(compute expression-2) ; -> 7
-(compute expression-3) ; -> 7
+(define compute
+  (let ([table (list [cons '+ +]
+                     [cons '- -]
+                     [cons '* *]
+                     [cons '× *]
+                     [cons '/ /]
+                     [cons '÷ /]
+                     [cons '^ expt]
+                     [cons '√ sqrt]
+                     [cons 'π 3.141592653589793]
+                     [cons 'e 2.718281828459045]
+                     [cons 'φ 1.618033988749894])]
+        [lookup (lambda (var env)
+                  (let ([result (assq var env)])
+                    (if result
+                        (cdr result)
+                        (error "undefined operator or constant:" var))))])
+    (lambda (expression)
+      (cond
+        [(symbol? expression) (lookup expression table)]
+        [(number? expression) expression]
+        [(pair? expression)
+         (apply (compute (car expression))
+                (map (lambda (x) (compute x))
+                     (cdr expression)))]
+        [else (error "invalid expression:" expression)]))))
