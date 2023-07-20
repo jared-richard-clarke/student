4f04402b7c60d5a54160d56b56cde2426e8b3b3c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-07-19 17:29:05 -0700

    Redefine generator in terms of producer and consumer

--- scheme/data-structures/generator.scm
+++ scheme/data-structures/generator.scm
@@ -1,24 +1,34 @@
-;; (generate-numbers number number? number?) -> function -> number | 'end
-;; Returns a function that yields a number incremented until it reaches
-;; the end of its specified range. Every subsequent call returns the symbol 'end.
-;;
-;; (define yield (generate-numbers 2))     -> (list (yield) (yield) (yield)) -> '(1 2 end)
-;; (define yield (generate-numbers 1 2))   -> (list (yield) (yield) (yield)) -> '(1 2 end)
-;; (define yield (generate-numbers 1 6 5)) -> (list (yield) (yield) (yield)) -> '(1 6 end)
+;; End flag for communication between producer and consumer.
+(define END 'end)
 
-(define generate-numbers
-  (let ([END 'end])
-    (case-lambda
-      [(stop)
-       (generate-numbers 1 stop 1)]
-      [(start stop)
-       (generate-numbers start stop 1)]
-      [(start stop step)
-       (if (<= step 0)
-           END
-           (lambda ()
-             (if (> start stop)
-                 END
-                 (let ([result start])
-                   (set! start (+ start step))
-                   result))))])))
+;; producer
+(define range
+  (case-lambda
+    [(stop)
+     (range 1 stop 1)]
+    [(start stop)
+     (range start stop 1)]
+    [(start stop step)
+     (if (<= step 0)
+         END
+         (lambda ()
+           (if (> start stop)
+               END
+               (let ([result start])
+                 (set! start (+ start step))
+                 result))))]))
+
+;; consumer factory
+(define consumer
+  (lambda (op id)
+    (lambda (yield)
+      (let loop ([accum id]
+                 [value (yield)])
+        (if (eq? value END)
+            accum
+            (loop (op accum value)
+                  (yield)))))))
+
+;; consumers of range
+(define product (consumer * 1))
+(define sum     (consumer + 0))
