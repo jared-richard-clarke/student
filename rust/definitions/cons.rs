53c113548172ca0cb3729ec45abe15e4990920cd
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-08-28 00:18:00 -0700

    Replace cons list with lengthier example

--- rust/definitions/cons.rs
+++ rust/definitions/cons.rs
@@ -1,24 +1,36 @@
-// Example pulled from The Rust Programming Language, chapter 15.1
+// Linked List pulled from Rust By Example
 
-#[derive(Debug)]
+use crate::List::*;
+
 enum List {
-    Cons(i32, Box<List>),
+    Cons(u32, Box<List>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
+impl List {
+    fn new() -> List {
+        Nil
+    }
 
-// Potentially infinite in size, recursive data structures cannot exist on the stack.
-// They must be allocated on the heap and pointed to by reference.
-// Box<T> provides this facility.
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
 
-fn main() {
-    // The Box<T> type is a smart pointer. It implements the Deref trait,
-    // which allows Box<T> values to be treated like references.
-    // Box<T> also implements the Drop trait, which cleans up heap data
-    // when the Box<T> pointer falls out of scope.
+    fn length(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.length(),
+            Nil => 0
+        }
+    }
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
-    println!("{:?}", list); // -> Cons(1, Cons(2, Cons(3, Nil)))
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
 }
