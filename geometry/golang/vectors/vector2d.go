64b315154c848c2be235f8d176864bf725cd4eb3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-12-01 22:33:32 -0800

    Replace with generics

--- geometry/golang/vectors/vector2d.go
+++ geometry/golang/vectors/vector2d.go
@@ -2,91 +2,123 @@ package vectors
 
 import (
 	"didact/geometry/golang/matrices"
-	"didact/geometry/golang/utils"
+	"didact/geometry/golang/types"
 	"fmt"
 	"math"
 )
 
-// A two-dimensional cartesian vector. Represented by a two-part array.
-type Vec2 [2]float64
+// A two-dimensional cartesian vector.
+type Vec2[T types.Float] struct{ X, Y T }
 
 // As opposed to operator "==", function "ApproxEq" tests whether
 // floating-point, 2D vector components are approximately equal.
-// Check "didact/geometry/golang/utils/approx-eq.go" for details.
-func (v1 Vec2) ApproxEq(v2 Vec2) bool {
-	eq := utils.ApproxEq
-	return eq(v1[0], v2[0]) && eq(v1[1], v2[1])
+func (v1 Vec2[T]) ApproxEq(v2 Vec2[T]) bool {
+	eq := types.ApproxEq[T]
+	x1 := v1.X
+	y1 := v1.Y
+	x2 := v2.X
+	y2 := v2.Y
+	return eq(x1, x2) && eq(y1, y2)
 }
 
 // Returns a 2D vector with the absolute values of its components.
-func (v Vec2) Abs() Vec2 {
-	return Vec2{math.Abs(v[0]), math.Abs(v[1])}
+func (v Vec2[T]) Abs() Vec2[T] {
+	x := T(math.Abs(float64(v.X)))
+	y := T(math.Abs(float64(v.Y)))
+	return Vec2[T]{x, y}
 }
 
 // Returns a 2D vector that is the sum of two 2D vectors.
-func (v1 Vec2) Add(v2 Vec2) Vec2 {
-	return Vec2{v1[0] + v2[0], v1[1] + v2[1]}
+func (v1 Vec2[T]) Add(v2 Vec2[T]) Vec2[T] {
+	x1 := v1.X
+	y1 := v1.Y
+	x2 := v2.X
+	y2 := v2.Y
+	return Vec2[T]{x1 + x2, y1 + y2}
 }
 
 // Returns a 2D vector that is the difference of two 2D vectors.
-func (v1 Vec2) Sub(v2 Vec2) Vec2 {
-	return Vec2{v1[0] - v2[0], v1[1] - v2[1]}
+func (v1 Vec2[T]) Sub(v2 Vec2[T]) Vec2[T] {
+	x1 := v1.X
+	y1 := v1.Y
+	x2 := v2.X
+	y2 := v2.Y
+	return Vec2[T]{x1 - x2, y1 - y2}
 }
 
 // Flips the signs of the 2D vector components.
-func (v Vec2) Negate() Vec2 {
-	return Vec2{-v[0], -v[1]}
+func (v Vec2[T]) Negate() Vec2[T] {
+	x := v.X
+	y := v.Y
+	return Vec2[T]{-x, -y}
 }
 
 // Inverts the 2D vector components.
-func (v Vec2) Invert() Vec2 {
-	return Vec2{1.0 / v[0], 1.0 / v[1]}
+func (v Vec2[T]) Invert() Vec2[T] {
+	x := v.X
+	y := v.Y
+	one := T(1)
+	return Vec2[T]{one / x, one / y}
 }
 
 // Returns the magnitude of a 2D vector.
-func (v Vec2) Magnitude() float64 {
-	return math.Hypot(v[0], v[1])
+func (v Vec2[T]) Magnitude() T {
+	x := float64(v.X)
+	y := float64(v.Y)
+	return T(math.Hypot(x, y))
 }
 
 // Multiplies a 2D vector by a scalar.
-func (v Vec2) Scale(s float64) Vec2 {
-	return Vec2{v[0] * s, v[1] * s}
+func (v Vec2[T]) Scale(s T) Vec2[T] {
+	x := v.X
+	y := v.Y
+	return Vec2[T]{x * s, y * s}
 }
 
 // Computes the dot product of two 2D vectors.
-func (v1 Vec2) Dot(v2 Vec2) float64 {
-	return v1[0]*v2[0] + v1[1]*v2[1]
+func (v1 Vec2[T]) Dot(v2 Vec2[T]) T {
+	x1 := v1.X
+	y1 := v1.Y
+	x2 := v2.X
+	y2 := v2.Y
+	return x1*x2 + y1*y2
 }
 
 // Calculates the distance between two 2D vector points.
-func (v1 Vec2) Distance(v2 Vec2) float64 {
+func (v1 Vec2[T]) Distance(v2 Vec2[T]) T {
 	return v2.Sub(v1).Magnitude()
 }
 
 // Interpolates the components of the two 2D vectors.
-func (v1 Vec2) Lerp(t float64, v2 Vec2) Vec2 {
+func (v1 Vec2[T]) Lerp(t T, v2 Vec2[T]) Vec2[T] {
 	return v1.Add(v2.Sub(v1).Scale(t))
 }
 
 // Returns the unit vector of a 2D vector.
-func (v Vec2) Normalize() Vec2 {
+func (v Vec2[T]) Normalize() Vec2[T] {
 	mag := v.Magnitude()
-	return Vec2{v[0] / mag, v[1] / mag}
+	x := v.X
+	y := v.Y
+	return Vec2[T]{x / mag, y / mag}
 }
 
 // Returns a 2D vector with its components rounded.
-func (v Vec2) Round() Vec2 {
-	return Vec2{math.Round(v[0]), math.Round(v[1])}
+func (v Vec2[T]) Round() Vec2[T] {
+	x := T(math.Round(float64(v.X)))
+	y := T(math.Round(float64(v.Y)))
+	return Vec2[T]{x, y}
 }
 
-func (v Vec2) TransformBy(m matrices.Mat3) Vec2 {
-	return Vec2{
-		m[0]*v[0] + m[2]*v[1],
-		m[1]*v[0] + m[3]*v[1],
+func (v Vec2[T]) TransformBy(m matrices.Mat3[T]) Vec2[T] {
+	return Vec2[T]{
+		m.A*v.X + m.C*v.Y,
+		m.B*v.X + m.D*v.Y,
 	}
 }
 
 // Fulfills the Stringer interface for Vec2.
-func (v Vec2) String() string {
-	return fmt.Sprintf("vec2(%.2f, %.2f)", v[0], v[1])
+func (v Vec2[T]) String() string {
+	x := v.X
+	y := v.Y
+	return fmt.Sprintf("Vec2(%v, %v)", x, y)
 }
