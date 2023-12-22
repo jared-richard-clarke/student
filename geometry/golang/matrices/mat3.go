7c69b3a0d6d7aff83c382a36c49083aac7f5661b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-12-21 20:49:38 -0800

    Remove generics

--- geometry/golang/matrices/mat3.go
+++ geometry/golang/matrices/mat3.go
@@ -2,29 +2,24 @@
 package matrices
 
 import (
-	"didact/geometry/golang/types"
+	utils "didact/geometry/golang/utilities"
 	"math"
-)
-
-const (
-	zero = 0
-	one  = 1
 )
 
 // A 3 × 3 affine transformation matrix. Parameters are described in
 // column-major order. Constants are implied.
-type Mat3[T types.Float] struct{ A, B, C, D, E, F T }
+type Mat3 struct{ A, B, C, D, E, F float64 }
 
 // As opposed to operator "==", method "ApproxEq" checks whether
 // floating-point matrix components are approximately equal.
-func (m Mat3[T]) ApproxEq(n Mat3[T]) bool {
-	eq := types.ApproxEq[T]
+func (m Mat3) ApproxEq(n Mat3) bool {
+	eq := utils.ApproxEq
 	return eq(m.A, n.A) && eq(m.B, n.B) && eq(m.C, n.C) && eq(m.D, n.D) && eq(m.E, n.E) && eq(m.F, n.F)
 }
 
 // Combines matrix transformations through multiplication.
-func (m Mat3[T]) Multiply(n Mat3[T]) Mat3[T] {
-	return Mat3[T]{
+func (m Mat3) Multiply(n Mat3) Mat3 {
+	return Mat3{
 		m.A*n.A + m.B*n.C,
 		m.A*n.B + m.B*n.D,
 		m.C*n.A + m.D*n.C,
@@ -35,49 +30,48 @@ func (m Mat3[T]) Multiply(n Mat3[T]) Mat3[T] {
 }
 
 // Creates an identity matrix.
-func Identity[T types.Float]() Mat3[T] {
-	return Mat3[T]{one, zero, zero, one, zero, zero}
+func Identity() Mat3 {
+	return Mat3{1, 0, 0, 1, 0, 0}
 }
 
 // Creates a translation matrix.
-func Translate[T types.Float](x, y T) Mat3[T] {
-	return Mat3[T]{one, zero, zero, one, x, y}
+func Translate(x, y float64) Mat3 {
+	return Mat3{1, 0, 0, 1, x, y}
 }
 
 // Creates a scaling matrix.
-func Scale[T types.Float](x, y T) Mat3[T] {
-	return Mat3[T]{x, zero, zero, y, zero, zero}
+func Scale(x, y float64) Mat3 {
+	return Mat3{x, 0, 0, y, 0, 0}
 }
 
 // Creates a rotation matrix.
-func Rotate[T types.Float](angle T) Mat3[T] {
-	a := float64(angle)
-	c := T(math.Cos(a))
-	s := T(math.Sin(a))
-	return Mat3[T]{c, s, -s, c, zero, zero}
+func Rotate(angle float64) Mat3 {
+	c := math.Cos(angle)
+	s := math.Sin(angle)
+	return Mat3{c, s, -s, c, 0, 0}
 }
 
 // Creates a shearing matrix.
-func Shear[T types.Float](x, y T) Mat3[T] {
-	return Mat3[T]{one, y, x, one, zero, zero}
+func Shear(x, y float64) Mat3 {
+	return Mat3{1, y, x, 1, 0, 0}
 }
 
 // Translates matrix by scalars "x" and "y". Transformation can be chained.
-func (m Mat3[T]) Translate(x, y T) Mat3[T] {
+func (m Mat3) Translate(x, y float64) Mat3 {
 	return Translate(x, y).Multiply(m)
 }
 
 // Scales matrix by scalars "x" and "y". Transformation can be chained.
-func (m Mat3[T]) Scale(x, y T) Mat3[T] {
+func (m Mat3) Scale(x, y float64) Mat3 {
 	return Scale(x, y).Multiply(m)
 }
 
 // Rotates matrix by "angle", measured in radians. Transformation can be chained.
-func (m Mat3[T]) Rotate(angle T) Mat3[T] {
+func (m Mat3) Rotate(angle float64) Mat3 {
 	return Rotate(angle).Multiply(m)
 }
 
 // Shears matrix by scalars "x" and "y". Transformation can be chained.
-func (m Mat3[T]) Shear(x, y T) Mat3[T] {
+func (m Mat3) Shear(x, y float64) Mat3 {
 	return Shear(x, y).Multiply(m)
 }
