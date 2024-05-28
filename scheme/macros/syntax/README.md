e518497f208e41ecf53e1a9787228c8e65ba935e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-05-27 19:58:37 -0700

    Delete scheme/macros/syntax directory

--- scheme/macros/syntax/README.md
+++
@@ -1,5 +0,0 @@
-# Syntax and Macro Expansion
-
-> "Transformers normally destructure their input with **syntax-case** and rebuild their output with **syntax**."
->
-> — The Scheme Programming Language, section 8.3, R. Kent Dybvig
--- scheme/macros/syntax/syntax-rules.scm
+++
@@ -1,9 +0,0 @@
-;; syntax-rules defined in terms of syntax-case.
-
-(define-syntax syntax-rules
-  (lambda (stx)
-    (syntax-case stx ()
-      [(_ (i ...) ((keyword . pattern) template) ...)
-       (syntax (lambda (stx)
-                 (syntax-case stx (i ...)
-                   [(_ . pattern) (syntax template)] ...)))])))
