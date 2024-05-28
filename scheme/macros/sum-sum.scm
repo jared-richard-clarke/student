2240fa1796b510ddf8796763d55c6707ed3d0f08
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-05-28 12:47:33 -0700

    Revise documentation

--- scheme/macros/sum-sum.scm
+++ scheme/macros/sum-sum.scm
@@ -1,8 +1,9 @@
-; In order to use a recursive function with let, I must pass the function to itself.
-; Normally, I would use either letrec or letrec* syntax for recursive functions.
-; I include this awkward example to help me better understand variable binding in Scheme.
+;; In order to use a recursive function with "let", I must pass the function to itself.
+;; Normally, I would use either "letrec" or "letrec*" syntax for recursive functions.
+;; I include this awkward example to help me better understand variable binding and
+;; recursion in Scheme.
 
-; === let expression ===
+;; === let expression ===
 (let ([sum (lambda (sum lst)
              (if (null? lst)
                  0
@@ -10,7 +11,7 @@
                     (sum sum (cdr lst)))))])
   (sum sum '(1 2 3 4 5 6 7)))
 
-; === macro expansion ===
+;; === lambda expansion ===
 ((lambda (sum) (sum sum '(1 2 3 4 5 6 7)))
  (lambda (sum lst)
    (if (null? lst)
