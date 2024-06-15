fc30422d561924f72a6faca4c57a82c8059fa920
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-15 16:53:19 -0700

    Use "assoc", not "assq", for deep comparisons

--- scheme/definitions/memoize.scm
+++ scheme/definitions/memoize.scm
@@ -1,12 +1,13 @@
-;; (memoize function) -> function
+;; (memoize function) -> (function any ...) -> any
 ;; Wraps a function in a function that caches values so that they need not be recomputed.
+;; Maps a tuple of inputs to its output within an associative list.
 ;; (define fib-cache (memoize fibonacci))
 
 (define (memoize fn)
   (define cache '())
   (lambda xs
     (cond
-      [(assq xs cache) => cdr]
+      [(assoc xs cache) => cdr]
       [else
        (let ([result (apply fn xs)])
          (set! cache (cons (cons xs result) cache))
