f18406cfde5f1d5cbb533ade8b9b53dd38693085
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-15 19:18:10 -0700

    Create vector-count.scm

---
+++ scheme/definitions/vectors/vector-count.scm
@@ -0,0 +1,13 @@
+;; (vector-count function vector vectors ...) -> number
+;; Counts the number of elements from one or more
+;; vectors that satisfy the given predicate.
+;; (vector-count (lambda (x y) (and (< x 3) (< y 3))) '#(1 2) '#(-2 0 4)) -> 2
+
+(define vector-count
+  (lambda (predicate v . vs)
+    (apply vector-fold-left
+           (lambda (count . x)
+             (+ count (if (apply predicate x) 1 0)))
+           0
+           v
+           vs)))
