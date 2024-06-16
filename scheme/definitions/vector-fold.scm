7b265652fd63ce66aa0bd5a96b870a00c8272db6
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-15 17:24:40 -0700

    Rename parameter "f", "fn"

--- scheme/definitions/vector-fold.scm
+++ scheme/definitions/vector-fold.scm
@@ -1,22 +1,22 @@
 ;; (vector-fold function any vector) -> any
-;; Combines vector elements piecewise into an accumulative value using function "f".
+;; Combines vector elements piecewise into an accumulative value using function "fn".
 ;; The combining function takes two arguments: the accumulative value and the next
 ;; element in the vector. "base" provides the initial value for the accumulator.
 ;; (vector-fold + 0 '#(1 2 3 4 5 6 7 8 9 10)) -> 55
 (define vector-fold
-  (lambda (f base xs)
+  (lambda (fn base xs)
     (let loop ([size (vector-length xs)]
                [index 0]
                [accum base])
       (if (>= index size)
           accum
-          (loop size (+ index 1) (f accum (vector-ref xs index)))))))
+          (loop size (+ index 1) (fn accum (vector-ref xs index)))))))
 
 ;; === Alternative ===
 ;;
-;; (define (vector-fold f base xs)
+;; (define (vector-fold fn base xs)
 ;;   (define (loop size index accum)
 ;;     (if (>= index size)
 ;;         accum
-;;         (loop size (+ index 1) (f accum (vector-ref xs index)))))
+;;         (loop size (+ index 1) (fn accum (vector-ref xs index)))))
 ;;   (loop (vector-length xs) 0 base))
