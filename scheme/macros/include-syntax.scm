c9cd0beb3750a580d833c52a197a99f3d429fbc9
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-17 16:53:02 -0700

    Create include-syntax.scm

---
+++ scheme/macros/include-syntax.scm
@@ -0,0 +1,23 @@
+;; (include string) -> (begin expression ...)
+;; Expands into a "begin" expression containing the forms found in the file named by "string".
+;; Defined according to the "Chez Scheme User Guide" by R. Kent Dybvig.
+
+;; The "include" form uses "datum->syntax" to convert the objects read from the file
+;; into syntax objects in the proper lexical context, so that identifier references
+;; and definitions within those expressions are scoped where the "include"
+;; form appears.
+
+(define-syntax include
+  (lambda (x)
+    (define read-file
+      (lambda (fn k)
+        (let ([p (open-input-file fn)])
+          (let loop ([x (read p)])
+            (if (eof-object? x)
+                (begin (close-port p) '())
+                (cons (datum->syntax k x) (loop (read p))))))))
+    (syntax-case x ()
+      [(k filename)
+       (let ([fn (syntax->datum (syntax filename))])
+         (with-syntax ([(expr ...) (read-file fn (syntax k))])
+           (syntax (begin expr ...))))])))
