98c4e4056c7312c6e0399ef4db16ac3adaae2aff
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-18 15:36:27 -0700

    Add example

--- scheme/definitions/traverse.scm
+++ scheme/definitions/traverse.scm
@@ -1,6 +1,7 @@
 ;; (traverse list function) -> list
 ;; Traverses sequences, both flat and recursive, applying
 ;; an arbitrary function to each one of its nodes.
+;; (traverse '((1 2) 3 (4)) add1) -> '((2 3) 4 (5))
 
 (define traverse
   (lambda (xs fn)
