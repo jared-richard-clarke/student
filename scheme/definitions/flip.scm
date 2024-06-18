9ce64acc74d7672969f9b70f96aa4f476b2f579b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-18 15:20:50 -0700

    Delete scheme/definitions/flip.scm

--- scheme/definitions/flip.scm
+++
@@ -1,8 +0,0 @@
-;; (flip function) -> (function x y) -> (function y x)
-;; Applies a two-argument function with its arguments flipped.
-;; ((flip -) 3 10) -> 7
-
-(define flip
-  (lambda (f)
-    (lambda (x y)
-      (f y x))))
