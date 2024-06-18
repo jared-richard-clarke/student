ac734510b5c3bcb35b1a9474d3390c0813de0d14
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-18 15:31:41 -0700

    Update traverse.scm

--- scheme/definitions/traverse.scm
+++ scheme/definitions/traverse.scm
@@ -1,19 +1,10 @@
-;; traverse tree
-;; Many tree operations can be implemented via combinations
-;; of sequence operations and recursion.
+;; (traverse list function) -> list
+;; Traverses sequences, both flat and recursive, applying
+;; an arbitrary function to each one of its nodes.
 
-(define (traverse-1 tree action)
-  (cond [(null? tree) null]
-        [(not (pair? tree)) (action tree)]
-        [else (cons (traverse-1 (car tree) action)
-                    (traverse-1 (cdr tree) action))]))
-
-;; traverse tree: map implementation
-;; Regards tree as a sequence of sub-trees.
-
-(define (traverse tree action)
+(define (traverse tree fn)
   (map (lambda (sub-tree)
          (if (pair? sub-tree)
-             (traverse sub-tree action)
-             (action sub-tree)))
+             (traverse sub-tree fn)
+             (fn sub-tree)))
        tree))
