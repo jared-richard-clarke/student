e3b6ca42191fc706316eda3350036a8c0e3195e2
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-20 13:41:16 -0700

    Add unzip

--- scheme/definitions/zip.scm
+++ scheme/definitions/zip.scm
@@ -5,3 +5,11 @@
 (define zip
   (lambda lists
     (apply map list lists)))
+
+;; (unzip list) -> (values list list)
+;; Transforms a list of pairs into a list of cars and a list of cdrs.
+;; (unzip '((1 2 3) (4 5 6) (7 8 9))) -> (values (1 4 7) ((2 3) (5 6) (8 9)))
+
+(define unzip
+  (lambda (xs)
+    (values (map car xs) (map cdr xs))))
