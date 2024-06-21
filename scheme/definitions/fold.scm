a2e153cfacbb1b844c9d692e7d0d06370e8ad96a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-20 22:11:26 -0700

    Add fold of arbitrary arity

--- scheme/definitions/fold.scm
+++ scheme/definitions/fold.scm
@@ -28,3 +28,36 @@
 ;; === Side Note ===
 ;; Racket:      (foldl cons 'a '(b c)) -----> '(c b . a)
 ;; Chez Scheme: (fold-left cons 'a '(b c)) -> '((a . b) . c)
+
+(define map-onto
+  (lambda (fn xs ys)
+    (let loop ([xs (reverse xs)]
+               [ys ys])
+      (if (null? xs)
+          ys
+          (loop (cdr xs)
+                (cons (fn (car xs)) ys))))))
+
+;; (fold function any list lists) -> any
+;; This particular definition of fold takes a function, a starting
+;; accumulator, and one or more lists. The function must take as
+;; many arguments as their are lists plus the accumulator.
+;; The accumulation terminates as soon as the shortest list has
+;; been exhausted. Folds left to right.
+;; (fold + 10 '(9 8 7) '(6 5 4) '(3 2 1)) -> 55
+
+(define fold
+  (lambda (fn base x . xs)
+    (if (null? xs)
+        (let loop ([x     x]
+                   [accum base])
+          (if (pair? x)
+              (loop (cdr x)
+                    (fn (car x) accum))
+              accum))
+        (let loop ([xs    (cons x xs)]
+                   [accum base])
+          (if (for-all pair? xs)
+              (loop (map cdr xs)
+                    (apply fn (map-onto car xs (list accum))))
+              accum)))))
