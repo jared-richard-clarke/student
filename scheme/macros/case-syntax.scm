e5cf23004c9d477b0badd863fc3c8c535e227d17
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-21 16:24:59 -0700

    Create case-syntax.scm

---
+++ scheme/macros/case-syntax.scm
@@ -0,0 +1,34 @@
+;; Syntactic form "case" as defined in "The Scheme Programming Language" by R. Kent Dybvig.
+
+(define-syntax case
+  (lambda (x)
+    (syntax-case x ()
+      [(_ e c1 c2 ...)
+       #`(let ([t e])
+           #,(let loop ([c1 #'c1] [cx #'(c2 ...)])
+               (if (null? cx)
+                   (syntax-case c1 (else)
+                     [(else e1 e2 ...) #'(begin e1 e2 ...)]
+                     [((k ...) e1 e2 ...)
+                      #'(if (memv t '(k ...)) (begin e1 e2 ...))])
+                   (syntax-case c1 ()
+                     [((k ...) e1 e2 ...)
+                      #`(if (memv t '(k ...))
+                            (begin e1 e2 ...)
+                            #,(loop (car cx) (cdr cx)))]))))])))
+
+(let ([x 4] [y 5])
+  (case (+ x y)
+    [(1 3 5 7 9) 'odd]
+    [(0 2 4 6 8) 'even]
+    [else 'out-of-range]))
+
+;; - expands ->
+
+(let ([x 4] [y 5])
+  (let ([t (+ x y)])
+    (if (memv t '(1 3 5 7 9))
+        (begin 'odd)
+        (if (memv t '(0 2 4 6 8))
+            (begin 'even)
+            (begin 'out-of-range)))))
