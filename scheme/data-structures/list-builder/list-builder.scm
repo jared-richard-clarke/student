358b94b0d7ef66847da657d6a06ae79fb2482032
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-06-28 19:58:26 -0700

    Create list-builder.scm

---
+++ scheme/data-structures/list-builder/list-builder.scm
@@ -0,0 +1,48 @@
+(library (list-builder)
+         (export for <- when)
+         (import (rnrs base)
+                 (rnrs lists))
+
+         (define-syntax for
+           (syntax-rules (<- when)
+             ;; base case
+             [(for ([x <- mx]) expression)
+              (concat-map mx (lambda (x)
+                               (list expression)))]
+             ;; recursive case
+             [(for ([x <- mx] [y <- my] ...) expression)
+              (concat-map mx (lambda (x)
+                               (for ([y <- my] ...) expression)))]
+             ;; base case with predicate
+             [(for ([x <- mx]) (when predicate) expression)
+              (concat-map mx (lambda (x)
+                               (if predicate
+                                   (list expression)
+                                   empty)))]
+             ;; recursive case with predicate
+             [(for ([x <- mx] [y <- my] ...) (when predicate) expression)
+              (concat-map mx (lambda (x)
+                               (for ([y <- my] ...) (when predicate) expression)))]))
+
+         ;; === monad ===
+
+         (define empty '())
+
+         ;; (define concat
+         ;;   (lambda (xs)
+         ;;     (fold-right append empty xs)))
+         ;;
+         ;; Calling map between concatenations produces intermediary lists.
+         ;; Avoid extra allocations by integrating map directly into concat.
+         ;;
+         ;; (define concat-map
+         ;;   (lambda (xs f)
+         ;;     (concat (map f xs))))
+
+         (define concat-map
+           (lambda (xs f)
+             (if (null? xs)
+                 empty
+                 (append (f (car xs)) (concat-map (cdr xs) f)))))
+
+         )
