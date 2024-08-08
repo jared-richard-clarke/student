abc24b4c69b946d4898bfc2f1278da277bb4ad5c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-08-08 16:20:08 -0700

    Replace code with simpler definition

--- rust/definitions/channel.rs
+++ rust/definitions/channel.rs
@@ -1,86 +1,35 @@
 // === A channel type as implemented by Mara Bos for "Rust Atomics and Locks" ===
+// This channel type is a queue for sharing data between multiple threads.
 
-use std::cell::UnsafeCell;
-use std::marker::PhantomData;
-use std::mem::MaybeUninit;
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::{Acquire, Release};
-use std::thread;
-use std::thread::Thread;
+use std::collections::VecDeque;
+use std::sync::Condvar;
+use std::sync::Mutex;
 
 pub struct Channel<T> {
-    message: UnsafeCell<MaybeUninit<T>>,
-    ready: AtomicBool,
-}
-
-unsafe impl<T> Sync for Channel<T> where T: Send {}
-
-pub struct Sender<'a, T> {
-    channel: &'a Channel<T>,
-    receiving_thread: Thread,
-}
-
-pub struct Receiver<'a, T> {
-    channel: &'a Channel<T>,
-    _no_send: PhantomData<*const ()>,
+    queue: Mutex<VecDeque<T>>,
+    item_ready: Condvar,
 }
 
 impl<T> Channel<T> {
-    pub const fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            message: UnsafeCell::new(MaybeUninit::uninit()),
-            ready: AtomicBool::new(false),
+            queue: Mutex::new(VecDeque::new()),
+            item_ready: Condvar::new(),
         }
     }
 
-    pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
-        *self = Self::new();
-        (
-            Sender {
-                channel: self,
-                receiving_thread: thread::current(),
-            },
-            Receiver {
-                channel: self,
-                _no_send: PhantomData,
+    pub fn send(&self, message: T) {
+        self.queue.lock().unwrap().push_back(message);
+        self.item_ready.notify_one();
+    }
+
+    pub fn receive(&self) -> T {
+        let mut b = self.queue.lock().unwrap();
+        loop {
+            if let Some(message) = b.pop_front() {
+                return message;
             }
-        )
-    }
-}
-
-impl<T> Sender<'_, T> {
-    pub fn send(self, message: T) {
-        unsafe { (*self.channel.message.get()).write(message) };
-        self.channel.ready.store(true, Release);
-        self.receiving_thread.unpark();
-    }
-}
-
-impl<T> Receiver<'_, T> {
-    pub fn receive(self) -> T {
-        while !self.channel.ready.swap(false, Acquire) {
-            thread::park();
-        }
-        unsafe { (*self.channel.message.get()).assume_init_read() }
-    }
-}
-
-impl<T> Drop for Channel<T> {
-    fn drop(&mut self) {
-        if *self.ready.get_mut() {
-            unsafe { self.message.get_mut().assume_init_drop() }
+            b = self.item_ready.wait(b).unwrap();
         }
     }
-}
-
-#[test]
-fn main() {
-    let mut channel = Channel::new();
-    thread::scope(|s| {
-        let (sender, receiver) = channel.split();
-        s.spawn(move || {
-            sender.send("hello world!");
-        });
-        assert_eq!(receiver.receive(), "hello world!");
-    });
 }
