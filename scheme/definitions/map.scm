9fe5f616c68bad1d227b9576e010db1294b21c06
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-08-30 15:21:43 -0700

    Update map.scm

--- scheme/definitions/map.scm
+++ scheme/definitions/map.scm
@@ -3,15 +3,15 @@
 ;; and returns a list of resulting values.
 ;; (map + '(1 2 3) '(1 2 3)) -> '(2 4 6)
 
-(define (map fun x . xs)
+(define (map fn x . xs)
   (if (null? xs)
       (let map-x ([x x])
         (if (null? x)
             '()
-            (cons (fun (car x)) (map-x (cdr x)))))
+            (cons (fn (car x)) (map-x (cdr x)))))
       (let map-xs ([x x]
                    [xs xs])
         (if (null? x)
             '()
-            (cons (apply fun (car x) (map car xs))
+            (cons (apply fn (car x) (map car xs))
                   (map-xs (cdr x) (map cdr xs)))))))
