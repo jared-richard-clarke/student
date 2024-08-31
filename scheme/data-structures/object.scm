cb484cfed9078a0398ee4c5609bc212f84f9ecb3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-08-31 16:22:46 -0700

    Add expanded form

--- scheme/data-structures/object.scm
+++ scheme/data-structures/object.scm
@@ -32,3 +32,25 @@
                (set! state (cdr state))
                item)))
    (clear! (lambda () (set! state '())))])
+
+;; - expands ->
+;;
+;; (define stack
+;;   (lambda ()
+;;     (let* ([id 'stack] [state '()])
+;;       (letrec ([type   (lambda () id)]
+;;                [empty? (lambda () (null? state))]
+;;                [push!  (lambda xs (set! state (append xs state)))]
+;;                [peek   (lambda () (car state))]
+;;                [pop!   (lambda () (let ([item (car state)]) (set! state (cdr state)) item))]
+;;                [clear! (lambda () (set! state '()))])
+;;         (lambda (message . arguments)
+;;           (case message
+;;             [(type)   (apply type arguments)]
+;;             [(empty?) (apply empty? arguments)]
+;;             [(push!)  (apply push! arguments)]
+;;             [(peek)   (apply peek arguments)]
+;;             [(pop!)   (apply pop! arguments)]
+;;             [(clear!) (apply clear! arguments)]
+;;             [else
+;;              (assertion-violation 'stack "invalid input" (cons message arguments))]))))))
