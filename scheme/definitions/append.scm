cf5356555040071ca3080b7283a1b1614ee6c22e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-04 09:12:58 -0700

    Define "append" only in terms of "fold-right"

--- scheme/definitions/append.scm
+++ scheme/definitions/append.scm
@@ -1,25 +1,5 @@
 ;; (append (list any) ...) -> (list any)
 ;; Combines elements of multiple lists into a single list.
-;; (append '(1 2 3) '())  -> '(1 2 3)
-;; (append '(1 2) '(3 4) '(5 6 7)) -> '(1 2 3 4 5 6 7)
-
-(define append
-  (lambda args
-    (let loop-1 ([lst '()]
-                 [args args])
-      (if (null? args)
-          lst
-          (let loop-2 ([lst lst])
-            (if (null? lst)
-                (loop-1 (car args)
-                        (cdr args))
-                (cons (car lst)
-                      (loop-2 (cdr lst)))))))))
-
-;; === alternative ===
-
-;; (append (list any) ...) -> (list any)
-;; Combines elements of multiple lists into a single list.
 ;; (append '(1 2) '(3 4) '(5 6 7)) -> '(1 2 3 4 5 6 7)
 
 (define append
