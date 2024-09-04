394ec816bd2ef9c2930b5304c60c028591708cce
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-04 09:14:21 -0700

    Create list.scm

---
+++ scheme/definitions/list.scm
@@ -0,0 +1,18 @@
+;; (list? any) -> boolean
+;; Checks if object is a proper list.
+;; (list? '(x y z))   -> #t
+;; (list? '(x y . z)) -> #f
+;; (list? x)          -> #f
+(define list?
+  (lambda (xs)
+    (let loop ([hare xs] [tortoise xs])
+      (if (pair? hare)
+          (let ([hare (cdr hare)])
+            (if (pair? hare)
+                (and (not (eq? hare tortoise))
+                     (loop (cdr hare)
+                           (if (pair? tortoise)
+                               (cdr tortoise)
+                               hare)))
+                (null? hare)))
+          (null? hare)))))
