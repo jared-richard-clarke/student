ce748855d99a41753542963825dd56181acde311
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-04 08:59:30 -0700

    Define "filter" only in terms of "fold-right"

--- scheme/definitions/filter.scm
+++ scheme/definitions/filter.scm
@@ -2,20 +2,11 @@
 ;; Returns list whose elements satisfy the given predicate function.
 ;; (filter odd? '(1 2 3 4 5 6 7)) -> '(1 3 5 7)
 
-(define (filter test xs)
-  (cond [(null? xs) '()]
-        [(test (car xs))
-         (cons (car xs)
-               (filter test (cdr xs)))]
-        [else  (filter test (cdr xs))]))
-
-;; === alternate ===
-
 (define filter
   (lambda (test xs)
-    (fold-right (lambda (x acc)
+    (fold-right (lambda (x accum)
                   (if (test x)
-                      (cons x acc)
-                      acc))
+                      (cons x accum)
+                      accum))
                 '()
                 xs)))
