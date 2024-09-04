c6f34dfb5fd6b674171a97d767d681abb84820f8
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-04 09:04:51 -0700

    Consolidate "flat-map" and "flatten"

--- scheme/definitions/flatten.scm
+++ scheme/definitions/flatten.scm
@@ -1,10 +1,19 @@
+;; (flat-map function (list any)) -> (list any)
+;; Flattens a nested list to its base elements and transforms
+;; those elements with an arbitrary function.
+;; (flat-map (lambda (x) (+ x 2)) '(1 ((2) (3)))) -> '(3 4 5)
+
+(define flat-map
+  (lambda (fn xs)
+    (cond [(null? xs) '()]
+          [(not (pair? xs)) (list (fn xs))]
+          [else (append (flat-map fn (car xs))
+                        (flat-map fn (cdr xs)))])))
+
 ;; (flatten (list any)) -> (list any)
 ;; Flattens a nested list to its base elements.
 ;; (flatten '(((1 2) 3) 4)) -> '(1 2 3 4)
 (define flatten
   (lambda (xs)
-    (cond
-      [(null? xs) '()]
-      [(not (pair? xs)) (list xs)]
-      [else (append (flatten (car xs))
-                    (flatten (cdr xs)))])))
+    (let ([id (lambda (x) x)])
+      (flat-map id xs))))
