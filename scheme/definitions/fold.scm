ab98684bb703c8be369a84da7eeb005f8423725f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-05 18:56:26 -0700

    Remove "map-onto", update examples

--- scheme/definitions/fold.scm
+++ scheme/definitions/fold.scm
@@ -29,22 +29,14 @@
 ;; Racket:      (foldl cons 'a '(b c)) -----> '(c b . a)
 ;; Chez Scheme: (fold-left cons 'a '(b c)) -> '((a . b) . c)
 
-(define map-onto
-  (lambda (fn xs ys)
-    (let loop ([xs (reverse xs)]
-               [ys ys])
-      (if (null? xs)
-          ys
-          (loop (cdr xs)
-                (cons (fn (car xs)) ys))))))
-
 ;; (fold function any list lists) -> any
 ;; This particular definition of fold takes a function, a starting
 ;; accumulator, and one or more lists. The function must take as
 ;; many arguments as their are lists plus the accumulator.
 ;; The accumulation terminates as soon as the shortest list has
 ;; been exhausted. Folds left to right.
-;; (fold + 10 '(9 8 7) '(6 5 4) '(3 2 1)) -> 55
+;; (fold + 10 '(9 8 7) '(6 5 4) '(3 2 1))       -> 55
+;; (fold list 'init '(1 2 3) '(3 4 5) '(6 7 8)) -> (((init 1 3 6) 2 4 7) 3 5 8)
 
 (define fold
   (lambda (fn base x . xs)
