9637cdd09464484940776df655a9d40bba80fbeb
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-08 21:26:58 -0700

    Create promise.scm

---
+++ scheme/data-structures/promise.scm
@@ -0,0 +1,43 @@
+;; "The syntactic form 'delay' and the procedure 'force' may be used in combination
+;;  to implement lazy evaluation. An expression subject to lazy evaluation is not
+;;  evaluated until its value is required and, once evaluated, is never reevaluated."
+;;
+;; — The Scheme Programming Language, chapter 5.7, by R. Kent Dybvig
+
+(define-syntax delay
+  (syntax-rules ()
+    [(_ expr) 
+     (promise (lambda () expr))]))
+
+(define promise
+  (lambda (thunk)
+    (unless (procedure? thunk)
+      (assertion-violation 'promise "argument is not a procedure" thunk))
+    (let ([value #f] [set? #f])
+      (lambda ()
+        (case set?
+          [(single) value]
+          [(multiple) (apply values value)]
+          [else
+           (call-with-values
+             thunk
+             (case-lambda
+               [(x)
+                (case set?
+                  [(single) value]
+                  [(multiple) (apply values value)]
+                  [(#f) (set! value x)
+                        (set! set? 'single)
+                        x])]
+               [xs
+                (case set?
+                  [(single) value]
+                  [(multiple) (apply values value)]
+                  [(#f) (set! value x)
+                        (set! set? 'multiple)
+                        (apply values xs)])]))])))))
+(define force
+  (lambda (promise)
+    (unless (procedure? promise)
+      (assertion-violation 'force "argument is not a procedure" promise))
+    (promise)))
