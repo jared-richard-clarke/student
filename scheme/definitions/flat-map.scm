9dda6a919889413d65d4576d07ac55bcce8fa2aa
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-09-17 17:10:02 -0700

    Update flat-map.scm

--- scheme/definitions/flat-map.scm
+++ scheme/definitions/flat-map.scm
@@ -1,25 +1,8 @@
 ;; (flat-map function (list any)) -> (list any)
-;; Flattens a nested list to its base elements and transforms
-;; those elements with an arbitrary function.
-;; (flat-map (lambda (x) (+ x 2)) '(1 ((2) (3)))) -> '(3 4 5)
+;; Maps a function over a list of lists, then flattens that list by one level.
+;; (flat-map (lambda (x) (list (+ (car x) 1))) '((1) (2) (3))) -> '(2 3 4)
 
-(define flat-map
-  (lambda (fn xs)
-    (cond [(null? xs) '()]
-          [(not (pair? xs)) (list (fn xs))]
-          [else (append (flat-map fn (car xs))
-                        (flat-map fn (cdr xs)))])))
-
-;; (flatten (list any)) -> (list any)
-;; Flattens a nested list to its base elements.
-;; (flatten '(((1 2) 3) 4)) -> '(1 2 3 4)
-(define flatten
-  (lambda (xs)
-    (let ([id (lambda (x) x)])
-      (flat-map id xs))))
-
-;; === Side Note ===
-;; An alternative definition of "flat-map" is the monadic definition "bind":
+;; Side Note: The monadic definition of "flat-map" is "bind".
 ;; flat-map: (a -> [b]) -> [a] -> [b]
 ;; bind:     [a] -> (a -> [b]) -> [b]
 
