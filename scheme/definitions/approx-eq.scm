1dc4637b4a532c8166e187d51050022fc82a6c00
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2026-04-07 15:58:01 -0700

    Update approx-eq.scm

--- scheme/definitions/approx-eq.scm
+++ scheme/definitions/approx-eq.scm
@@ -1,13 +1,12 @@
 ;; (approx-eq? number number) -> boolean
-;; Tests for approximate equality between two floating-point numbers within an absolute
-;; or relative tolerance of TOLERANCE. An absolute tolerance is used for values
-;; less than or equal to 1.0. A relative tolerance is used for larger values.
+;; Tests for equality between two numbers. If those two numbers are unequal,
+;; tests for approximate equality within a tolerance of 1e-7.
 ;; (approx-eq? 0.2 0.19999999) -> #t
 
-(define (approx-eq? x y)
-  (let ([TOLERANCE 1e-7])
-    (<= (abs (- x y))
-        (* TOLERANCE
-           (max 1.0
-                (abs x)
-                (abs y))))))
+(define approx-eq?
+  (lambda (x y)
+    (let ([TOLERANCE 1e-7])
+      (or (= x y)
+          (< (/ (abs (- x y))
+                (max (abs x) (abs y)))
+             TOLERANCE)))))
